af68d6b2ef0d867f5ef1070ba11678a48bcb77f9
Michael Bostock <mbostock@gmail.com>
2010-10-04 20:57:56 -0700

    Quantile color scale.

---
+++ examples/calendar/quantile.js
@@ -0,0 +1,29 @@
+function quantile(array, n) {
+  var thresholds = [],
+      i = 0;
+      k = (array.length - 1) / n;
+
+  array.sort(function(a, b) { return a < b ? -1 : a > b ? 1 : 0; });
+  thresholds[0] = array[0];
+  while (++i < n) thresholds[i] = array[~~(i * k)];
+
+  function quantile(value) {
+    if (isNaN(value)) return NaN;
+    var low = 0, high = n - 1;
+    while (low <= high) {
+      var mid = (low + high) >> 1, midValue = thresholds[mid];
+      if (midValue < value) low = mid + 1;
+      else if (midValue > value) high = mid - 1;
+      else return mid;
+    }
+    return high < 0 ? 0 : high;
+  }
+
+  return quantile;
+}
+
+function values(map) {
+  var array = [], key;
+  for (key in map) array.push(map[key]);
+  return array;
+}
