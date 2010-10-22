d7bc33eafde93ad2caff504e620669f4f856cbe5
Michael Bostock <mbostock@gmail.com>
2010-10-22 12:55:50 -0700

    First pass at ticks for linear scale.

--- src/linear.js
+++ src/linear.js
@@ -32,5 +32,26 @@ d3.linear = function() {
     return scale;
   };
 
+  // TODO Dates? Ugh.
+  scale.ticks = function(m) {
+    var start = Math.min(x0, x1),
+        stop = Math.max(x0, x1),
+        span = stop - start,
+        step = Math.pow(10, Math.floor(Math.log(span / m) / Math.LN10)),
+        err = m / (span / step),
+        ticks = [],
+        digits;
+    if (err <= .15) step *= 10;
+    else if (err <= .35) step *= 5;
+    else if (err <= .75) step *= 2;
+    start = Math.ceil(start / step) * step;
+    stop = Math.floor(stop / step) * step + step * .5; // inclusive
+    digits = Math.max(0, -Math.floor(Math.log(step) / Math.LN10 + .01));
+    do {
+      ticks.push({value: start, label: start.toFixed(digits)});
+    } while ((start += step) < stop);
+    return ticks;
+  };
+
   return scale;
 };
