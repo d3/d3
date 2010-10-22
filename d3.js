5e381ba73e1426217ef498b0d4d791bdf4e0307d
Michael Bostock <mbostock@gmail.com>
2010-10-22 13:36:48 -0700

    Export a tickFormat function.
    The tick format function doesn't have to be used with ticks, though. It's also
    for convenient for determining an appropriate precision for any value label.

--- d3.js
+++ d3.js
@@ -664,24 +664,38 @@ d3.linear = function() {
   };
 
   // TODO Dates? Ugh.
-  scale.ticks = function(m) {
+  function ticks(m) {
     var start = Math.min(x0, x1),
         stop = Math.max(x0, x1),
         span = stop - start,
         step = Math.pow(10, Math.floor(Math.log(span / m) / Math.LN10)),
         err = m / (span / step),
-        ticks = [],
         digits;
+
+    // Filter ticks to get closer to the desired count.
     if (err <= .15) step *= 10;
     else if (err <= .35) step *= 5;
     else if (err <= .75) step *= 2;
+
+    // Round start and stop values to step interval.
     start = Math.ceil(start / step) * step;
     stop = Math.floor(stop / step) * step + step * .5; // inclusive
+
+    // Determine the precision for tick labels.
     digits = Math.max(0, -Math.floor(Math.log(step) / Math.LN10 + .01));
-    do {
-      ticks.push({value: start, label: start.toFixed(digits)});
-    } while ((start += step) < stop);
-    return ticks;
+
+    return {
+      range: function() { return d3.range(start, stop, step); },
+      format: function(d) { return d.toFixed(digits); }
+    };
+  }
+
+  scale.ticks = function(m) {
+    return ticks(m).range();
+  };
+
+  scale.tickFormat = function(m) {
+    return ticks(m).format;
   };
 
   return scale;
