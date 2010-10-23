04a6dce44c3a78cb10cb261cb0b89109db966ec4
Michael Bostock <mbostock@gmail.com>
2010-10-23 10:19:37 -0700

    Checkpoint transitions (d3.alt).

--- d3.alt.js
+++ d3.alt.js
@@ -6,6 +6,8 @@
 
   d3_document.data = [];
 
+  d3.version = "0.1.0";
+
   d3.ns = {
     prefix: {
       svg: "http://www.w3.org/2000/svg",
@@ -43,7 +45,11 @@
         : d3_selection([d3_array(query)], d3_root); // assume node[]
   };
 
-  function d3_selection(groups, deselect) {
+  d3.transition = function() {
+    return d3_root.transition();
+  };
+
+  function d3_selection(groups, deselect, transition) {
     var nodes = d3_blend(groups);
 
     /**
@@ -53,19 +59,19 @@
      */
     function visit(value, callback) {
       if (typeof value == "function") {
-        for (var i = 0, n = groups.length; i < n; i++) {
+        for (var i = 0, k = 0, n = groups.length; i < n; i++) {
           var group = groups[i], data = group.data;
           data.unshift(null);
           for (var j = 0, m = group.length; j < m; j++) {
             var node = group[j];
             data[0] = node.__data__;
-            callback(node, value.apply(node, data));
+            callback(node, value.apply(node, data), k++);
           }
           data.shift();
         }
       } else {
         for (var i = 0, n = nodes.length; i < n; i++) {
-          callback(nodes[i], value);
+          callback(nodes[i], value, i);
         }
       }
     }
@@ -78,7 +84,7 @@
         });
         subgroup.data = group.data;
         return subgroup;
-      }), nodes);
+      }), nodes, transition);
     };
 
     // TODO selectAll(function)?
@@ -89,12 +95,20 @@
           subgroup.data = [node.__data__].concat(group.data);
           return subgroup;
         });
-      })), nodes);
+      })), nodes, transition);
     };
 
     nodes.deselect = function() {
       return deselect;
     };
+
+    // I think rather than having a data stack, it might make more sense to
+    // pass the datum and an index (or a local index / group index) to each
+    // value function. If you want to use the data stack, you can use a data
+    // function that derives child data from the parent data. (The additional
+    // arguments in Protovis were often a source of confusion.) Passing the
+    // index as an additional argument makes the behavior consistent with the
+    // built-in map and forEach methods.
 
     // TODO key
     // TODO enter
@@ -213,7 +227,7 @@
           return e.appendChild(document.createElement(name));
         });
       }
-      return d3_selection(children, nodes);
+      return d3_selection(children, nodes, transition);
     };
 
     // TODO remove(node)?
@@ -229,14 +243,91 @@
     // TODO event
     // TODO on?
 
-    // TODO filter, slice? or d3.selectAll(node[])?
+    // TODO filter, slice?
 
-    // TODO transition
-    // TODO delay
-    // TODO duration
-    // TODO easing
+    nodes.transition = function() {
+      return d3_transition(nodes, visit);
+    };
 
     return nodes;
+  }
+
+  function d3_transition(nodes, visit) {
+    var transition = {},
+        tweenAttrs = {},
+        timeout = setTimeout(start, 1),
+        interval,
+        then,
+        delay = [],
+        duration = [],
+        durationMax = 0,
+        easing = []; // TODO
+
+    function start() {
+      then = Date.now();
+      interval = setInterval(tick, 24);
+    }
+
+    function tick() {
+      var elapsed = Date.now() - then;
+      if (elapsed > durationMax) clearInterval(interval);
+      visit(null, function(node, _, i) {
+        var t = (elapsed - delay[i]) / duration[i]; // TODO easing
+        if (t < 0) return;
+        if (t >= 1) { t = 1; delay[i] = Infinity; }
+        for (var name in tweenAttrs) {
+          node.setAttribute(name, tweenAttrs[name][i](t));
+        }
+      });
+    }
+
+    transition.delay = function(value) {
+      var delayMin = Infinity;
+      visit(value, function(node, value, i) {
+        delay[i] = value;
+        if (value < delayMin) delayMin = value;
+      });
+      clearTimeout(timeout);
+      timeout = setTimeout(start, delayMin);
+      return transition;
+    };
+
+    transition.duration = function(value) {
+      durationMax = 0;
+      visit(value, function(node, value, i) {
+        duration[i] = value;
+        if (value > durationMax) durationMax = value;
+      });
+      return transition;
+    };
+
+    // TODO custom easing function?
+    // transition.easing = function(value) {
+    //   visit(value, function(node, value, i) { easing[i] = value; });
+    //   return transition;
+    // };
+
+    transition.attr = function(name, value) {
+      var tweens = tweenAttrs[name] = [];
+      visit(value, function(node, value, i) {
+        tweens[i] = d3_tween(node.getAttribute(name), value);
+      });
+    };
+
+    // transition.select = function(query) {
+    //   var select = d3_transition(nodes.select(query)), i;
+    //   i = 0; select.delay(function() { return delay[i++]; }); // TODO index
+    //   i = 0; select.duration(function() { return duration[i++]; });
+    //   return select;
+    // };
+
+    return transition.delay(0).duration(250);
+  }
+
+  function d3_tween(a, b) {
+    return function(t) {
+      return a * (1 - t) + b * t;
+    };
   }
 
 })(this);
