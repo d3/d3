b9ad0a4af1acd107678e2cd92be0678bef1b2e61
Jeffrey Heer <jeffrey.heer@gmail.com>
2010-12-06 20:48:11 -0800

    Minor tweaks and beautification

--- src/geom/contour.js
+++ src/geom/contour.js
@@ -4,16 +4,30 @@
  * marching squares</a> algorithm. Returns the contour polygon as an
  * array of points [[x1, y1], [x2, y2], ...]
  *
- * @param grid a two input (x,y) function that returns either 0 or 1
+ * @param grid a two input (x,y) function that returns 1 or true for values
+ *  inside the contour and 0 or false for values outside the contour
  * @param start [x1, y1] an optional starting point on the grid
  */
 d3["geom"]["contour"] = function(grid, start) {
-  var s = start || d3_geom_contour_start(grid),
-      c = [], x = s[0], y = s[1],
-      i, dx=0, dy=0, pdx=NaN, pdy=NaN;
+  var s = start || d3_geom_contour_start(grid), // starting point
+      c = [],    // contour polygon
+      x = s[0],  // current x position
+      y = s[1],  // current y position
+      dx = 0,    // next x direction
+      dy = 0,    // next y direction
+      pdx = NaN, // previous x direction
+      pdy = NaN, // previous y direction
+      i = 0;
 
   do {
-    i = grid(x-1,y-1) + 2*grid(x,y-1) + 4*grid(x-1,y) + 8*grid(x,y);
+	// determine marching squares index
+	i = 0;
+	if (grid(x-1, y-1)) i += 1;
+	if (grid(x,   y-1)) i += 2;
+	if (grid(x-1, y  )) i += 4;
+	if (grid(x,   y  )) i += 8;
+	
+	// determine next direction
     if (i == 6) {
       dx = pdy==-1 ? -1 : 1;
       dy = 0;
@@ -24,10 +38,13 @@ d3["geom"]["contour"] = function(grid, start) {
       dx = d3_geom_contour_dx[i];
       dy = d3_geom_contour_dy[i];
     }
+
+    // update contour polygon
 	if (dx != pdx && dy != pdy) {
       c.push([x, y]);
       pdx = dx; pdy = dy;
     }
+    
     x += dx;
     y += dy;
   } while (s[0] != x || s[1] != y);
@@ -35,13 +52,16 @@ d3["geom"]["contour"] = function(grid, start) {
   return c;
 }
 
+// lookup tables for marching directions
 var d3_geom_contour_dx = [1, 0, 1, 1,-1, 0,-1, 1,0, 0,0,0,-1, 0,-1,NaN];
 var d3_geom_contour_dy = [0,-1, 0, 0, 0,-1, 0, 0,1,-1,1,1, 0,-1, 0,NaN];
 
 function d3_geom_contour_start(grid) {
+  var x = 0,
+      y = 0;
+
   // search for a starting point; begin at origin
-  // and proceed along outward-expanding diagonals 
-  var x = 0, y = 0;
+  // and proceed along outward-expanding diagonals
   while (true) {
     if (grid(x,y)) {
       return [x,y];
