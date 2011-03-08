9857dade141670d6d51606df1f36b129e4cf52bf
Michael Bostock <mbostock@gmail.com>
2011-03-08 14:23:31 -0800

    Woot, requestAnimationFrame!

--- d3.js
+++ d3.js
@@ -1902,8 +1902,9 @@ function d3_timer(callback, delay) {
 }
 
 function d3_timer_start() {
-  d3_timer_interval = setInterval(d3_timer_step, 24);
+  d3_timer_interval = 1;
   d3_timer_timeout = 0;
+  d3_timer_frame(d3_timer_step);
 }
 
 function d3_timer_step() {
@@ -1917,6 +1918,7 @@ function d3_timer_step() {
     t1 = (t0 = t1).next;
   }
   d3_timer_flush();
+  if (d3_timer_interval) d3_timer_frame(d3_timer_step);
 }
 
 // Flush after callbacks, to avoid concurrent queue modification.
@@ -1928,8 +1930,15 @@ function d3_timer_flush() {
         ? (t0 ? t0.next = t1.next : d3_timer_queue = t1.next)
         : (t0 = t1).next;
   }
-  if (!t0) d3_timer_interval = clearInterval(d3_timer_interval);
+  if (!t0) d3_timer_interval = 0;
 }
+
+var d3_timer_frame = window.requestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.oRequestAnimationFrame
+    || window.msRequestAnimationFrame
+    || function(callback) { setTimeout(callback, 17); };
 d3.scale = {};
 d3.scale.linear = function() {
   var x0 = 0,
