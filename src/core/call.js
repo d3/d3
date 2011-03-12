651fb56c0a0b1f97a643f9e268a8891cbc22bf4b
Mike Bostock <mbostock@gmail.com>
2011-03-12 08:25:49 -0800

    Minor tweaks to d3_call and tests.

--- src/core/call.js
+++ src/core/call.js
@@ -1,5 +1,15 @@
+//
+// Note: assigning to the arguments array simultaneously changes the value of
+// the corresponding argument! However, the Google Closure compiler doesn't
+// realize this, and so can optimize-away our attempt to avoid side-effects.
+// This fix by Jason Davies has been tested to survive minimization.
+//
+// TODO The `this` argument probably shouldn't be the first argument to the
+// callback, anyway, since it's redundant. However, that will require a major
+// version bump due to backwards compatibility, so I'm not changing it right
+// away.
+//
 function d3_call(callback) {
-  var f = callback;
-  f.apply(this, (arguments[0] = this) && arguments);
+  callback.apply(this, (arguments[0] = this, arguments));
   return this;
 }
--- tests/test-call.js
+++ tests/test-call.js
@@ -2,10 +2,20 @@ require("./../lib/env-js/envjs/node");
 require("./../lib/sizzle/sizzle");
 require("./../d3.min");
 
-var vis = d3.select("body").append('svg:svg');
+var vis = d3.select("body");
 
-console.log("simple:");
+console.log("zero arguments:");
 vis.call(function(a, b) {
-  console.log("  ", b);
+  console.log("  this:", vis === this ? "vis" : this);
+  console.log("  a:", vis === a ? "vis" : a);
+  console.log("  b:", b);
+});
+console.log("");
+
+console.log("one argument:");
+vis.call(function(a, b) {
+  console.log("  this:", vis === this ? "vis" : this);
+  console.log("  a:", vis === a ? "vis" : a);
+  console.log("  b:", b);
 }, "test");
 console.log("");
