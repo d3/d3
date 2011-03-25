1dfd3350dcd78ce29f76f4f15da0f94ca219ffad
Jason Davies <jason@jasondavies.com>
2011-03-25 10:25:43 +0000

    Replace Google's closure compiler with UglifyJS.
    UglifyJS requires Node.js to run, but it's a lot faster than Google's
    compiler and produces smaller gzipped sizes.  Some of the non-gzipped
    sizes are a bit larger than Google's but I think the gzipped size is
    more important.  Faster runtime is also good when we start testing the
    minified versions too.

--- d3.js
+++ d3.js
@@ -195,19 +195,10 @@ function d3_splitter(d) {
 function d3_collapse(s) {
   return s.replace(/(^\s+)|(\s+$)/g, "").replace(/\s+/g, " ");
 }
-//
-// Note: assigning to the arguments array simultaneously changes the value of
-// the corresponding argument! However, the Google Closure compiler doesn't
-// realize this, and so can optimize-away our attempt to avoid side-effects.
-// This fix by Jason Davies has been tested to survive minimization.
-//
-// TODO The `this` argument probably shouldn't be the first argument to the
-// callback, anyway, since it's redundant. However, that will require a major
-// version bump due to backwards compatibility, so I'm not changing it right
-// away.
-//
 function d3_call(callback) {
-  callback.apply(this, (arguments[0] = this, arguments));
+  var f = callback;
+  arguments[0] = this;
+  f.apply(this, arguments);
   return this;
 }
 /**
