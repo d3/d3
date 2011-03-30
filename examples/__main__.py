3b2a08a77abef4eb53d681161a19ad8b82cd8628
Jason Davies <jason@jasondavies.com>
2011-03-30 10:32:09 +0100

    Default filename `index.html` for examples server.

--- examples/__main__.py
+++ examples/__main__.py
@@ -8,11 +8,11 @@ tornado.options.define("port", default=8888, type=int)
 
 if __name__ == "__main__":
   tornado.options.parse_command_line()
-  application = tornado.web.Application([], **{
-    "static_path": ".",
-    "static_url_prefix": "/"
-  })
+  application = tornado.web.Application([
+    (r"/(.*)", tornado.web.StaticFileHandler,
+      {"path": ".", "default_filename": "index.html"}),
+  ])
   http_server = tornado.httpserver.HTTPServer(application)
   http_server.listen(tornado.options.options.port)
-  print "http://localhost:%d/examples/index.html" % tornado.options.options.port
+  print "http://localhost:%d/examples/" % tornado.options.options.port
   tornado.ioloop.IOLoop.instance().start()
