b08ecad5ed71acfa9408ad82eae39f47779fd7a6
Jason Davies <jason@jasondavies.com>
2011-04-06 20:49:15 +0100

    Replace percentages with width/height properties.
    Percentages were too tricky for adding margins.

--- src/layout/bullet.js
+++ src/layout/bullet.js
@@ -18,42 +18,66 @@ d3.layout.bullet = function() {
       measures = [],
       horizontal,
       maximum = null,
+      width = 500,
+      height = 30,
       rangeColor = d3.scale.linear(),
       measureColor = d3.scale.linear(),
-      scale = d3.scale.linear();
+      scale = d3.scale.linear(),
+      tickFormat = d3.format(',.0f');
 
   var x = function(d) {
-    return scale(d) + '%';
+    return scale(d);
   };
 
   var bullet = function() {
     ranges.sort(function(a, b) { return b - a });
     measures.sort(function(a, b) { return b - a });
-    this.selectAll('rect.range')
+    var chart = this.append('svg:g');
+    chart.selectAll('rect.range')
       .data(ranges)
         .enter().append('svg:rect')
         .attr('class', 'range')
         .attr('width', x)
-        .attr('height', '100%')
+        .attr('height', height)
         .attr('style', function(d, i) { return 'fill:' + rangeColor(i) });
-    this.selectAll('rect.measure')
+    chart.selectAll('rect.measure')
       .data(measures)
         .enter().append('svg:rect')
         .attr('class', 'measure')
         .attr('width', x)
-        .attr('height', '34%')
-        .attr('y', '33%')
+        .attr('height', height / 3)
+        .attr('y', height / 3)
         .attr('fill', function(d, i) { return measureColor(i) });
-    this.selectAll('line.marker')
+    chart.selectAll('line.marker')
       .data(markers)
         .enter().append('svg:line')
         .attr('class', 'marker')
         .attr('x1', x)
         .attr('x2', x)
-        .attr('y1', '15%')
-        .attr('y2', '85%')
+        .attr('y1', height/6)
+        .attr('y2', height * 5/6)
         .attr('stroke', '#000')
         .attr('stroke-width', '2px')
+    var ticks = scale.ticks(10);
+    this.selectAll('line.rule')
+      .data(ticks)
+        .enter().append('svg:line')
+        .attr('class', 'rule')
+        .attr('x1', x)
+        .attr('x2', x)
+        .attr('y1', height)
+        .attr('y2', height * 7/6)
+        .attr('stroke', '#666')
+        .attr('stroke-width', '.5px')
+    this.selectAll('text.tick')
+      .data(ticks)
+        .enter().append('svg:text')
+        .attr('class', 'tick')
+        .attr('x', x)
+        .attr('y', height * 7/6)
+        .attr('text-anchor', 'middle')
+        .attr('dy', '1em')
+        .text(tickFormat)
   }
 
   /** Cache layout state to optimize properties. */
@@ -64,7 +88,7 @@ d3.layout.bullet = function() {
     measureColor.domain([0, Math.max(1, measures.length - 1)])
         .range(["lightsteelblue", "steelblue"]);
     maximum = maximum || d3.max([].concat(ranges, markers, measures));
-    scale.domain([0, maximum]).range([0, 100]);
+    scale.domain([0, maximum]).range([0, width]);
   };
 
   // left, right, top, bottom
@@ -99,6 +123,27 @@ d3.layout.bullet = function() {
   bullet.maximum = function(x) {
     if (!arguments.length) return maximum;
     maximum = x;
+    buildCache();
+    return bullet;
+  };
+
+  bullet.width = function(x) {
+    if (!arguments.length) return width;
+    width = x;
+    buildCache();
+    return bullet;
+  };
+
+  bullet.height = function(x) {
+    if (!arguments.length) return height;
+    height = x;
+    buildCache();
+    return bullet;
+  };
+
+  bullet.tickFormat = function(x) {
+    if (!arguments.length) return tickFormat;
+    tickFormat = x;
     buildCache();
     return bullet;
   };
