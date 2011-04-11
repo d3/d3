c5c6614d9eea6d7255e4bc20c26fcc47e5a64b96
Mike Bostock <mbostock@gmail.com>
2011-04-11 12:44:17 -0700

    Simplify pack layout.
    The pack layout now sorts nodes by ascending value by default. The `radius`
    property is removed, as the radius is always computed from the node value. The
    `spacing` property is also removed; it doesn't seem particularly useful. The
    `radius` property is renamed to `r` to match `x` and `y`.
    
    I also optimized the implementation slightly, and namespaced temporary state
    under the prefix `_pack_`.

--- src/layout/pack.js
+++ src/layout/pack.js
@@ -1,24 +1,22 @@
 d3.layout.pack = function() {
   var hierarchy = d3.layout.hierarchy(),
-      separation = 1, // spacing
       size = [1, 1];
-      radius = d3_layout_packRadius;
 
   function pack(d, i) {
     var nodes = hierarchy.call(this, d, i),
         root = nodes[0];
 
-    d3_layout_packRadii(nodes, radius);
-
-    /* Recursively compute the layout. */
+    // Recursively compute the layout.
     root.x = 0;
     root.y = 0;
-    root.radius = d3_layout_packTree(root, separation);
+    d3_layout_packTree(root);
 
+    // Scale the layout to fit the requested size.
     var w = size[0],
         h = size[1],
-        k = 1 / Math.max(2 * root.radius / w, 2 * root.radius / h);
+        k = 1 / Math.max(2 * root.r / w, 2 * root.r / h);
     d3_layout_packTransform(root, w / 2, h / 2, k);
+
     return nodes;
   }
 
@@ -26,103 +24,95 @@ d3.layout.pack = function() {
   pack.children = d3.rebind(pack, hierarchy.children);
   pack.value = d3.rebind(pack, hierarchy.value);
 
-  pack.separation = function(x) {
-    if (!arguments.length) return separation;
-    separation = x;
-    return pack;
-  };
-
   pack.size = function(x) {
     if (!arguments.length) return size;
     size = x;
     return pack;
   };
 
-  pack.radius = function(x) {
-    if (!arguments.length) return radius;
-    radius = x;
-    return pack;
-  };
-
-  return pack;
+  return pack.sort(d3_layout_packSort);
 };
 
-function d3_layout_packRadius(d) {
-  return d.radius;
+function d3_layout_packSort(a, b) {
+  return a.value - b.value;
 }
 
-function d3_layout_packCircle(nodes, spacing) {
+function d3_layout_packInsert(a, b) {
+  var c = a._pack_next;
+  a._pack_next = b;
+  b._pack_prev = a;
+  b._pack_next = c;
+  c._pack_prev = b;
+}
+
+function d3_layout_packSplice(a, b) {
+  a._pack_next = b;
+  b._pack_prev = a;
+}
+
+function d3_layout_packIntersects(a, b) {
+  var dx = b.x - a.x,
+      dy = b.y - a.y,
+      dr = a.r + b.r;
+  return (dr * dr - dx * dx - dy * dy) > .001; // within epsilon
+}
+
+function d3_layout_packCircle(nodes) {
   var xMin = Infinity,
       xMax = -Infinity,
       yMin = Infinity,
       yMax = -Infinity,
+      n = nodes.length,
       a, b, c, j, k;
 
   function bound(n) {
-    xMin = Math.min(n.x - n.radius, xMin);
-    xMax = Math.max(n.x + n.radius, xMax);
-    yMin = Math.min(n.y - n.radius, yMin);
-    yMax = Math.max(n.y + n.radius, yMax);
+    xMin = Math.min(n.x - n.r, xMin);
+    xMax = Math.max(n.x + n.r, xMax);
+    yMin = Math.min(n.y - n.r, yMin);
+    yMax = Math.max(n.y + n.r, yMax);
   }
 
-  function insert(a, b) {
-    var c = a.n;
-    a.n = b;
-    b.p = a;
-    b.n = c;
-    c.p = b;
-  }
+  // Create node links.
+  nodes.forEach(d3_layout_packLink);
 
-  function splice(a, b) {
-    a.n = b;
-    b.p = a;
-  }
-
-  function intersects(a, b) {
-    var dx = b.x - a.x,
-        dy = b.y - a.y,
-        dr = a.radius + b.radius;
-    return (dr * dr - dx * dx - dy * dy) > .001; // within epsilon
-  }
-
-  /* Create first node. */
+  // Create first node.
   a = nodes[0];
-  a.x = -a.radius;
+  a.x = -a.r;
   a.y = 0;
   bound(a);
 
-  /* Create second node. */
-  if (nodes.length > 1) {
+  // Create second node.
+  if (n > 1) {
     b = nodes[1];
-    b.x = b.radius;
+    b.x = b.r;
     b.y = 0;
     bound(b);
 
-    /* Create third node and build chain. */
-    if (nodes.length > 2) {
+    // Create third node and build chain.
+    if (n > 2) {
       c = nodes[2];
       d3_layout_packPlace(a, b, c);
       bound(c);
-      insert(a, c);
-      a.p = c;
-      insert(c, b);
-      b = a.n;
+      d3_layout_packInsert(a, c);
+      a._pack_prev = c;
+      d3_layout_packInsert(c, b);
+      b = a._pack_next;
 
-      /* Now iterate through the rest. */
-      for (var i = 3; i < nodes.length; i++) {
+      // Now iterate through the rest.
+      for (var i = 3; i < n; i++) {
         d3_layout_packPlace(a, b, c = nodes[i]);
 
-        /* Search for the closest intersection. */
+        // Search for the closest intersection.
         var isect = 0, s1 = 1, s2 = 1;
-        for (j = b.n; j != b; j = j.n, s1++) {
-          if (intersects(j, c)) {
+        for (j = b._pack_next; j != b; j = j._pack_next, s1++) {
+          if (d3_layout_packIntersects(j, c)) {
             isect = 1;
             break;
           }
         }
         if (isect == 1) {
-          for (k = a.p; k != j.p; k = k.p, s2++) {
-            if (intersects(k, c)) {
+          for (k = a._pack_prev; k != j._pack_prev; k = k._pack_prev, s2++) {
+            if (d3_layout_packIntersects(k, c)) {
               if (s2 < s1) {
                 isect = -1;
                 j = k;
@@ -132,17 +122,17 @@ function d3_layout_packCircle(nodes, spacing) {
           }
         }
 
-        /* Update node chain. */
+        // Update node chain.
         if (isect == 0) {
-          insert(a, c);
+          d3_layout_packInsert(a, c);
           b = c;
           bound(c);
         } else if (isect > 0) {
-          splice(a, j);
+          d3_layout_packSplice(a, j);
           b = j;
           i--;
-        } else if (isect < 0) {
-          splice(j, b);
+        } else { // isect < 0
+          d3_layout_packSplice(j, b);
           a = j;
           i--;
         }
@@ -150,51 +140,56 @@ function d3_layout_packCircle(nodes, spacing) {
     }
   }
 
-  /* Re-center the circles and return the encompassing radius. */
+  // Re-center the circles and return the encompassing radius.
   var cx = (xMin + xMax) / 2,
       cy = (yMin + yMax) / 2,
       cr = 0;
-  for (var i = 0; i < nodes.length; i++) {
-    var n = nodes[i];
-    n.x -= cx;
-    n.y -= cy;
-    cr = Math.max(cr, n.radius + Math.sqrt(n.x * n.x + n.y * n.y));
+  for (var i = 0; i < n; i++) {
+    var node = nodes[i];
+    node.x -= cx;
+    node.y -= cy;
+    cr = Math.max(cr, node.r + Math.sqrt(node.x * node.x + node.y * node.y));
   }
-  return cr + spacing;
+
+  // Remove node links.
+  nodes.forEach(d3_layout_packUnlink);
+
+  return cr;
 }
 
-function d3_layout_packTree(n, spacing) {
-  var nodes = [],
-      children = n.children;
-  if (children) {
-    for (var i = 0, l = children.length; i < l; i++) {
-      var c = children[i];
-      if (c.children) c.radius = d3_layout_packTree(c, spacing);
-      c.n = c.p = c;
-      nodes.push(c);
-    }
-  }
-  return d3_layout_packCircle(nodes, spacing);
+function d3_layout_packLink(node) {
+  node._pack_next = node._pack_prev = node;
 }
 
-function d3_layout_packTransform(n, x, y, k) {
-  var children = n.children;
+function d3_layout_packUnlink(node) {
+  delete node._pack_next;
+  delete node._pack_prev;
+}
+
+function d3_layout_packTree(node) {
+  var children = node.children;
   if (children) {
-    for (var i = 0, l = children.length; i < l; i++) {
-      var c = children[i];
-      c.x += n.x;
-      c.y += n.y;
-      d3_layout_packTransform(c, x, y, k);
-    }
+    children.forEach(d3_layout_packTree);
+    node.r = d3_layout_packCircle(children);
+  } else {
+    node.r = Math.sqrt(node.value);
   }
-  n.x = x + k * n.x;
-  n.y = y + k * n.y;
-  n.radius *= k;
+}
+
+function d3_layout_packTransform(node, x, y, k) {
+  var children = node.children;
+  node.x = (x += k * node.x);
+  node.y = (y += k * node.y);
+  node.r *= k;
+  if (children) {
+    var i = -1, n = children.length;
+    while (++i < n) d3_layout_packTransform(children[i], x, y, k);
+  }
 }
 
 function d3_layout_packPlace(a, b, c) {
-  var da = b.radius + c.radius,
-      db = a.radius + c.radius,
+  var da = b.r + c.r,
+      db = a.r + c.r,
       dx = b.x - a.x,
       dy = b.y - a.y,
       dc = Math.sqrt(dx * dx + dy * dy),
@@ -206,17 +201,4 @@ function d3_layout_packPlace(a, b, c) {
   dy /= dc;
   c.x = a.x + x * dx + h * dy;
   c.y = a.y + x * dy - h * dx;
-}
-
-// Computes the radii of the leaf nodes.
-// TODO (from Protovis version):
-// Is it possible for spacing to operate in pixel space?
-// Right now it appears to be multiples of the smallest radius.
-function d3_layout_packRadii(nodes, radius) {
-  for (var i = 0, n = nodes.length; i < n; i++) {
-    var c = nodes[i];
-    if (!c.children) {
-      c.radius = radius.call(this, c, i);
-    }
-  }
 }
