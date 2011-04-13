2e8cd200df552ac6182889deded7f780007e7403
Jason Davies <jason@jasondavies.com>
2011-04-13 18:53:52 +0100

    Add boxplot chart.

---
+++ examples/boxplot/boxplot.js
@@ -0,0 +1,45 @@
+var w = 120,
+    h = 500,
+    m = [10, 40, 20, 40], // top right bottom left
+    max = 50;
+
+var chart = d3.chart.boxplot()
+    .tickFormat(d3.format(".1"))
+    .domain([0, max])
+    .width(w - m[1] - m[3])
+    .height(h - m[0] - m[2]);
+
+d3.json("boxplot.json", function(data) {
+
+  var vis = d3.select("#chart").selectAll("svg")
+      .data(data)
+    .enter().append("svg:svg")
+      .attr("class", "boxplot")
+      .attr("width", w)
+      .attr("height", h)
+    .append("svg:g")
+      .attr("transform", "translate(" + m[3] + "," + m[0] + ")")
+      .call(chart);
+  
+  var title = vis.append("svg:g")
+      .attr("text-anchor", "end")
+      .attr("transform", "translate(" + (w - m[0] - m[2]) / 2 + ")");
+
+  chart.duration(1000);
+  window.transition = function() {
+    vis.map(randomize).call(chart);
+  };
+});
+
+function randomize(d) {
+  if (!d.randomizer) d.randomizer = randomizer(d);
+  d = d.map(d.randomizer);
+  return d;
+}
+
+function randomizer(d) {
+  var k = d3.max(d) * .2;
+  return function(d) {
+    return Math.min(Math.max(0, d + k * (Math.random() - .5)), max);
+  };
+}
---
+++ src/chart/boxplot.js
@@ -0,0 +1,156 @@
+// Inspired by http://informationandvisualization.de/files/boxplottest.html
+d3.chart.boxplot = function() {
+  var width = 1,
+      height = 1,
+      duration = 0,
+      domain = null,
+      tickFormat = null;
+
+  // For each small multiple…
+  function boxplot(g) {
+    g.each(function(d, i) {
+      d = d.slice();
+      d.sort(function(a, b) { return a - b; });
+      var len = d.length,
+          min = d[0],
+          q1 = d[Math.floor(.25 * len)],
+          median = d[Math.floor(.5 * len)],
+          q3 = d[Math.floor(.75 * len)],
+          max = d[len-1],
+          g = d3.select(this);
+
+      // Compute the new x-scale.
+      var x1 = d3.scale.linear()
+          .domain(domain || [min, max])
+          .range([height, 0]);
+
+      // Retrieve the old x-scale, if this is an update.
+      var x0 = this.__chart__ || d3.scale.linear()
+          .domain([0, Infinity])
+          .range(x1.range());
+
+      // Stash the new scale.
+      this.__chart__ = x1;
+
+      // Update center line.
+      var center = g.selectAll("line.center")
+          .data([[min, max]]);
+
+      center.enter().append("svg:line")
+          .attr("class", "center")
+          .attr("x1", width / 2)
+          .attr("y1", function(d) { return x0(d[0]); })
+          .attr("x2", width / 2)
+          .attr("y2", function(d) { return x0(d[1]); })
+        .transition()
+          .duration(duration)
+          .attr("y1", function(d) { return x1(d[0]); })
+          .attr("y2", function(d) { return x1(d[1]); });
+
+      center.transition()
+          .duration(duration)
+          .attr("y1", function(d) { return x1(d[0]); })
+          .attr("y2", function(d) { return x1(d[1]); });
+
+      center.exit().remove();
+
+      // Update boxes.
+      var box = g.selectAll("rect.box")
+          .data([[q1, q3]]);
+
+      box.enter().append("svg:rect")
+          .attr("class", "box")
+          .attr("x", 0)
+          .attr("y", function(d) { return x0(d[1]); })
+          .attr("width", width)
+          .attr("height", function(d) { return x0(d[0]) - x0(d[1]); })
+        .transition()
+          .duration(duration)
+          .attr("y", function(d) { return x1(d[1]); })
+          .attr("height", function(d) { return x1(d[0]) - x1(d[1]); });
+
+      box.transition()
+          .duration(duration)
+          .attr("y", function(d) { return x1(d[1]); })
+          .attr("height", function(d) { return x1(d[0]) - x1(d[1]); });
+
+      box.exit().remove();
+
+      // Update markers.
+      var marker = g.selectAll("line.marker")
+          .data([min, median, max]);
+
+      marker.enter().append("svg:line")
+          .attr("class", "marker")
+          .attr("x1", 0)
+          .attr("y1", x0)
+          .attr("x2", width)
+          .attr("y2", x0)
+        .transition()
+          .duration(duration)
+          .attr("y1", x1)
+          .attr("y2", x1);
+
+      marker.transition()
+          .duration(duration)
+          .attr("y1", x1)
+          .attr("y2", x1);
+
+      marker.exit().remove();
+
+      // Compute the tick format.
+      var format = tickFormat || x1.tickFormat(8);
+
+      var tick = g.selectAll("text")
+          .data([min, q1, median, q3, max]);
+
+      tick.enter().append("svg:text")
+          .attr("dy", ".3em")
+          .attr("dx", function(d, i) { return i&1 ? 8 : -8 })
+          .attr("x", function(d, i) { return i&1 ? width : 0 })
+          .attr("y", x0)
+          .attr("text-anchor", function(d, i) { return i&1 ? "start" : "end"; })
+          .text(format)
+        .transition()
+          .duration(duration)
+          .attr("y", x1);
+
+      tick.text(format)
+        .transition()
+          .duration(duration)
+          .attr("y", x1);
+    });
+  }
+
+  boxplot.width = function(x) {
+    if (!arguments.length) return width;
+    width = x;
+    return boxplot;
+  };
+
+  boxplot.height = function(x) {
+    if (!arguments.length) return height;
+    height = x;
+    return boxplot;
+  };
+
+  boxplot.tickFormat = function(x) {
+    if (!arguments.length) return tickFormat;
+    tickFormat = x;
+    return boxplot;
+  };
+
+  boxplot.duration = function(x) {
+    if (!arguments.length) return duration;
+    duration = x;
+    return boxplot;
+  };
+
+  boxplot.domain = function(x) {
+    if (!arguments.length) return domain;
+    domain = x;
+    return boxplot;
+  };
+
+  return boxplot;
+};
