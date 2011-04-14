c29d4dc1a7b47614fc3b65ef091cc2d1ac395b2e
Mike Bostock <mbostock@gmail.com>
2011-04-13 17:31:16 -0700

    Better box chart transitions.
    Preserving object constancy across transitions is tricky! For example, what
    happens if we remove the whiskers in a transition? How do we join outliers? This
    commit makes a few assumptions explicit:
    
    1. The `quartiles` function must return exactly three elements. This property
    must be specified as a function.
    
    2. The `whiskers` function must return exactly 2 elements, or null if no
    whiskers are to be displayed. This property must be specified as a function.
    
    3. The `domain` function must return exactly 2 elements, or null if the default
    domain should be used. This property can be specified either as a constant or as
    a function.
    
    We could generalize this chart to support more than two whiskers, but it doesn't
    seem urgent, and it would complicate the transition if the number of whiskers
    changes. In a related change, the `whiskers` function does not receive a third
    argument containing the quartiles; instead, this is made available by the
    `quartiles` property on the values array (the first argument).
    
    The outliers are joined using the `Number` key function. The outlier data is now
    stored as indices; this allows reasonable object constancy across transitions
    with outliers. Similarly, the tick labels for the quartiles are whiskers are now
    separated, such that the whisker labels can be added or removed without spurious
    transition.

--- examples/box/box.js
+++ examples/box/box.js
@@ -1,20 +1,11 @@
 var w = 120,
     h = 500,
     m = [10, 50, 20, 50], // top right bottom left
-    min = Number.MAX_VALUE,
-    max = Number.MIN_VALUE;
+    min = Infinity,
+    max = -Infinity;
 
 var chart = d3.chart.box()
-    .whiskers(function(d, i, quartiles) {
-      var q1 = quartiles[0],
-          q3 = quartiles[2],
-          iqr = q3 - q1,
-          w0 = -1,
-          w1 = d.length;
-      while (d[++w0] < q1 - iqr * 1.5);
-      while (d[--w1] > q3 + iqr * 1.5);
-      return [w0, w1];
-    })
+    .whiskers(iqr(1.5))
     .width(w - m[1] - m[3])
     .height(h - m[0] - m[2]);
 
@@ -43,7 +34,7 @@ d3.csv("morley.csv", function(csv) {
     .append("svg:g")
       .attr("transform", "translate(" + m[3] + "," + m[0] + ")")
       .call(chart);
-  
+
   chart.duration(1000);
   window.transition = function() {
     vis.map(randomize).call(chart);
@@ -52,13 +43,26 @@ d3.csv("morley.csv", function(csv) {
 
 function randomize(d) {
   if (!d.randomizer) d.randomizer = randomizer(d);
-  d = d.map(d.randomizer);
-  return d;
+  return d.map(d.randomizer);
 }
 
 function randomizer(d) {
-  var k = d3.max(d) * .2;
+  var k = d3.max(d) * .02;
   return function(d) {
-    return Math.min(Math.max(min, d + k * (Math.random() - .5)), max);
+    return Math.max(min, Math.min(max, d + k * (Math.random() - .5)));
+  };
+}
+
+// Returns a function to compute the interquartile range.
+function iqr(k) {
+  return function(d, i) {
+    var q1 = d.quartiles[0],
+        q3 = d.quartiles[2],
+        iqr = (q3 - q1) * k,
+        i = -1,
+        j = d.length;
+    while (d[++i] < q1 - iqr);
+    while (d[--j] > q3 + iqr);
+    return [i, j];
   };
 }
--- src/chart/box.js
+++ src/chart/box.js
@@ -13,21 +13,27 @@ d3.chart.box = function() {
   function box(g) {
     g.each(function(d, i) {
       d = d.map(value).sort(d3.ascending);
-      var len = d.length,
+      var g = d3.select(this),
+          n = d.length,
           min = d[0],
-          max = d[len-1],
-          quartileData = quartiles(d),
-          whiskerIndices = whiskers.call(this, d, i, quartileData),
-          whiskerData = whiskerIndices.map(function(i) { return d[i]; }),
-          firstWhisker = whiskerIndices[0],
-          lastWhisker = whiskerIndices[whiskerIndices.length - 1],
-          whiskerRange = lastWhisker - firstWhisker,
-          outliers = d.slice(0, firstWhisker).concat(d.slice(lastWhisker + 1, len)),
-          g = d3.select(this);
+          max = d[n - 1];
+
+      // Compute quartiles. Must return exactly 3 elements.
+      var quartileData = d.quartiles = quartiles(d);
+
+      // Compute whiskers. Must return exactly 2 elements, or null.
+      var whiskerIndices = whiskers && whiskers.call(this, d, i),
+          whiskerData = whiskerIndices && whiskerIndices.map(function(i) { return d[i]; });
+
+      // Compute outliers. If no whiskers are specified, all data are "outliers".
+      // We compute the outliers as indices, so that we can join across transitions!
+      var outlierIndices = whiskerIndices
+          ? d3.range(0, whiskerIndices[0]).concat(d3.range(whiskerIndices[1] + 1, n))
+          : d3.range(n);
 
       // Compute the new x-scale.
       var x1 = d3.scale.linear()
-          .domain(domain ? domain.call(this, d, i) : [min, max])
+          .domain(domain && domain.call(this, d, i) || [min, max])
           .range([height, 0]);
 
       // Retrieve the old x-scale, if this is an update.
@@ -38,9 +44,14 @@ d3.chart.box = function() {
       // Stash the new scale.
       this.__chart__ = x1;
 
-      // Update center line.
+      // Note: the box, median, and box tick elements are fixed in number,
+      // so we only have to handle enter and update. In contrast, the outliers
+      // and other elements are variable, so we need to exit them! Variable
+      // elements also fade in and out.
+
+      // Update center line: the vertical line spanning the whiskers.
       var center = g.selectAll("line.center")
-          .data([[whiskerData[0], whiskerData[whiskerData.length - 1]]]);
+          .data(whiskerData ? [whiskerData] : []);
 
       center.enter().append("svg:line")
           .attr("class", "center")
@@ -48,41 +59,47 @@ d3.chart.box = function() {
           .attr("y1", function(d) { return x0(d[0]); })
           .attr("x2", width / 2)
           .attr("y2", function(d) { return x0(d[1]); })
+          .style("opacity", 1e-6)
         .transition()
           .duration(duration)
+          .style("opacity", 1)
           .attr("y1", function(d) { return x1(d[0]); })
           .attr("y2", function(d) { return x1(d[1]); });
 
       center.transition()
           .duration(duration)
+          .style("opacity", 1)
           .attr("y1", function(d) { return x1(d[0]); })
           .attr("y2", function(d) { return x1(d[1]); });
 
-      center.exit().remove();
+      center.exit().transition()
+          .duration(duration)
+          .style("opacity", 1e-6)
+          .attr("y1", function(d) { return x1(d[0]); })
+          .attr("y2", function(d) { return x1(d[1]); })
+          .remove();
 
-      // Update boxes.
+      // Update innerquartile box.
       var box = g.selectAll("rect.box")
-          .data([[quartileData[0], quartileData[2]]]);
+          .data([quartileData]);
 
       box.enter().append("svg:rect")
           .attr("class", "box")
           .attr("x", 0)
-          .attr("y", function(d) { return x0(d[1]); })
+          .attr("y", function(d) { return x0(d[2]); })
           .attr("width", width)
-          .attr("height", function(d) { return x0(d[0]) - x0(d[1]); })
+          .attr("height", function(d) { return x0(d[0]) - x0(d[2]); })
         .transition()
           .duration(duration)
-          .attr("y", function(d) { return x1(d[1]); })
-          .attr("height", function(d) { return x1(d[0]) - x1(d[1]); });
+          .attr("y", function(d) { return x1(d[2]); })
+          .attr("height", function(d) { return x1(d[0]) - x1(d[2]); });
 
       box.transition()
           .duration(duration)
-          .attr("y", function(d) { return x1(d[1]); })
-          .attr("height", function(d) { return x1(d[0]) - x1(d[1]); });
+          .attr("y", function(d) { return x1(d[2]); })
+          .attr("height", function(d) { return x1(d[0]) - x1(d[2]); });
 
-      box.exit().remove();
-
-      // Update median line
+      // Update median line.
       var medianLine = g.selectAll("line.median")
           .data([quartileData[1]]);
 
@@ -102,11 +119,9 @@ d3.chart.box = function() {
           .attr("y1", x1)
           .attr("y2", x1);
 
-      medianLine.exit().remove();
-
       // Update whiskers.
       var whisker = g.selectAll("line.whisker")
-          .data(whiskerData);
+          .data(whiskerData || []);
 
       whisker.enter().append("svg:line")
           .attr("class", "whisker")
@@ -114,59 +129,106 @@ d3.chart.box = function() {
           .attr("y1", x0)
           .attr("x2", width)
           .attr("y2", x0)
+          .style("opacity", 1e-6)
         .transition()
           .duration(duration)
           .attr("y1", x1)
-          .attr("y2", x1);
+          .attr("y2", x1)
+          .style("opacity", 1);
 
       whisker.transition()
           .duration(duration)
           .attr("y1", x1)
-          .attr("y2", x1);
+          .attr("y2", x1)
+          .style("opacity", 1);
 
-      whisker.exit().remove();
+      whisker.exit().transition()
+          .duration(duration)
+          .attr("y1", x1)
+          .attr("y2", x1)
+          .style("opacity", 1e-6)
+          .remove();
 
       // Update outliers.
       var outlier = g.selectAll("circle.outlier")
-          .data(outliers);
+          .data(outlierIndices, Number);
 
       outlier.enter().append("svg:circle")
           .attr("class", "outlier")
           .attr("r", 5)
           .attr("cx", width / 2)
-          .attr("cy", x0)
+          .attr("cy", function(i) { return x0(d[i]); })
+          .style("opacity", 1e-6)
         .transition()
           .duration(duration)
-          .attr("cy", x1);
+          .attr("cy", function(i) { return x1(d[i]); })
+          .style("opacity", 1);
 
       outlier.transition()
           .duration(duration)
-          .attr("cy", x1);
+          .attr("cy", function(i) { return x1(d[i]); })
+          .style("opacity", 1);
 
-      outlier.exit().remove();
+      outlier.exit().transition()
+          .duration(duration)
+          .attr("cy", function(i) { return x1(d[i]); })
+          .style("opacity", 1e-6)
+          .remove();
 
       // Compute the tick format.
       var format = tickFormat || x1.tickFormat(8);
 
-      // Update ticks.
-      var tick = g.selectAll("text")
-          .data(quartileData.concat(whiskerData));
+      // Update box ticks.
+      var boxTick = g.selectAll("text.box")
+          .data(quartileData);
 
-      tick.enter().append("svg:text")
+      boxTick.enter().append("svg:text")
+          .attr("class", "box")
           .attr("dy", ".3em")
-          .attr("dx", function(d, i) { return i&1 ? 8 : -8 })
-          .attr("x", function(d, i) { return i&1 ? width : 0 })
+          .attr("dx", function(d, i) { return i & 1 ? 6 : -6 })
+          .attr("x", function(d, i) { return i & 1 ? width : 0 })
           .attr("y", x0)
-          .attr("text-anchor", function(d, i) { return i&1 ? "start" : "end"; })
+          .attr("text-anchor", function(d, i) { return i & 1 ? "start" : "end"; })
           .text(format)
         .transition()
           .duration(duration)
           .attr("y", x1);
 
-      tick.text(format)
+      boxTick.text(format)
         .transition()
           .duration(duration)
           .attr("y", x1);
+
+      // Update whisker ticks. These are handled separately from the box
+      // ticks because they may or may not exist, and we want don't want
+      // to join box ticks pre-transition with whisker ticks post-.
+      var whiskerTick = g.selectAll("text.whisker")
+          .data(whiskerData || []);
+
+      whiskerTick.enter().append("svg:text")
+          .attr("class", "whisker")
+          .attr("dy", ".3em")
+          .attr("dx", 6)
+          .attr("x", width)
+          .attr("y", x0)
+          .text(format)
+          .style("opacity", 1e-6)
+        .transition()
+          .duration(duration)
+          .attr("y", x1)
+          .style("opacity", 1);
+
+      whiskerTick.text(format)
+        .transition()
+          .duration(duration)
+          .attr("y", x1)
+          .style("opacity", 1);
+
+      whiskerTick.exit().transition()
+          .duration(duration)
+          .attr("y", x1)
+          .style("opacity", 1e-6)
+          .remove();
     });
   }
 
@@ -196,7 +258,7 @@ d3.chart.box = function() {
 
   box.domain = function(x) {
     if (!arguments.length) return domain;
-    domain = d3.functor(x);
+    domain = x == null ? x : d3.functor(x);
     return box;
   };
 
@@ -222,13 +284,13 @@ d3.chart.box = function() {
 };
 
 function d3_chart_boxWhiskers(d) {
-  return [0, d.length-1];
+  return [0, d.length - 1];
 }
 
 function d3_chart_boxQuartiles(d) {
-  var len = d.length;
+  var n = d.length;
   return [.25, .5, .75].map(function(q) {
-    q *= len;
+    q *= n;
     return ~~q === q ? (d[q] + d[q + 1]) / 2 : d[Math.round(q)];
   });
 }
