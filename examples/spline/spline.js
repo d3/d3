de1e679cfc42fd6608228600f194313155982f09
Jason Davies <jason@jasondavies.com>
2011-04-27 18:30:47 +0100

    Move spline editor to standalone directory.
    I've dropped path transitions on Mike's recommendation as D3 doesn't have smart
    path interpolation at the moment so they don't work too well.
    
    I've also fixed some issues with data joins.

--- examples/spline/spline.js
+++ examples/spline/spline.js
@@ -1,8 +1,6 @@
 var w = 800,
     h = 600,
-    p = 20,
-    index = 3,
-    duration = 1000;
+    p = 20;
 
 var points = d3.range(1, 5).map(function(i) {
   return [
@@ -11,8 +9,9 @@ var points = d3.range(1, 5).map(function(i) {
   ];
 });
 
+var selected = points[0];
+
 var wrapper = d3.select("#chart")
-    .data([points])
   .append("svg:svg")
     .attr("width", w + p * 2)
     .attr("height", h + p * 2);
@@ -20,43 +19,48 @@ var wrapper = d3.select("#chart")
 var vis = wrapper.append("svg:g")
     .attr("transform", "translate(" + p + "," + p + ")");
 
+// Use <rect> to ensure all "mousemove" events are captured.
+vis.append("svg:rect")
+    .attr("fill", "#fff")
+    .attr("width", w)
+    .attr("height", h);
+
 var line = d3.svg.line()
     .x(function(d) { return d[0]; })
     .y(function(d) { return d[1]; });
 
-vis.append("svg:path")
-    .attr("class", "line")
-
 function update() {
-  vis.select("path")
-    .transition().duration(duration)
+  var path = vis.selectAll("path")
+    .data(points.length ? [points] : []);
+
+  path.enter().append("svg:path")
+      .attr("class", "line")
       .attr("d", line);
+
+  path.attr("d", line);
+
+  path.exit().remove();
 
   var circle = vis.selectAll("circle")
       .data(points, function(d) { return d; });
 
   circle.enter().append("svg:circle")
-      .attr("class", function(d, i) { return i === index ? "selected" : null; })
+      .attr("class", function(d) { return d === selected ? "selected" : null; })
       .attr("cx", function(d) { return d[0]; })
       .attr("cy", function(d) { return d[1]; })
       .attr("r", 6.5)
-      .attr("opacity", 1)
-      .on("mousedown", function(d, i) {
-        vis.selectAll("circle").filter(function(d, j) {
-          return j === index;
-        }).attr("class", null);
-        var circle = d3.select(this)
-            .attr("class", "selected");
-        index = i;
+      .on("mousedown", function(d) {
+        selected = d;
+        update();
+        var circle = d3.select(this);
         wrapper.on("mousemove", function() {
           var m = d3.svg.mouse(this);
-          m[0] -= p;
-          m[1] -= p;
-          points[index] = m;
+          d[0] = m[0] - p;
+          d[1] = m[1] - p;
           circle
-              .attr("cx", m[0])
-              .attr("cy", m[1]);
-          vis.select("path")
+              .attr("cx", d[0])
+              .attr("cy", d[1]);
+          vis.selectAll("path")
               .attr("d", line);
         });
         d3.event.preventDefault();
@@ -64,31 +68,32 @@ function update() {
       });
 
   circle
-      .attr("class", function(d, i) { return i === index ? "selected" : null; });
+      .attr("class", function(d) { return d === selected ? "selected" : null; });
 
-  circle.exit().transition()
-      .duration(duration / 4)
-      .attr("opacity", 1e-6)
-      .remove();
+  circle.exit().remove();
 }
 
 update();
 
-wrapper.on("mousedown", function() {
-  var m = d3.svg.mouse(this);
-  index = points.push([m[0] - p, m[1] - p]) - 1;
+wrapper.on("mousedown", function(d) {
+  var m = d3.svg.mouse(this),
+      index = points.push([m[0] - p, m[1] - p]) - 1;
+  selected = points[index];
   update();
 });
 
 wrapper.on("mouseup", function() {
   wrapper.on("mousemove", null);
+  update();
 });
 
 d3.select(window).on("keydown", function() {
   var e = d3.event;
   // code 8 is backspace, code 46 is delete
-  if ((e.keyCode == 8 || e.keyCode == 46) && (index >= 0)) {
-    points.splice(index--, 1);
+  if ((e.keyCode == 8 || e.keyCode == 46) && selected !== null) {
+    var index = points.indexOf(selected);
+    points.splice(index, 1);
+    selected = points.length ? points[index > 0 ? index - 1 : 0] : null;
     update();
     e.preventDefault();
   }
@@ -115,7 +120,5 @@ select.selectAll("option")
 
 select.on("change", function() {
   line.interpolate(this.value);
-  vis.select("path")
-    .transition().duration(1000)
-      .attr("d", line);
+  update();
 });
