05442751f9db86286012d779e5aad4f6250b44d8
Jason Davies <jason@jasondavies.com>
2011-05-03 13:38:56 +0100

    Use "Object.keys" if available.
    Also added "hasOwnProperty" check in the fallback to avoid enumerating
    properties from the prototype chain.  This is the approach recommended in
    <https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Object/keys>
    and elsewhere.
    
    See also: #96.

--- src/core/keys.js
+++ src/core/keys.js
@@ -1,5 +1,6 @@
-d3.keys = function(map) {
+d3.keys = Object.keys || function(map) {
   var keys = [];
-  for (var key in map) keys.push(key);
+  for (var key in map)
+    if (Object.prototype.hasOwnProperty.call(map, key)) keys.push(key);
   return keys;
 };
