08a5aa383fbaeef2e3d1587b746bb44fd9ca0ce3
Mike Bostock <mbostock@gmail.com>
2011-05-21 16:38:29 -0700

    Histogram improvements.
    Rename `ticks` to `bins` to match numpy, matplotlib, and Protovis. Also allow
    the bins to be specified simply as a count, in which case the range is divided
    uniformly into the specified number of bins.
    
    The histogram layout now also takes a `range` property, which specifies the
    minimum and maximum value of the histogram rather than implicitly computing it
    from the values, which is nice if you expect values to fall in a specific range,
    such as [0,1].

--- src/layout/histogram.js
+++ src/layout/histogram.js
@@ -1,56 +1,99 @@
 d3.layout.histogram = function() {
   var frequency = true,
-      value = Number,
-      ticksFunction = d3_layout_histogramTicks;
+      valuer = Number,
+      ranger = d3_layout_histogramRange,
+      binner = d3_layout_histogramBins10;
 
   function histogram(data, i) {
-    var x = data.map(value),
-        bins = [],
+    var bins = [],
+        values = data.map(valuer, this),
+        range = ranger.call(this, values, i),
+        thresholds = binner.call(this, range, i),
         bin,
-        ticks = ticksFunction.call(this, data, i),
         i = -1,
-        n = x.length,
-        m = ticks.length - 1,
-        k = frequency ? 1 / n : 1;
+        n = values.length,
+        m = thresholds.length - 1,
+        k = frequency ? 1 / n : 1,
+        x;
 
     // Initialize the bins.
     while (++i < m) {
       bin = bins[i] = [];
-      bin.dx = ticks[i + 1] - (bin.x = ticks[i]);
+      bin.dx = thresholds[i + 1] - (bin.x = thresholds[i]);
       bin.y = 0;
     }
 
-    // Count the number of samples per bin.
+    // Fill the bins, ignoring values outside the range.
     i = -1; while(++i < n) {
-      bin = bins[d3.bisect(ticks, x[i], 0, m - 1)];
-      bin.y += k;
-      bin.push(data[i]);
+      x = values[i];
+      if ((x >= range[0]) && (x <= range[1])) {
+        bin = bins[d3.bisect(thresholds, x, 1, m) - 1];
+        bin.y += k;
+        bin.push(data[i]);
+      }
     }
 
     return bins;
   }
 
+  // Specifies how to extract a value from the associated data. The default
+  // value function is `Number`, which is equivalent to the identity function.
+  histogram.value = function(x) {
+    if (!arguments.length) return valuer;
+    valuer = x;
+    return histogram;
+  };
+
+  // Specifies the range of the histogram. Values outside the specified range
+  // will be ignored. The argument `x` may be specified either as a two-element
+  // array representing the minimum and maximum value of the range, or as a
+  // function that returns the range given the array of values and the current
+  // index `i`. The default range is the extent (minimum and maximum) of the
+  // values.
+  histogram.range = function(x) {
+    if (!arguments.length) return ranger;
+    ranger = d3.functor(x);
+    return histogram;
+  };
+
+  // Specifies how to bin values in the histogram. The argument `x` may be
+  // specified as a number, in which case the range of values will be split
+  // uniformly into the given number of bins. Or, `x` may be an array of
+  // threshold values, defining the bins; the specified array must contain the
+  // rightmost (upper) value, thus specifying n + 1 values for n bins. Or, `x`
+  // may be a function which is evaluated, being passed the array of values and
+  // the current index `i`, returning an array of thresholds. The default bin
+  // function will divide the values into ten uniform bins.
+  histogram.bins = function(x) {
+    if (!arguments.length) return binner;
+    binner = typeof x === "number" ? d3_layout_histogramBins(x) : d3.functor(x);
+    return histogram;
+  };
+
+  // Specifies whether the histogram's `y` value is a count (frequency) or a
+  // probability (density). The default value is true.
   histogram.frequency = function(x) {
     if (!arguments.length) return frequency;
     frequency = !!x;
     return histogram;
   };
 
-  histogram.ticks = function(x) {
-    if (!arguments.length) return ticksFunction;
-    ticksFunction = d3.functor(x);
-    return histogram;
-  };
-
-  histogram.value = function(x) {
-    if (!arguments.length) return value;
-    value = x;
-    return histogram;
-  };
-
   return histogram;
 };
 
-function d3_layout_histogramTicks(x) {
-  return d3.scale.linear().domain(x).ticks(10);
+var d3_layout_histogramBins10 = d3_layout_histogramBins(10);
+
+function d3_layout_histogramBins(n) {
+  return function(range) {
+    var x = -1,
+        b = +range[0],
+        m = (range[1] - b) / n,
+        f = [];
+    while (++x <= n) f[x] = m * x + b;
+    return f;
+  };
+}
+
+function d3_layout_histogramRange(values) {
+  return [d3.min(values), d3.max(values)];
 }
