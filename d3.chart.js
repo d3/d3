06a3ef3816dc23b3d3b59eb48314277e1936e801
Jason Davies <jason@jasondavies.com>
2011-06-04 17:11:49 +0100

    Add transitions to Mekko chart.

--- d3.chart.js
+++ d3.chart.js
@@ -739,19 +739,41 @@ function d3_chart_horizonY(d) {
 d3.chart.mekko = function() {
   var width = 1,
       height = 1,
-      x = d3.scale.linear().range([0, width]),
-      y = d3.scale.linear().range([0, height]),
       z = d3.scale.category10(),
       n = d3.format(",d"),
       p = d3.format("%"),
       value = d3_chartMekkoValue,
       categoryX = d3_chartMekkoCategoryX,
-      categoryY = d3_chartMekkoCategoryY;
+      categoryY = d3_chartMekkoCategoryY,
+      duration = 0;
 
   // For each small multiple…
   function mekko(g) {
     g.each(function(data, i) {
-      var offset = 0;
+      var offset = 0,
+          x0, // old x-scale
+          y0; // old y-scale
+
+      // Compute the new x-scale.
+      var x1 = d3.scale.linear()
+          .range([0, width]);
+
+      // Compute the new y-scale.
+      var y1 = d3.scale.linear()
+          .range([0, height]);
+
+      // Retrieve the old scales, if this is an update.
+      if (this.__chart__) {
+        x0 = this.__chart__.x;
+        y0 = this.__chart__.y;
+      } else {
+        x0 = d3.scale.linear().domain([0, Infinity]).range(x1.range());
+        y0 = d3.scale.linear().domain([0, Infinity]).range(y1.range());
+      }
+
+      // Stash the new scales.
+      this.__chart__ = {x: x1, y: y1};
+
 
       // Nest values by x-category. We assume each (x-category, y-category) is
       // unique.
@@ -770,73 +792,162 @@ d3.chart.mekko = function() {
         }, 0));
       }, 0);
 
+      var tx = function(d) { return "translate(" + x1(d) + "," + y1(1) + ")"; },
+          ty = function(d) { return "translate(0," + y1(1 - d) + ")"; };
+
       // Add x-axis ticks.
       var xtick = svg.selectAll("g.x")
-          .data(x.ticks(10))
-        .enter().append("svg:g")
-          .attr("class", "x")
-          .attr("transform", function(d) { return "translate(" + x(d) + "," + y(1) + ")"; });
+          .data(x1.ticks(10));
 
-      xtick.append("svg:line")
+      var xtickEnter = xtick.enter().append("svg:g")
+          .attr("class", "x")
+          .attr("transform", function(d) { return "translate(" + x0(d) + "," + y0(1) + ")"; })
+          .style("opacity", 1e-6);
+
+      xtickEnter.append("svg:line")
           .attr("y2", 6)
           .style("stroke", "#000");
 
-      xtick.append("svg:text")
+      xtickEnter.append("svg:text")
           .attr("y", 8)
           .attr("text-anchor", "middle")
           .attr("dy", ".71em")
           .text(p);
 
+      // Transition the entering ticks to the new scale, x1.
+      xtickEnter.transition()
+          .duration(duration)
+          .attr("transform", tx)
+          .style("opacity", 1);
+
+      // Transition the updating ticks to the new scale, x1.
+      xtick.transition()
+          .duration(duration)
+          .attr("transform", tx)
+          .style("opacity", 1);
+
+      // Transition the exiting ticks to the new scale, x1.
+      xtick.exit().transition()
+          .duration(duration)
+          .attr("transform", tx)
+          .style("opacity", 1e-6)
+          .remove();
+
       // Add y-axis ticks.
       var ytick = svg.selectAll("g.y")
-          .data(y.ticks(10))
-        .enter().append("svg:g")
-          .attr("class", "y")
-          .attr("transform", function(d) { return "translate(0," + y(1 - d) + ")"; });
+          .data(y1.ticks(10));
 
-      ytick.append("svg:line")
+      var ytickEnter = ytick.enter().append("svg:g")
+          .attr("class", "y")
+          .attr("transform", function(d) { return "translate(0," + y0(1 - d) + ")"; });
+
+      ytickEnter.append("svg:line")
           .attr("x1", -6)
           .style("stroke", "#000");
 
-      ytick.append("svg:text")
+      ytickEnter.append("svg:text")
           .attr("x", -8)
           .attr("text-anchor", "end")
           .attr("dy", ".35em")
           .text(p);
 
+      // Transition the entering ticks to the new scale, y1.
+      ytickEnter.transition()
+          .duration(duration)
+          .attr("transform", ty)
+          .style("opacity", 1);
+
+      // Transition the updating ticks to the new scale, y1.
+      ytick.transition()
+          .duration(duration)
+          .attr("transform", ty)
+          .style("opacity", 1);
+
+      // Transition the exiting ticks to the new scale, y1.
+      ytick.exit().transition()
+          .duration(duration)
+          .attr("transform", ty)
+          .style("opacity", 1e-6)
+          .remove();
+
       // Add a group for each x-category.
       var xGroup = svg.selectAll("g.group")
-          .data(groups)
-        .enter().append("svg:g")
+          .data(groups);
+
+      xGroup.enter().append("svg:g")
           .attr("class", "group")
           .attr("xlink:title", function(d) { return d.key; })
-          .attr("transform", function(d) { return "translate(" + x(d.offset / sum) + ")"; });
+          .attr("transform", function(d) { return "translate(" + x0(d.offset / sum) + ")"; })
+          .style("opacity", 1e-6)
+        .transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + x1(d.offset / sum) + ")"; })
+          .style("opacity", 1);
+
+      xGroup.transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + x1(d.offset / sum) + ")"; });
+
+      xGroup.exit().transition()
+          .duration(duration)
+          .attr("transform", function(d) { return "translate(" + x1(d.offset / sum) + ")"; })
+          .style("opacity", 1e-6)
+          .remove();
 
       // Add a rect for each y-category.
-      var yGroup = xGroup.selectAll(".y")
-          .data(function(d) { return d.values; })
-        .enter().append("svg:a")
+      function title(d, i) {
+        return categoryY.call(this, d, i) + " " + d.parent.key + ": " + n(d.value);
+      }
+
+      // Re-select to include entering and existing x-category groups.
+      var yGroup = svg.selectAll("g.group").selectAll("a.y")
+          .data(function(d) { return d.values; });
+
+      yGroup.enter().append("svg:a")
           .attr("class", "y")
-          .attr("xlink:title", function(d, i) { return categoryY.call(this, d, i) + " " + d.parent.key + ": " + n(d.value); })
+          .attr("xlink:title", title)
         .append("svg:rect")
-          .attr("y", function(d) { return y(d.offset / d.parent.sum); })
-          .attr("height", function(d) { return y(d.value / d.parent.sum); })
-          .attr("width", function(d) { return x(d.parent.sum / sum); })
-          .style("fill", function(d, i) { return z(categoryY.call(this, d, i)); });
+          .attr("y", function(d) { return y0(d.offset / d.parent.sum); })
+          .attr("height", function(d) { return y0(d.value / d.parent.sum); })
+          .attr("width", function(d) { return x0(d.parent.sum / sum); })
+          .style("fill", function(d, i) { return z(categoryY.call(this, d, i)); })
+          .style("opacity", 1e-6)
+        .transition()
+          .duration(duration)
+          .attr("y", function(d) { return y1(d.offset / d.parent.sum); })
+          .attr("height", function(d) { return y1(d.value / d.parent.sum); })
+          .attr("width", function(d) { return x1(d.parent.sum / sum); })
+          .style("opacity", 1);
+
+      yGroup
+          .attr("xlink:title", title)
+        .select("rect").transition()
+          .duration(duration)
+          .attr("y", function(d) { return y1(d.offset / d.parent.sum); })
+          .attr("height", function(d) { return y1(d.value / d.parent.sum); })
+          .attr("width", function(d) { return x1(d.parent.sum / sum); });
+
+      yGroup.exit().transition()
+          .duration(duration)
+          .attr("y", function(d) { return y1(d.offset / d.parent.sum); })
+          .attr("height", function(d) { return y1(d.value / d.parent.sum); })
+          .attr("width", function(d) { return x1(d.parent.sum / sum); })
+          .style("opacity", 1e-6)
+          .remove();
+
+      d3.timer.flush();
     });
   }
 
   mekko.width = function(w) {
     if (!arguments.length) return width;
     width = w;
-    x.range([0, width]);
     return mekko;
   };
 
   mekko.height = function(h) {
     if (!arguments.length) return height;
     height = h;
-    y.range([0, height]);
     return mekko;
   };
 
@@ -855,6 +966,12 @@ d3.chart.mekko = function() {
   mekko.categoryY = function(x) {
     if (!arguments.length) return categoryY;
     categoryY = x;
+    return mekko;
+  };
+
+  mekko.duration = function(x) {
+    if (!arguments.length) return duration;
+    duration = x;
     return mekko;
   };
 
