209a020114b9a7e343c395dc7bd5fdb69667e411
Jason Davies <jason@jasondavies.com>
2011-06-18 11:19:06 +0100

    Add ticks tests for pow scale.

--- tests/test-scale-pow.js
+++ tests/test-scale-pow.js
@@ -50,6 +50,22 @@ console.log("         1.00  -> ", x(1.00));
 console.log("         2.00  -> ", x(2.00));
 console.log("");
 
+var x = d3.scale.pow().exponent(2);
+console.log("ticks:");
+console.log("            1  -> ", x.ticks(1).map(x.tickFormat(1)).join(", "));
+console.log("            2  -> ", x.ticks(2).map(x.tickFormat(2)).join(", "));
+console.log("            5  -> ", x.ticks(5).map(x.tickFormat(5)).join(", "));
+console.log("           10  -> ", x.ticks(10).map(x.tickFormat(10)).join(", "));
+console.log("");
+
+var x = d3.scale.pow().exponent(2).domain([1, 0]);
+console.log("descending ticks:");
+console.log("            1  -> ", x.ticks(1).map(x.tickFormat(1)).join(", "));
+console.log("            2  -> ", x.ticks(2).map(x.tickFormat(2)).join(", "));
+console.log("            5  -> ", x.ticks(5).map(x.tickFormat(5)).join(", "));
+console.log("           10  -> ", x.ticks(10).map(x.tickFormat(10)).join(", "));
+console.log("");
+
 var x = d3.scale.pow().exponent(2).domain([1, 2]).range([0, 1]).clamp(true);
 console.log("domain clamping:")
 console.log("        under  -> ", x(-1));
