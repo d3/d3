906e7eefc386f7fcf139569b3514e63692b31c45
Mike Bostock <mbostock@gmail.com>
2011-06-23 20:03:27 -0700

    Add x0 & x1 accessors to d3.svg.area.
    Thanks for the inspiration, @strongh!

--- src/svg/area.js
+++ src/svg/area.js
@@ -1,49 +1,66 @@
 d3.svg.area = function() {
-  var x = d3_svg_lineX,
+  var x0 = d3_svg_lineX,
+      x1 = d3_svg_lineX,
       y0 = 0,
       y1 = d3_svg_lineY,
       interpolate = "linear",
       interpolator = d3_svg_lineInterpolators[interpolate],
       tension = .7;
 
-  // TODO horizontal / vertical / radial orientation
-
   function area(d) {
     if (d.length < 1) return null;
-    var points1 = d3_svg_linePoints(this, d, x, y1),
-        points0 = d3_svg_linePoints(this, d, d3_svg_areaX(points1), y0).reverse();
+    var points0 = d3_svg_linePoints(this, d, x0, y0),
+        points1 = d3_svg_linePoints(this, d, x0 === x1 ? d3_svg_areaX(points0) : x1, y0 === y1 ? d3_svg_areaY(points0) : y1);
     return "M" + interpolator(points1, tension)
-         + "L" + interpolator(points0, tension)
+         + "L" + interpolator(points0.reverse(), tension)
          + "Z";
   }
 
-  area.x = function(v) {
-    if (!arguments.length) return x;
-    x = v;
+  area.x = function(x) {
+    if (!arguments.length) return x1;
+    x0 = x1 = x;
     return area;
   };
 
-  area.y0 = function(v) {
-    if (!arguments.length) return y0;
-    y0 = v;
+  area.x0 = function(x) {
+    if (!arguments.length) return x0;
+    x0 = x;
     return area;
   };
 
-  area.y1 = function(v) {
+  area.x1 = function(x) {
+    if (!arguments.length) return x1;
+    x1 = x;
+    return area;
+  };
+
+  area.y = function(y) {
     if (!arguments.length) return y1;
-    y1 = v;
+    y0 = y1 = y;
     return area;
   };
 
-  area.interpolate = function(v) {
+  area.y0 = function(y) {
+    if (!arguments.length) return y0;
+    y0 = y;
+    return area;
+  };
+
+  area.y1 = function(y) {
+    if (!arguments.length) return y1;
+    y1 = y;
+    return area;
+  };
+
+  area.interpolate = function(x) {
     if (!arguments.length) return interpolate;
-    interpolator = d3_svg_lineInterpolators[interpolate = v];
+    interpolator = d3_svg_lineInterpolators[interpolate = x];
     return area;
   };
 
-  area.tension = function(v) {
+  area.tension = function(x) {
     if (!arguments.length) return tension;
-    tension = v;
+    tension = x;
     return area;
   };
 
@@ -52,6 +69,12 @@ d3.svg.area = function() {
 
 function d3_svg_areaX(points) {
   return function(d, i) {
-    return d3_svg_lineX.call(this, points[i], i);
+    return points[i][0];
+  };
+}
+
+function d3_svg_areaY(points) {
+  return function(d, i) {
+    return points[i][1];
   };
 }
