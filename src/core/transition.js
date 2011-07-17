0afaaa248f4f63810456b4f72d6f8723dbcaf159
Mike Bostock <mbostock@gmail.com>
2011-07-16 20:42:49 -0700

    Update source files.

--- src/core/transition.js
+++ src/core/transition.js
@@ -97,7 +97,7 @@ function d3_transition(groups) {
           var owner = tx.owner;
           if (owner === transitionId) {
             delete this.__transition__;
-            if (remove) this.parentNode.removeChild(this);
+            if (remove && this.parentNode) this.parentNode.removeChild(this);
           }
           d3_transitionInheritId = transitionId;
           event.end.dispatch.apply(this, arguments);
