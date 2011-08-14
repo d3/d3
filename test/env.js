94800d39d2f6b22bef10c6921ee44906dcaa85d3
Mike Bostock <mbostock@gmail.com>
2011-08-13 17:27:54 -0700

    Add test for selection.sort.

--- test/env.js
+++ test/env.js
@@ -6,3 +6,17 @@ require("../lib/sizzle/sizzle");
 Sizzle = window.Sizzle;
 
 process.env.TZ = "America/Los_Angeles";
+
+var assert = require("assert");
+
+assert.domNull = function(actual, message) {
+  if (actual !== null) {
+    assert.fail(actual+"", null, message || "expected null, got {actual}", "===", assert.domNull);
+  }
+};
+
+assert.domEqual = function(actual, expected, message) {
+  if (actual !== expected) {
+    assert.fail(actual+"", expected+"", message || "expected {expected}, got {actual}", "===", assert.domEqual);
+  }
+};
