3685a428fe55252527c6e072ce171b0004685d47
Mike Bostock <mbostock@gmail.com>
2011-08-14 10:44:58 -0700

    Add test for rgb.

--- test/env.js
+++ test/env.js
@@ -20,3 +20,15 @@ assert.domEqual = function(actual, expected, message) {
     assert.fail(actual+"", expected+"", message || "expected {expected}, got {actual}", "===", assert.domEqual);
   }
 };
+
+assert.rgbEqual = function(actual, r, g, b, message) {
+  if (actual.r !== r || actual.g !== g || actual.b !== b) {
+    assert.fail("rgb(" + actual.r + ", " + actual.g + ", " + actual.b + ")", "rgb(" + r + ", " + g + ", " + b + ")", message || "expected {expected}, got {actual}", "===", assert.rgbEqual);
+  }
+};
+
+assert.hslEqual = function(actual, h, s, l, message) {
+  if (actual.h !== h || actual.s !== s || actual.l !== l) {
+    assert.fail(actual+"", "hsl(" + h + ", " + (s * 100) + "%, " + (l * 100) + "%)", message || "expected {expected}, got {actual}", "===", assert.hslEqual);
+  }
+};
