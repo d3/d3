ffb445bb1b581dd1e780d043cb130479300e5681
Mike Bostock <mbostock@gmail.com>
2011-08-16 10:51:55 -0700

    Add test for requote.
    And, fix a bug where pipes were not escaped.

--- d3.js
+++ d3.js
@@ -336,7 +336,7 @@ d3.requote = function(s) {
   return s.replace(d3_requote_re, "\\$&");
 };
 
-var d3_requote_re = /[\\\^\$\*\+\?\[\]\(\)\.\{\}]/g;
+var d3_requote_re = /[\\\^\$\*\+\?\|\[\]\(\)\.\{\}]/g;
 d3.round = function(x, n) {
   return n
       ? Math.round(x * Math.pow(10, n)) * Math.pow(10, -n)
