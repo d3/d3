4b25f0a62b8028690a9b28f99ab9a2b32bb8bce6
Mike Bostock <mbostock@gmail.com>
2011-08-20 17:04:50 -0700

    Simplify map implementation.

--- src/core/selection-map.js
+++ src/core/selection-map.js
@@ -1,8 +1,5 @@
 function d3_selection_map(map) {
-  for (var j = 0, m = this.length; j < m; j++) {
-    for (var group = this[j], i = 0, n = group.length, node; i < n; i++) {
-      if (node = group[i]) node.__data__ = map.call(node, node.__data__, i);
-    }
-  }
-  return this;
+  return this.each(function() {
+    this.__data__ = map.apply(this, arguments);
+  });
 }
