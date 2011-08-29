51f66413142bbc26d5ebd2d50fd245416709a16f
Mike Bostock <mbostock@gmail.com>
2011-08-28 17:10:52 -0700

    Default priority to empty string, not null.

--- d3.js
+++ d3.js
@@ -1910,12 +1910,12 @@ d3_transitionPrototype.attrTween = function(name, tween) {
   return this.tween("attr." + name, name.local ? attrTweenNS : attrTween);
 };
 d3_transitionPrototype.style = function(name, value, priority) {
-  if (arguments.length < 3) priority = null;
+  if (arguments.length < 3) priority = "";
   return this.styleTween(name, d3_transitionTween(value), priority);
 };
 
 d3_transitionPrototype.styleTween = function(name, tween, priority) {
-  if (arguments.length < 3) priority = null;
+  if (arguments.length < 3) priority = "";
   return this.tween("style." + name, function(d, i) {
     var f = tween.call(this, d, i, window.getComputedStyle(this, null).getPropertyValue(name));
     return f && function(t) {
--- src/core/transition-style.js
+++ src/core/transition-style.js
@@ -1,10 +1,10 @@
 d3_transitionPrototype.style = function(name, value, priority) {
-  if (arguments.length < 3) priority = null;
+  if (arguments.length < 3) priority = "";
   return this.styleTween(name, d3_transitionTween(value), priority);
 };
 
 d3_transitionPrototype.styleTween = function(name, tween, priority) {
-  if (arguments.length < 3) priority = null;
+  if (arguments.length < 3) priority = "";
   return this.tween("style." + name, function(d, i) {
     var f = tween.call(this, d, i, window.getComputedStyle(this, null).getPropertyValue(name));
     return f && function(t) {
