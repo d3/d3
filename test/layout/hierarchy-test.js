ab525c4784335113a9c60f6236a9ddc2a4c63af4
Jason Davies <jason@jasondavies.com>
2011-09-08 07:09:51 +0100

    Fix empty children handling for revalue.
    Also, fix empty children handling for cluster layout.
    
    Tests included.

--- test/layout/hierarchy-test.js
+++ test/layout/hierarchy-test.js
@@ -11,8 +11,23 @@ suite.addBatch({
   "hierarchy": {
     topic: d3.layout.hierarchy,
     "doesn't overwrite the value of a node that has an empty children array": function(hierarchy) {
-      assert.deepEqual(hierarchy({value: 1, children: []}), [
-        {data: {value: 1, children: []}, depth: 0, value: 1}
+      var nodes = hierarchy.nodes({value: 1, children: []});
+      assert.deepEqual(nodes, [
+        {children: [], depth: 0, value: 1}
+      ]);
+      hierarchy.revalue(nodes[0]);
+      assert.deepEqual(nodes, [
+        {children: [], depth: 0, value: 1}
+      ]);
+    },
+    "a valueless node that has an empty children array gets a value of 0": function(hierarchy) {
+      var nodes = hierarchy.nodes({children: []});
+      assert.deepEqual(nodes, [
+        {children: [], depth: 0, value: 0}
+      ]);
+      hierarchy.revalue(nodes[0]);
+      assert.deepEqual(nodes, [
+        {children: [], depth: 0, value: 0}
       ]);
     }
   }
