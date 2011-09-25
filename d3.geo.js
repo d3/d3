a6eb9284908fdd3853c26d70212e701701c805a7
Jason Davies <jason@jasondavies.com>
2011-09-25 23:40:28 +0100

    Add Bonne geographic projection.
    Note that the Werner projection is a special case with standard parallel
    at 90°N, and the Sinusoidal projection is also a special case with
    standard parallel at 0°N.

--- d3.geo.js
+++ d3.geo.js
@@ -197,6 +197,70 @@ d3.geo.albersUsa = function() {
 };
 
 var d3_radians = Math.PI / 180;
+d3.geo.bonne = function() {
+  var origin,
+      scale = 200,
+      translate = [480, 250],
+      standardParallel, // 90 for Wener, 0 for Sinusoidal
+      p0,
+      ctp0,
+      x0,
+      y0;
+
+  function bonne(coordinates) {
+    var x1 = coordinates[0] * d3_radians - x0,
+        y1 = coordinates[1] * d3_radians - y0,
+        p = ctp0 + p0 - y1,
+        E = x1 * Math.cos(y1) / p,
+        x = p * Math.sin(E),
+        y = p * Math.cos(E) - ctp0;
+    return [
+      scale * x + translate[0],
+      scale * y + translate[1]
+    ];
+  }
+
+  bonne.invert = function(coordinates) {
+    var x = (coordinates[0] - translate[0]) / scale,
+        y = (coordinates[1] - translate[1]) / scale,
+        c = ctp0 + y,
+        p = Math.sqrt(x * x + c * c),
+        y1 = (ctp0 + p0 - p);
+    return [
+      (x0 + p * Math.atan2(x, c) / Math.cos(y1)) / d3_radians,
+      y1 / d3_radians
+    ];
+  };
+
+  bonne.standardParallel = function(x) {
+    if (!arguments.length) return standardParallel;
+    standardParallel = +x;
+    ctp0 = 1 / Math.tan(p0 = standardParallel * d3_radians);
+    return bonne;
+  };
+
+  bonne.origin = function(x) {
+    if (!arguments.length) return origin;
+    origin = x;
+    x0 = origin[0] * d3_radians;
+    y0 = origin[1] * d3_radians;
+    return bonne;
+  };
+
+  bonne.scale = function(x) {
+    if (!arguments.length) return scale;
+    scale = +x;
+    return bonne;
+  };
+
+  bonne.translate = function(x) {
+    if (!arguments.length) return translate;
+    translate = [+x[0], +x[1]];
+    return bonne;
+  };
+
+  return bonne.origin([0, 0]).standardParallel(40);
+};
 d3.geo.equirectangular = function() {
   var scale = 500,
       translate = [480, 250];
