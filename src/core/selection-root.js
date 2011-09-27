80fafd527c92464e52fd9842446ddd2cb6521c1e
Mike Bostock <mbostock@gmail.com>
2011-09-27 08:45:06 -0700

    Add back d3_array for IE9.
    This also tweaks the array conversion slightly so that it is called by
    selection.selectAll rather than d3_selectAll; this guarantees that the selection
    groups are always arrays, even when a function selector is used.

--- src/core/selection-root.js
+++ src/core/selection-root.js
@@ -3,14 +3,16 @@ var d3_selectionRoot = d3_selection([[document]]);
 d3_selectionRoot[0].parentNode = document.documentElement;
 
 // TODO fast singleton implementation!
+// TODO select(function)
 d3.select = function(selector) {
   return typeof selector === "string"
       ? d3_selectionRoot.select(selector)
       : d3_selection([[selector]]); // assume node
 };
 
+// TODO selectAll(function)
 d3.selectAll = function(selector) {
   return typeof selector === "string"
       ? d3_selectionRoot.selectAll(selector)
-      : d3_selection([selector]); // assume node[]
+      : d3_selection([d3_array(selector)]); // assume node[]
 };
