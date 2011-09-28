05af35f3d35e05409778b8ced688c9b6223d8636
Jason Davies <jason@jasondavies.com>
2011-09-28 10:18:27 +0100

    Rename cylindrical test.

--- test/geo/cylindrical-test.js
+++ test/geo/cylindrical-test.js
@@ -5,23 +5,20 @@ require("../../d3.geo");
 var vows = require("vows"),
     assert = require("assert");
 
-var suite = vows.describe("d3.geo.equirectangular");
-
-var geoTests = {
-};
+var suite = vows.describe("d3.geo.cylindrical");
 
 suite.addBatch({
-  "equirectangular": {
+  "cylindrical": {
     "zero origin": {
-      topic: d3.geo.equirectangular,
+      topic: d3.geo.cylindrical,
       "Arctic":      testProjection([0,    85], [480, 131.944444]),
       "Antarctic":   testProjection([0,   -85], [480, 368.055555]),
       "Hawaii":      testProjection([-180,  0], [230, 250]),
       "Phillipines": testProjection([ 180,  0], [730, 250]),
       "Inversion works for non-zero translation": function() {
-        var equirectangular = d3.geo.equirectangular().translate([123, 99]).scale(100),
-            coords = equirectangular([0, 85]),
-            lonlat = equirectangular.invert(coords);
+        var cylindrical = d3.geo.cylindrical().translate([123, 99]).scale(100),
+            coords = cylindrical([0, 85]),
+            lonlat = cylindrical.invert(coords);
         assert.inDelta(lonlat[0], 0, 1e-6);
         assert.inDelta(lonlat[1], 85, 1e-6);
       }
