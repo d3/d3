cf882501efb75850e073a8421bf573a35c4d6498
Jason Davies <jason@jasondavies.com>
2011-09-30 07:51:33 +0100

    Simplify.
    Thanks again, @mbostock!

--- d3.js
+++ d3.js
@@ -1332,18 +1332,15 @@ d3_selectionPrototype.attr = function(name, value) {
 };
 d3_selectionPrototype.classed = function(name, value) {
   var names = name.split(d3_selection_classedWhitespace),
-      n = names.length;
-  if (n > 1) {
-    var i = -1;
-    if (arguments.length > 1) {
-      while (++i < n) d3_selection_classed.call(this, names[i], value);
-      return this;
-    } else {
-      while (++i < n) if (!d3_selection_classed.call(this, names[i])) return false;
-      return true;
-    }
+      n = names.length,
+      i = -1;
+  if (arguments.length > 1) {
+    while (++i < n) d3_selection_classed.call(this, names[i], value);
+    return this;
+  } else {
+    while (++i < n) if (!d3_selection_classed.call(this, names[i])) return false;
+    return true;
   }
-  return d3_selection_classed.apply(this, arguments);
 };
 
 var d3_selection_classedWhitespace = /\s+/g;
