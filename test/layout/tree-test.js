886b6599bbf7d81f20c3ef650bf86ee3e4a15a10
Jason Davies <jason@jasondavies.com>
2011-09-30 08:43:22 +0100

    Another test for empty children arrays.
    This tests for the issue fixed in
    6804a60fe4899fa83586bad51b45ad94ef215203.

--- test/layout/tree-test.js
+++ test/layout/tree-test.js
@@ -11,11 +11,32 @@ suite.addBatch({
   "tree": {
     topic: d3.layout.tree,
     "can handle an empty children array": function(tree) {
-      assert.deepEqual(tree.nodes({children: []}), [
-        {children: [], depth: 0, x: 0.5, y: 0}
+      assert.deepEqual(tree.nodes({children: []}).map(layout), [
+        {depth: 0, x: 0.5, y: 0}
+      ]);
+      var result = tree.nodes({children: [
+        {children: []},
+        {children: [{}]},
+        {children: [{}]}
+      ]}).map(layout);
+      assert.deepEqual(result, [
+        {depth: 0, x: .5,   y: 0},
+        {depth: 1, x: .125, y: 0.5},
+        {depth: 1, x: .375, y: 0.5},
+        {depth: 2, x: .375, y: 1},
+        {depth: 1, x: .875, y: 0.5},
+        {depth: 2, x: .875, y: 1}
       ]);
     }
   }
 });
+
+function layout(node) {
+  return {
+    depth: node.depth,
+    x: node.x,
+    y: node.y
+  };
+}
 
 suite.export(module);
