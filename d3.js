7fe3ae41839e059db6f985d5556bcfbd5d9d80ff
Mike Bostock <mbostock@gmail.com>
2011-11-14 16:07:11 -0800

    Fix a couple bugs with transforms.
    We need to check the matrix determinant for flipped coordinates. We also need to
    check for invalid transforms, which are now treated as the identity transform.

--- d3.js
+++ d3.js
@@ -2250,7 +2250,8 @@ var d3_timer_frame = window.requestAnimationFrame
     || function(callback) { setTimeout(callback, 17); };
 d3.transform = function(string) {
   d3_transformG.setAttribute("transform", string);
-  return new d3_transform(d3_transformG.transform.baseVal.consolidate().matrix);
+  var t = d3_transformG.transform.baseVal.consolidate();
+  return new d3_transform(t ? t.matrix : d3_transformIdentity);
 };
 
 // Compute x-scale and normalize the first row.
@@ -2262,10 +2263,11 @@ function d3_transform(m) {
       r1 = [m.c, m.d],
       kx = d3_transformNormalize(r0),
       kz = d3_transformDot(r0, r1),
-      ky = d3_transformNormalize(d3_transformCombine(r1, r0, -kz));
+      ky = d3_transformNormalize(d3_transformCombine(r1, r0, -kz)) || 0;
+  if (r0[0] * r1[1] < r1[0] * r0[1]) r0[1] *= -1, ky *= -1;
   this.translate = [m.e, m.f];
-  this.rotate = Math.atan2(m.b, m.a) * d3_transformDegrees;
-  this.scale = [kx, ky || 0];
+  this.rotate = Math.atan2(r0[1], r0[0]) * d3_transformDegrees;
+  this.scale = [kx, ky];
   this.skew = ky ? kz / ky * d3_transformDegrees : 0;
 };
 
@@ -2295,6 +2297,7 @@ function d3_transformCombine(a, b, k) {
 }
 
 var d3_transformG = document.createElementNS(d3.ns.prefix.svg, "g"),
+    d3_transformIdentity = {a: 1, b: 0, c: 0, d: 1, e: 0, f: 0},
     d3_transformDegrees = 180 / Math.PI;
 function d3_noop() {}
 d3.scale = {};
