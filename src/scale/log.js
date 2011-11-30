a752b9627f9aeb0d743ff3e6cd0b425cd70855f5
Mike Bostock <mbostock@gmail.com>
2011-11-29 21:36:51 -0800

    Fix #394.

--- src/scale/log.js
+++ src/scale/log.js
@@ -52,7 +52,7 @@ function d3_scale_log(linear, log) {
     if (arguments.length < 2) format = d3_scale_logFormat;
     if (arguments.length < 1) return format;
     var k = n / scale.ticks().length,
-        f = log === d3_scale_logn ? (e = -1e-15, Math.floor) : (e = 1e-15, Math.ceil),
+        f = log === d3_scale_logn ? (e = -1e-12, Math.floor) : (e = 1e-12, Math.ceil),
         e;
     return function(d) {
       return d / pow(f(log(d) + e)) < k ? format(d) : "";
