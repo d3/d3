2cda6f4eb10e9803a47df5b2631ff49ec17cac33
Mike Bostock <mbostock@gmail.com>
2012-02-12 08:42:45 -0800

    Fix for negative-size nodes in IE9.

--- _includes/treemap.js
+++ _includes/treemap.js
@@ -48,6 +48,6 @@ function cell() {
   this
       .style("left", function(d) { return d.x + "px"; })
       .style("top", function(d) { return d.y + "px"; })
-      .style("width", function(d) { return d.dx - 1 + "px"; })
-      .style("height", function(d) { return d.dy - 1 + "px"; });
+      .style("width", function(d) { return Math.max(0, d.dx - 1) + "px"; })
+      .style("height", function(d) { return Math.max(0, d.dy - 1) + "px"; });
 }
