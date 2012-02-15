8178c2961ba0f010ad6fbc891d3f9ecdf56d2a43
Trevor Norris <trev.norris@gmail.com>
2012-02-15 14:23:27 -0800

    clean up step calculations. add better method for values with no decimal part. included additional tests to demonstrate fixes.

--- src/core/range.js
+++ src/core/range.js
@@ -13,19 +13,26 @@ d3.range = function(start, stop, step) {
   }
   if ((stop - start) / step == Infinity) throw new Error("infinite range");
   var range = [],
-       h = calcRdx(start, stop),
+       h = calcRdx(start, step),
        i = -1,
        j;
-  if (step < 0) while ((j = (h * start + h * step * ++i) / h) > stop) range.push(j);
-  else while ((j = (h * start + h * step * ++i) / h) < stop) range.push(j);
+  start *= h;
+  stop *= h;
+  step *= h;
+  if (step < 0) while ((j = start + step * ++i) > stop) range.push(j / h);
+  else while ((j = start + step * ++i) < stop) range.push(j / h);
   return range;
 };
 
 // calculate correction for IEEE error
 function calcRdx(n, m) {
-  var low = n < m ? n : m,
-      fix = 1;
-  if (isNaN(+low)) return low;
-  while ((fix *= 10) * low % 1 !== 0);
+  var low = Math.abs(n) < Math.abs(m) ? n : m,
+      fix = 10;
+  // check for non-applicable values
+  if (low == 0 || isNaN(low)) return fix;
+  // there is a decimal part, so calculate "to integer" shift by accumulating error
+  if (low % 1) while (fix * low % 1) fix *= 10;
+  // no decimal part, and start fix from 1
+  else while (fix /= 10, fix * low % 10 === 0);
   return fix;
 }
