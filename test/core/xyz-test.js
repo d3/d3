68faca27c01aa43b7635f8d70ed1c6e86842b898
Justin Cormack <justin@specialbusservice.com>
2012-03-02 10:11:59 +0000

    clean up tests a little

--- test/core/xyz-test.js
+++ test/core/xyz-test.js
@@ -11,24 +11,19 @@ suite.addBatch({
       return d3.xyz;
     },
     "exposes x, y and z properties": function(xyz) {
-      var color = xyz("#abc");
-      assert.xyzEqual(color, 45.246971009995335, 48.44632879252148, 64.09304697440157);
+      assert.xyzEqual(xyz("#abc"), 45.246971009995335, 48.44632879252148, 64.09304697440157);
     },
     "can convert from XYZ": function(xyz) {
-      var color = xyz(d3.xyz(40, 50, 60));
-      assert.xyzEqual(color, 40, 50, 60);
+      assert.xyzEqual(xyz(d3.xyz(40, 50, 60)), 40, 50, 60);
     },
     "can convert from RGB": function(xyz) {
-      var color = d3.rgb("#abc").xyz();
-      assert.xyzEqual(color, 45.246971009995335, 48.44632879252148, 64.09304697440157);
+      assert.xyzEqual(d3.rgb("#abc").xyz(), 45.246971009995335, 48.44632879252148, 64.09304697440157);
     },
     "can convert from HSL": function(xyz) {
-      var color = d3.hsl("#abc").xyz();
-      assert.xyzEqual(color, 45.246971009995335, 48.44632879252148, 64.09304697440157);
+      assert.xyzEqual(d3.hsl("#abc").xyz(), 45.246971009995335, 48.44632879252148, 64.09304697440157);
     },
     "can initialize with RGB": function(xyz) {
-      var color = xyz(d3.rgb(30, 40, 50));
-      assert.xyzEqual(color, 1.8699354618049604, 2.0238922484735156, 3.309705799692103);
+      assert.xyzEqual(xyz(d3.rgb(30, 40, 50)), 1.8699354618049604, 2.0238922484735156, 3.309705799692103);
     },
     "can convert to RGB": function(xyz) {
       assert.rgbEqual(xyz(d3.rgb(30, 40, 50)).rgb(), 30, 40, 50);
