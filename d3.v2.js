2e83e39f4d3af87608245e04aa8ffe78c2c6b2b1
Jason Davies <jason@jasondavies.com>
2012-03-16 16:42:02 +0000

    An even better workaround for an IE9 bug.
    It's as if IE9 is forgetting about the visibility: hidden rect, and
    doing *something* to it jogs its memory.

--- d3.v2.js
+++ d3.v2.js
@@ -4267,7 +4267,6 @@ d3.svg.brush = function() {
       // An invisible, mouseable area for starting a new brush.
       bg.enter().append("rect")
           .attr("class", "background")
-          .style("pointer-events", "all") // required here due to IE9 not inheriting properly
           .style("visibility", "hidden")
           .style("cursor", "crosshair");
 
@@ -4368,6 +4367,7 @@ d3.svg.brush = function() {
 
     // Propagate the active cursor to the body for the drag duration.
     g.style("pointer-events", "none").selectAll(".resize").style("display", null);
+    g.select(".background").style("pointer-events", null); // workaround for IE9 bug
     d3.select("body").style("cursor", eventTarget.style("cursor"));
 
     // Notify listeners.
