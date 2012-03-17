ef7c743636e7b441cb0929f48f6801198b3e4d0b
Mike Bostock <mbostock@gmail.com>
2012-03-16 17:40:58 -0700

    Add area.defined.

--- src/svg/area.js
+++ src/svg/area.js
@@ -3,67 +3,77 @@ function d3_svg_area(projection) {
       x1 = d3_svg_lineX,
       y0 = 0,
       y1 = d3_svg_lineY,
+      defined,
       interpolate,
       i0,
       i1,
       tension = .7;
 
   function area(d) {
-    if (d.length < 1) return null;
-    var points0 = d3_svg_linePoints(this, d, x0, y0),
-        points1 = d3_svg_linePoints(this, d, x0 === x1 ? d3_svg_areaX(points0) : x1, y0 === y1 ? d3_svg_areaY(points0) : y1);
-    return "M" + i0(projection(points1), tension)
-         + "L" + i1(projection(points0.reverse()), tension)
-         + "Z";
+    var that = this;
+    d = defined ? d3.split(d, d3_svg_lineNot(defined)) : d.length ? [d] : [];
+    d = d.map(function(d) {
+      var points0 = d3_svg_linePoints(that, d, x0, y0);
+      return "M" + i0(projection(d3_svg_linePoints(that, d, x0 === x1 ? d3_svg_areaX(points0) : x1, y0 === y1 ? d3_svg_areaY(points0) : y1)), tension)
+           + "L" + i1(projection(points0.reverse()), tension)
+           + "Z";
+    });
+    return d.length ? d.join("") : null;
   }
 
-  area.x = function(x) {
+  area.x = function(_) {
     if (!arguments.length) return x1;
-    x0 = x1 = x;
+    x0 = x1 = _;
     return area;
   };
 
-  area.x0 = function(x) {
+  area.x0 = function(_) {
     if (!arguments.length) return x0;
-    x0 = x;
+    x0 = _;
     return area;
   };
 
-  area.x1 = function(x) {
+  area.x1 = function(_) {
     if (!arguments.length) return x1;
-    x1 = x;
+    x1 = _;
     return area;
   };
 
-  area.y = function(y) {
+  area.y = function(_) {
     if (!arguments.length) return y1;
-    y0 = y1 = y;
+    y0 = y1 = _;
     return area;
   };
 
-  area.y0 = function(y) {
+  area.y0 = function(_) {
     if (!arguments.length) return y0;
-    y0 = y;
+    y0 = _;
     return area;
   };
 
-  area.y1 = function(y) {
+  area.y1 = function(_) {
     if (!arguments.length) return y1;
-    y1 = y;
+    y1 = _;
     return area;
   };
 
-  area.interpolate = function(x) {
+  area.defined  = function(_) {
+    if (!arguments.length) return defined;
+    defined = _;
+    return area;
+  };
+
+  area.interpolate = function(_) {
     if (!arguments.length) return interpolate;
-    if (!d3_svg_lineInterpolators.has(x += "")) x = d3_svg_lineInterpolatorDefault;
-    i0 = d3_svg_lineInterpolators.get(interpolate = x);
+    if (!d3_svg_lineInterpolators.has(_ += "")) _ = d3_svg_lineInterpolatorDefault;
+    i0 = d3_svg_lineInterpolators.get(interpolate = _);
     i1 = i0.reverse || i0;
     return area;
   };
 
-  area.tension = function(x) {
+  area.tension = function(_) {
     if (!arguments.length) return tension;
-    tension = x;
+    tension = _;
     return area;
   };
 
--- src/svg/line.js
+++ src/svg/line.js
@@ -9,7 +9,9 @@ function d3_svg_line(projection) {
   function line(d) {
     var that = this;
     d = defined ? d3.split(d, d3_svg_lineNot(defined)) : d.length ? [d] : [];
-    d = d.map(function(d) { return "M" + interpolator(projection(d3_svg_linePoints(that, d, x, y)), tension); });
+    d = d.map(function(d) {
+      return "M" + interpolator(projection(d3_svg_linePoints(that, d, x, y)), tension);
+    });
     return d.length ? d.join("") : null;
   }
 
