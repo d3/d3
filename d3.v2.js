40c731d6885b51e469b02c2f6b65524410c8b097
Mike Bostock <mbostock@gmail.com>
2012-03-28 09:43:18 -0700

    Fix #434: chord groups returned in wrong order.

--- d3.v2.js
+++ d3.v2.js
@@ -5011,12 +5011,12 @@ d3.layout.chord = function() {
           value: v
         };
       }
-      groups.push({
+      groups[di] = {
         index: di,
         startAngle: x0,
         endAngle: x,
         value: (x - x0) / k
-      });
+      };
       x += padding;
     }
 
