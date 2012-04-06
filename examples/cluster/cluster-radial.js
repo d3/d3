b99ed1d79ea9da22fc871e979d130ebe6668504b
Kristofer Monisit <kmonisit@gmail.com>
2012-04-06 12:19:31 +0800

    Bring cluster example up-to-date

--- examples/cluster/cluster-radial.js
+++ examples/cluster/cluster-radial.js
@@ -1,16 +1,16 @@
-var r = 960 / 2;
+var radius = 960 / 2;
 
 var cluster = d3.layout.cluster()
-    .size([360, r - 120]);
+    .size([360, radius - 120]);
 
 var diagonal = d3.svg.diagonal.radial()
     .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
 
 var vis = d3.select("#chart").append("svg")
-    .attr("width", r * 2)
-    .attr("height", r * 2)
+    .attr("width", radius * 2)
+    .attr("height", radius * 2)
   .append("g")
-    .attr("transform", "translate(" + r + "," + r + ")");
+    .attr("transform", "translate(" + radius + "," + radius + ")");
 
 d3.json("../data/flare.json", function(json) {
   var nodes = cluster.nodes(json);
