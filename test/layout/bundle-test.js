7f2a3434b75156a72740ff582fc128704e1005b9
Jason Davies <jason@jasondavies.com>
2012-05-10 12:49:58 +0100

    Update d3.layout.bundle test.

--- test/layout/bundle-test.js
+++ test/layout/bundle-test.js
@@ -8,12 +8,6 @@ var suite = vows.describe("d3.layout.bundle");
 suite.addBatch({
   "bundle": {
     topic: d3.layout.bundle,
-    "can handle self-loops": function(bundle) {
-      var node = {id: 0};
-      assert.deepEqual(bundle([{source: node, target: node}]), [
-        [node, node]
-      ]);
-    },
     "simple": function(bundle) {
       var a = {id: "a"},
           b = {id: "b"},
@@ -22,11 +16,13 @@ suite.addBatch({
       assert.deepEqual(bundle([
         {source: a, target: b},
         {source: a, target: root},
-        {source: root, target: a}
+        {source: root, target: a},
+        {source: a, target: a}
       ]), [
         [a, root, b],
         [a, root],
         [root, a],
+        [a, root, a]
       ]);
     }
   }
