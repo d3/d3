83c32c9b2e7325f03ffd91981ad59087e1a67528
Ian MacLeod <ian@nevir.net>
2012-07-30 20:16:19 -0700

    Support for complex `selection.insert()` arguments
    * Both `name` and `before` support direct passing of DOM nodes.
    * They also can take a `function(d,i) {...}`, as you've come to expect!

--- d3.v2.js
+++ d3.v2.js
@@ -1764,25 +1764,60 @@ d3_selectionPrototype.append = function(name) {
 
   return this.select(name.local ? appendNS : append);
 };
-// TODO insert(node, function)?
-// TODO insert(function, string)?
 // TODO insert(function, function)?
 d3_selectionPrototype.insert = function(name, before) {
-  name = d3.ns.qualify(name);
+  var nodeFunc, beforeFunc;
 
-  function insert() {
-    return this.insertBefore(
-        document.createElementNS(this.namespaceURI, name),
-        d3_select(before, this));
+  function nodeFuncForName(nameOrNode) {
+    // Nodes are not strings.
+    if (typeof nameOrNode != "string") {
+      return function() { return nameOrNode; };
+    }
+
+    // insert gets hit pretty often, so we do as much work up front as we can,
+    // without completely sacrificing readability.
+    nameOrNode = d3.ns.qualify(nameOrNode);
+    if (nameOrNode.local) {
+      return function() {
+        return document.createElementNS(nameOrNode.space, nameOrNode.local);
+      };
+    } else {
+      return function() {
+        return document.createElementNS(this.namespaceURI, nameOrNode);
+      };
+    }
   }
 
-  function insertNS() {
-    return this.insertBefore(
-        document.createElementNS(name.space, name.local),
-        d3_select(before, this));
+  function beforeFuncForQuery(query) {
+    if (typeof query == "string") {
+      return function() { return d3_select(query, this); };
+    } else {
+      return function() { return query; };
+    }
   }
 
-  return this.select(name.local ? insertNS : insert);
+  if (typeof name == "function") {
+    nodeFunc = function() {
+      return nodeFuncForName(name.apply(this, arguments)).apply(this, arguments);
+    }
+  } else {
+    nodeFunc = nodeFuncForName(name);
+  }
+
+  if (typeof before == "function") {
+    beforeFunc = function() {
+      return beforeFuncForQuery(before.apply(this, arguments)).apply(this, arguments);
+    }
+  } else {
+    beforeFunc = beforeFuncForQuery(before);
+  }
+
+  return this.select(function() {
+    var newNode = nodeFunc.apply(this, arguments);
+    var beforeNode = beforeFunc.apply(this, arguments);
+
+    return this.insertBefore(newNode, beforeNode);
+  });
 };
 // TODO remove(selector)?
 // TODO remove(node)?
