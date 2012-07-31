bf34a43fbb1131bded8c76824b883533f05886e8
Ian MacLeod <ian@nevir.net>
2012-07-30 20:41:39 -0700

    Comment cleanup

--- src/core/selection-generation.js
+++ src/core/selection-generation.js
@@ -4,8 +4,6 @@ function d3_selection_nodeBuildingFunc(nameOrNode) {
     return function() { return nameOrNode; };
   }
 
-  // insert gets hit pretty often, so we do as much work up front as we can,
-  // without completely sacrificing readability.
   nameOrNode = d3.ns.qualify(nameOrNode);
   if (nameOrNode.local) {
     return function() {
