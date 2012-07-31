b77350822448378e88621005e7babd320474625a
gabriel montagné <gabriel@rojored.com>
2012-07-31 22:22:40 +0100

    Aligned selection-insert with selection-append: nodes and functions supported.

--- src/core/selection-insert.js
+++ src/core/selection-insert.js
@@ -1,8 +1,21 @@
 // TODO insert(node, function)?
-// TODO insert(function, string)?
 // TODO insert(function, function)?
-d3_selectionPrototype.insert = function(name, before) {
-  name = d3.ns.qualify(name);
+d3_selectionPrototype.insert = function(element, before) {
+  var node, func, name
+
+  switch (true) {
+    case element instanceof Function:
+      func = element;
+      break;
+
+    case element instanceof Node:
+      node = element;
+      break;
+
+    default:
+      name = d3.ns.qualify(element);
+      break;
+  }
 
   function insert() {
     return this.insertBefore(
@@ -16,5 +29,27 @@ d3_selectionPrototype.insert = function(name, before) {
         d3_select(before, this));
   }
 
-  return this.select(name.local ? insertNS : insert);
+  function insertNode() {
+    return this.insertBefore(
+        node,
+        d3_select(before, this));
+  }
+
+  function insertFunc() {
+    var result = func.apply(this, arguments);
+    if (result instanceof Node) {
+      return this.insertBefore(result,
+          d3_select(before, this));
+    }
+    name = d3.ns.qualify(result);
+    if (name.local) return insertNS.apply(this);
+    return insert.apply(this);
+  }
+
+  return this.select(
+      node        ? insertNode
+    : func        ? insertFunc
+    : name.local  ? insertNS
+    : append
+  );
 };
