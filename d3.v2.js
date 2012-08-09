952abefad0e92a628755938f8e4dc32c0a584ab2
Mike Bostock <mbostock@gmail.com>
2012-08-08 20:40:36 -0700

    Add d3.random.{logNormal,irwinHall}. Fixes #571.

--- d3.v2.js
+++ d3.v2.js
@@ -183,9 +183,10 @@
     return [ a, c ];
   };
   d3.random = {
-    normal: function(mean, deviation) {
-      if (arguments.length < 2) deviation = 1;
-      if (arguments.length < 1) mean = 0;
+    normal: function(µ, σ) {
+      var n = arguments.length;
+      if (n < 2) σ = 1;
+      if (n < 1) µ = 0;
       return function() {
         var x, y, r;
         do {
@@ -193,7 +194,22 @@
           y = Math.random() * 2 - 1;
           r = x * x + y * y;
         } while (!r || r > 1);
-        return mean + deviation * x * Math.sqrt(-2 * Math.log(r) / r);
+        return µ + σ * x * Math.sqrt(-2 * Math.log(r) / r);
+      };
+    },
+    logNormal: function(µ, σ) {
+      var n = arguments.length;
+      if (n < 2) σ = 1;
+      if (n < 1) µ = 0;
+      var random = d3.random.normal();
+      return function() {
+        return Math.exp(µ + σ * random());
+      };
+    },
+    irwinHall: function(m) {
+      return function() {
+        for (var s = 0, j = 0; j < m; j++) s += Math.random();
+        return s / m;
       };
     }
   };
