7a5ed691a766a3d22493598ccc435d886df1ff00
Jason Davies <jason@jasondavies.com>
2012-08-13 21:23:49 +0100

    d3.format: fix decimal points for null grouping.

--- d3.v2.js
+++ d3.v2.js
@@ -602,18 +602,19 @@
   function d3_format_typeDefault(x) {
     return x + "";
   }
-  var d3_format_group = d3_identity;
-  if (d3_format_grouping) {
-    var d3_format_groupingLength = d3_format_grouping.length;
-    d3_format_group = function(value) {
-      var i = value.lastIndexOf("."), f = i >= 0 ? d3_format_decimalPoint + value.substring(i + 1) : (i = value.length, ""), t = [], j = 0, g = d3_format_grouping[0];
+  function d3_format_group(value) {
+    var i = value.lastIndexOf("."), f = i >= 0 ? d3_format_decimalPoint + value.substring(i + 1) : (i = value.length, "");
+    if (d3_format_grouping) {
+      var t = [], j = 0, g = d3_format_grouping[0];
       while (i > 0 && g > 0) {
         t.unshift(value.substring(i -= g, i + g));
         g = d3_format_grouping[j = (j + 1) % d3_format_groupingLength];
       }
-      return t.join(d3_format_thousandsSeparator) + f;
-    };
+      return t.join(d3_format_thousandsSeparator || "") + f;
+    }
+    return value.substring(0, i) + f;
   }
+  var d3_format_groupingLength = d3_format_grouping ? d3_format_grouping.length : -1;
   var d3_formatPrefixes = [ "y", "z", "a", "f", "p", "n", "μ", "m", "", "k", "M", "G", "T", "P", "E", "Z", "Y" ].map(d3_formatPrefix);
   d3.formatPrefix = function(value, precision) {
     var i = 0;
--- src/core/format.js
+++ src/core/format.js
@@ -91,19 +91,20 @@ function d3_format_typeDefault(x) {
 }
 
 // Apply comma grouping for thousands.
-var d3_format_group = d3_identity;
-if (d3_format_grouping) {
-  var d3_format_groupingLength = d3_format_grouping.length;
-  d3_format_group = function(value) {
-    var i = value.lastIndexOf("."),
-        f = i >= 0 ? d3_format_decimalPoint + value.substring(i + 1) : (i = value.length, ""),
-        t = [],
+function d3_format_group(value) {
+  var i = value.lastIndexOf("."),
+      f = i >= 0 ? d3_format_decimalPoint + value.substring(i + 1) : (i = value.length, "");
+  if (d3_format_grouping) {
+    var t = [],
         j = 0,
         g = d3_format_grouping[0];
     while (i > 0 && g > 0) {
       t.unshift(value.substring(i -= g, i + g));
       g = d3_format_grouping[j = (j + 1) % d3_format_groupingLength];
     }
-    return t.join(d3_format_thousandsSeparator) + f;
-  };
+    return t.join(d3_format_thousandsSeparator || "") + f;
+  }
+  return value.substring(0, i) + f;
 }
+
+var d3_format_groupingLength = d3_format_grouping ? d3_format_grouping.length : -1;
