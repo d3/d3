597b7d0bc4c6fd58548d3f15cbca8bbeb8b724b0
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2012-09-18 11:47:14 -0400

    xhr execution delayed one tick, allowing method, headers and data to be set before execution.

--- src/core/xhr.js
+++ src/core/xhr.js
@@ -1,14 +1,49 @@
 d3.xhr = function(url, mime, callback) {
-  var req = new XMLHttpRequest;
+  var xhr = {},
+      headers = {}
+      method = "GET",
+      data = null,
+      req = new XMLHttpRequest;
   if (arguments.length < 3) callback = mime, mime = null;
   else if (mime && req.overrideMimeType) req.overrideMimeType(mime);
-  req.open("GET", url, true);
-  if (mime) req.setRequestHeader("Accept", mime);
-  req.onreadystatechange = function() {
-    if (req.readyState === 4) {
-      var s = req.status;
-      callback(!s && req.response || s >= 200 && s < 300 || s === 304 ? req : null);
+  if (mime) headers["Accept"] = mime;
+
+  // Push the XMLHttpRequest back one tick to allow changed parameters
+  setTimeout(function() {
+    req.open(method, url, true);
+    d3.entries(headers).forEach(function(d) {
+      req.setRequestHeader(d.key,d.value)
+    })
+    
+    req.onreadystatechange = function() {
+      if (req.readyState === 4) {
+        var s = req.status;
+        callback(!s && req.response || s >= 200 && s < 300 || s === 304 ? req : null);
+      }
+    };
+    console.log([headers,data])
+    req.send(data);
+  },0)
+  
+  xhr.setRequestHeader = function(key,value)  {
+    if (!value) {
+      delete headers[key];
+    } else {
+      headers[key] = value;  
     }
-  };
-  req.send(null);
+    return xhr;
+  }
+
+  xhr.data = function(d) {
+    data = d;
+    return xhr;
+  }
+
+  xhr.method = function(d) {
+    method = d;
+    return xhr;
+  }
+
+  return xhr;
+
 };
