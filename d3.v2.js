b12a5352d84ac8b3cb5624c9636fba7148f87b3b
Mike Bostock <mbostock@gmail.com>
2012-09-25 15:46:29 -0700

    Remove unnecessary modulo.

--- d3.v2.js
+++ d3.v2.js
@@ -2057,7 +2057,7 @@
         point = rotatePoint(coordinates[i]);
         λ1 = point[0];
         φ1 = point[1];
-        δλ = (Math.abs(λ1 - λ0) + 2 * π) % (2 * π);
+        δλ = Math.abs(λ1 - λ0);
         sλ0 = λ0 > 0;
         if (sλ0 ^ λ1 > 0 && (δλ >= π || δλ < ε && Math.abs(Math.abs(λ0) - π) < ε)) {
           φ0 = d3_geo_projectionIntersectAntemeridian(λ0, φ0, λ1, φ1);
