e409935c660c84355b55d2df1167d4e99fadec94
Jason Davies <jason@jasondavies.com>
2012-09-25 23:17:17 +0100

    Remove redundant code.

--- src/geo/antemeridian.js
+++ src/geo/antemeridian.js
@@ -13,16 +13,16 @@ function d3_geo_antemeridianRing(rotate, project) {
 
 function d3_geo_antemeridianClipLine(rotate, project, lineString, context) {
   if (!(n = lineString.length)) return;
-  var λ0, φ0, λ1, φ1, δλ, sλ0, n,
+  var λ0, φ0, λ1, φ1, δλ, sλ0, i = 0, n,
       point = rotate(lineString[0]);
   point = project(λ0 = point[0], φ0 = point[1]);
   context.moveTo(point[0], point[1]);
-  for (var i = 0, j = 0; j < n; j++) {
-    λ1 = (point = rotate(lineString[j]))[0];
+  while (++i < n) {
+    λ1 = (point = rotate(lineString[i]))[0];
     φ1 = point[1];
     δλ = (Math.abs(λ1 - λ0) + 2 * π) % (2 * π);
     sλ0 = λ0 > 0;
-    if (j > i && sλ0 ^ (λ1 > 0) && (δλ >= π || δλ < ε && Math.abs(Math.abs(λ0) - π) < ε)) {
+    if (sλ0 ^ (λ1 > 0) && (δλ >= π || δλ < ε && Math.abs(Math.abs(λ0) - π) < ε)) {
       φ0 = d3_geo_antemeridianIntersect(λ0, φ0, λ1, φ1);
       context.lineTo((point = project(sλ0 ? π : -π, φ0))[0], point[1]);
       context.moveTo((point = project(sλ0 ? -π : π, φ0))[0], point[1]);
