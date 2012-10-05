ac06afcdb04bbd98c03b1dc8d422e2311cf22536
Mike Bostock <mbostock@gmail.com>
2012-10-05 16:58:59 -0700

    Remove selection.map.

--- d3.v3.js
+++ d3.v3.js
@@ -4235,7 +4235,7 @@
     };
     return update;
   };
-  d3_selectionPrototype.datum = d3_selectionPrototype.map = function(value) {
+  d3_selectionPrototype.datum = function(value) {
     return arguments.length < 1 ? this.property("__data__") : this.property("__data__", value);
   };
   d3_selectionPrototype.filter = function(filter) {
--- src/core/selection-datum.js
+++ src/core/selection-datum.js
@@ -1,5 +1,4 @@
-d3_selectionPrototype.datum =
-d3_selectionPrototype.map = function(value) {
+d3_selectionPrototype.datum = function(value) {
   return arguments.length < 1
       ? this.property("__data__")
       : this.property("__data__", value);
