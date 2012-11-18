f2c948838b1561fd1f0942ea7beed238104d7ede
Mike Bostock <mbostock@gmail.com>
2012-11-18 09:01:00 -0800

    Needlessly optimize intersection code.

--- src/geom/polygon.js
+++ src/geom/polygon.js
@@ -75,14 +75,8 @@ function d3_geom_polygonInside(p, a, b) {
 
 // Intersect two infinite lines cd and ab.
 function d3_geom_polygonIntersect(c, d, a, b) {
-  var x1 = c[0], x2 = d[0], x3 = a[0], x4 = b[0],
-      y1 = c[1], y2 = d[1], y3 = a[1], y4 = b[1],
-      x13 = x1 - x3,
-      x21 = x2 - x1,
-      x43 = x4 - x3,
-      y13 = y1 - y3,
-      y21 = y2 - y1,
-      y43 = y4 - y3,
-      ua = (x43 * y13 - y43 * x13) / (y43 * x21 - x43 * y21);
+  var x1 = c[0], x3 = a[0], x21 = d[0] - x1, x43 = b[0] - x3,
+      y1 = c[1], y3 = a[1], y21 = d[1] - y1, y43 = b[1] - y3,
+      ua = (x43 * (y1 - y3) - y43 * (x1 - x3)) / (y43 * x21 - x43 * y21);
   return [x1 + ua * x21, y1 + ua * y21];
 }
