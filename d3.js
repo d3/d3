88890a600f4db7a969d5683faa7915e672cfb95f
Mike Bostock <mbostock@gmail.com>
2012-12-12 14:57:24 -0800

    Rewrite d3.geo.area using d3.geo.stream.

--- d3.js
+++ d3.js
@@ -6450,69 +6450,46 @@
   function d3_geo_pathCircle(radius) {
     return "m0," + radius + "a" + radius + "," + radius + " 0 1,1 0," + -2 * radius + "a" + radius + "," + radius + " 0 1,1 0," + +2 * radius + "z";
   }
-  d3.geo.area = d3_geo_type({
-    Point: d3_zero,
-    MultiPoint: d3_zero,
-    LineString: d3_zero,
-    MultiLineString: d3_zero,
-    Polygon: function(o) {
-      return d3_geo_areaPolygon(o.coordinates);
+  d3.geo.area = function(object) {
+    d3_geo_areaSum = 0;
+    d3.geo.stream(object, d3_geo_area);
+    return d3_geo_areaSum;
+  };
+  var d3_geo_areaSum, d3_geo_areaPolygon;
+  var d3_geo_area = {
+    sphere: function() {
+      d3_geo_areaSum += 4 * π;
     },
-    MultiPolygon: function(o) {
-      return d3.sum(o.coordinates, d3_geo_areaPolygon);
-    },
-    Sphere: function() {
-      return 4 * π;
-    },
-    Feature: function(o) {
-      return this.geometry(o.geometry);
-    },
-    FeatureCollection: function(o) {
-      var that = this;
-      return d3.sum(o.features, function(o) {
-        return that.Feature(o);
-      });
-    },
-    GeometryCollection: function(o) {
-      var that = this;
-      return d3.sum(o.geometries, function(o) {
-        return that.geometry(o);
-      });
-    }
-  });
-  function d3_geo_areaPolygon(polygon) {
-    var area = d3.sum(polygon, d3_geo_areaPolygonRing);
-    return area < 0 ? 4 * π + area : area;
+    point: d3_noop,
+    lineStart: d3_noop,
+    lineEnd: d3_noop,
+    polygonStart: d3_geo_areaPolygonStart,
+    polygonEnd: d3_geo_areaPolygonEnd
+  };
+  function d3_geo_areaPolygonStart() {
+    d3_geo_areaPolygon = 0;
+    d3_geo_area.lineStart = d3_geo_areaPolygonLineStart;
   }
-  function d3_geo_areaPolygonRing(ring) {
-    return d3_geo_areaRing(ring.map(d3_geo_areaRadians));
+  function d3_geo_areaPolygonEnd() {
+    d3_geo_areaSum += d3_geo_areaPolygon < 0 ? 4 * π + d3_geo_areaPolygon : d3_geo_areaPolygon;
+    d3_geo_area.point = d3_noop;
   }
-  function d3_geo_areaRadians(point) {
-    return [ point[0] * d3_radians, point[1] * d3_radians ];
-  }
-  function d3_geo_areaRing(ring) {
-    if (!(n = ring.length)) return 0;
-    var n, p = ring[0], λ00 = p[0], λ0 = λ00, λ, φ00 = p[1], φ0 = φ00, φ, dλ, cosdλ, cosφ0 = Math.cos(φ0), sinφ0 = Math.sin(φ0), cosφ, sinφ, d, s, area = 0;
-    for (var i = 1; i < n; ++i) {
-      p = ring[i];
-      φ = p[1];
-      if (Math.abs(Math.abs(φ0) - π / 2) < ε && Math.abs(Math.abs(φ) - π / 2) < ε) continue;
-      λ = p[0];
-      cosφ = Math.cos(φ);
-      sinφ = Math.sin(φ);
-      if (Math.abs(φ0 - π / 2) < ε) {
-        area += (λ - λ00) * 2;
-      } else {
-        cosdλ = Math.cos(dλ = λ - λ0);
-        d = Math.atan2(Math.sqrt((d = cosφ * Math.sin(λ - λ0)) * d + (d = cosφ0 * sinφ - sinφ0 * cosφ * cosdλ) * d), sinφ0 * sinφ + cosφ0 * cosφ * cosdλ);
-        s = (d + π + φ0 + φ) / 4;
-        area += (dλ < 0 && dλ > -π || dλ > π ? -4 : 4) * Math.atan(Math.sqrt(Math.abs(Math.tan(s) * Math.tan(s - d / 2) * Math.tan(s - π / 4 - φ0 / 2) * Math.tan(s - π / 4 - φ / 2))));
+  function d3_geo_areaPolygonLineStart() {
+    var λ00, φ00, λ0, φ0, cosφ0, sinφ0;
+    d3_geo_area.point = function(λ, φ) {
+      λ00 = λ0 = λ * d3_radians, φ00 = φ0 = φ * d3_radians, cosφ0 = Math.cos(φ0), sinφ0 = Math.sin(φ0);
+      d3_geo_area.point = nextPoint;
+    };
+    function nextPoint(λ, φ) {
+      λ *= d3_radians, φ *= d3_radians;
+      if (Math.abs(Math.abs(φ0) - π / 2) < ε && Math.abs(Math.abs(φ) - π / 2) < ε) return;
+      var cosφ = Math.cos(φ), sinφ = Math.sin(φ);
+      if (Math.abs(φ0 - π / 2) < ε) d3_geo_areaPolygon += (λ - λ00) * 2; else {
+        var dλ = λ - λ0, cosdλ = Math.cos(dλ), d = Math.atan2(Math.sqrt((d = cosφ * Math.sin(λ - λ0)) * d + (d = cosφ0 * sinφ - sinφ0 * cosφ * cosdλ) * d), sinφ0 * sinφ + cosφ0 * cosφ * cosdλ), s = (d + π + φ0 + φ) / 4;
+        d3_geo_areaPolygon += (dλ < 0 && dλ > -π || dλ > π ? -4 : 4) * Math.atan(Math.sqrt(Math.abs(Math.tan(s) * Math.tan(s - d / 2) * Math.tan(s - π / 4 - φ0 / 2) * Math.tan(s - π / 4 - φ / 2))));
       }
-      λ00 = λ0, φ00 = φ0;
-      λ0 = λ, φ0 = φ;
-      cosφ0 = cosφ, sinφ0 = sinφ;
+      λ00 = λ0, φ00 = φ0, λ0 = λ, φ0 = φ, cosφ0 = cosφ, sinφ0 = sinφ;
     }
-    return area;
   }
   d3.geo.projection = d3_geo_projection;
   d3.geo.projectionMutator = d3_geo_projectionMutator;
