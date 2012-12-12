6c68339cb2a6caca36ed42ab17eb950424d48bac
Mike Bostock <mbostock@gmail.com>
2012-12-12 14:59:51 -0800

    Rename & inline a bit.

--- d3.js
+++ d3.js
@@ -6455,7 +6455,7 @@
     d3.geo.stream(object, d3_geo_area);
     return d3_geo_areaSum;
   };
-  var d3_geo_areaSum, d3_geo_areaPolygon;
+  var d3_geo_areaSum, d3_geo_areaRing;
   var d3_geo_area = {
     sphere: function() {
       d3_geo_areaSum += 4 * π;
@@ -6463,18 +6463,16 @@
     point: d3_noop,
     lineStart: d3_noop,
     lineEnd: d3_noop,
-    polygonStart: d3_geo_areaPolygonStart,
-    polygonEnd: d3_geo_areaPolygonEnd
+    polygonStart: function() {
+      d3_geo_areaRing = 0;
+      d3_geo_area.lineStart = d3_geo_areaRingStart;
+    },
+    polygonEnd: function() {
+      d3_geo_areaSum += d3_geo_areaRing < 0 ? 4 * π + d3_geo_areaRing : d3_geo_areaRing;
+      d3_geo_area.point = d3_noop;
+    }
   };
-  function d3_geo_areaPolygonStart() {
-    d3_geo_areaPolygon = 0;
-    d3_geo_area.lineStart = d3_geo_areaPolygonLineStart;
-  }
-  function d3_geo_areaPolygonEnd() {
-    d3_geo_areaSum += d3_geo_areaPolygon < 0 ? 4 * π + d3_geo_areaPolygon : d3_geo_areaPolygon;
-    d3_geo_area.point = d3_noop;
-  }
-  function d3_geo_areaPolygonLineStart() {
+  function d3_geo_areaRingStart() {
     var λ00, φ00, λ0, φ0, cosφ0, sinφ0;
     d3_geo_area.point = function(λ, φ) {
       λ00 = λ0 = λ * d3_radians, φ00 = φ0 = φ * d3_radians, cosφ0 = Math.cos(φ0), sinφ0 = Math.sin(φ0);
@@ -6484,9 +6482,9 @@
       λ *= d3_radians, φ *= d3_radians;
       if (Math.abs(Math.abs(φ0) - π / 2) < ε && Math.abs(Math.abs(φ) - π / 2) < ε) return;
       var cosφ = Math.cos(φ), sinφ = Math.sin(φ);
-      if (Math.abs(φ0 - π / 2) < ε) d3_geo_areaPolygon += (λ - λ00) * 2; else {
+      if (Math.abs(φ0 - π / 2) < ε) d3_geo_areaRing += (λ - λ00) * 2; else {
         var dλ = λ - λ0, cosdλ = Math.cos(dλ), d = Math.atan2(Math.sqrt((d = cosφ * Math.sin(λ - λ0)) * d + (d = cosφ0 * sinφ - sinφ0 * cosφ * cosdλ) * d), sinφ0 * sinφ + cosφ0 * cosφ * cosdλ), s = (d + π + φ0 + φ) / 4;
-        d3_geo_areaPolygon += (dλ < 0 && dλ > -π || dλ > π ? -4 : 4) * Math.atan(Math.sqrt(Math.abs(Math.tan(s) * Math.tan(s - d / 2) * Math.tan(s - π / 4 - φ0 / 2) * Math.tan(s - π / 4 - φ / 2))));
+        d3_geo_areaRing += (dλ < 0 && dλ > -π || dλ > π ? -4 : 4) * Math.atan(Math.sqrt(Math.abs(Math.tan(s) * Math.tan(s - d / 2) * Math.tan(s - π / 4 - φ0 / 2) * Math.tan(s - π / 4 - φ / 2))));
       }
       λ00 = λ0, φ00 = φ0, λ0 = λ, φ0 = φ, cosφ0 = cosφ, sinφ0 = sinφ;
     }
--- src/geo/area.js
+++ src/geo/area.js
@@ -5,28 +5,26 @@ d3.geo.area = function(object) {
 };
 
 var d3_geo_areaSum,
-    d3_geo_areaPolygon;
+    d3_geo_areaRing;
 
 var d3_geo_area = {
   sphere: function() { d3_geo_areaSum += 4 * π; },
   point: d3_noop,
   lineStart: d3_noop,
   lineEnd: d3_noop,
-  polygonStart: d3_geo_areaPolygonStart,
-  polygonEnd: d3_geo_areaPolygonEnd
+
+  // Only count area for polygon rings.
+  polygonStart: function() {
+    d3_geo_areaRing = 0;
+    d3_geo_area.lineStart = d3_geo_areaRingStart;
+  },
+  polygonEnd: function() {
+    d3_geo_areaSum += d3_geo_areaRing < 0 ? 4 * π + d3_geo_areaRing : d3_geo_areaRing;
+    d3_geo_area.point = d3_noop;
+  }
 };
 
-function d3_geo_areaPolygonStart() {
-  d3_geo_areaPolygon = 0;
-  d3_geo_area.lineStart = d3_geo_areaPolygonLineStart;
-}
-
-function d3_geo_areaPolygonEnd() {
-  d3_geo_areaSum += d3_geo_areaPolygon < 0 ? 4 * π + d3_geo_areaPolygon : d3_geo_areaPolygon;
-  d3_geo_area.point = d3_noop;
-}
-
-function d3_geo_areaPolygonLineStart() {
+function d3_geo_areaRingStart() {
   var λ00, φ00, λ0,  φ0, cosφ0, sinφ0; // two previous points
 
   // For the first point, …
@@ -44,7 +42,7 @@ function d3_geo_areaPolygonLineStart() {
     var cosφ = Math.cos(φ), sinφ = Math.sin(φ);
 
     // If the previous point is at the south pole, something special…
-    if (Math.abs(φ0 - π / 2) < ε) d3_geo_areaPolygon += (λ - λ00) * 2;
+    if (Math.abs(φ0 - π / 2) < ε) d3_geo_areaRing += (λ - λ00) * 2;
 
     // TODO Explain this wonderous mathematics.
     else {
@@ -52,7 +50,7 @@ function d3_geo_areaPolygonLineStart() {
           cosdλ = Math.cos(dλ),
           d = Math.atan2(Math.sqrt((d = cosφ * Math.sin(λ - λ0)) * d + (d = cosφ0 * sinφ - sinφ0 * cosφ * cosdλ) * d), sinφ0 * sinφ + cosφ0 * cosφ * cosdλ),
           s = (d + π + φ0 + φ) / 4;
-      d3_geo_areaPolygon += (dλ < 0 && dλ > -π || dλ > π ? -4 : 4) * Math.atan(Math.sqrt(Math.abs(Math.tan(s) * Math.tan(s - d / 2) * Math.tan(s - π / 4 - φ0 / 2) * Math.tan(s - π / 4 - φ / 2))));
+      d3_geo_areaRing += (dλ < 0 && dλ > -π || dλ > π ? -4 : 4) * Math.atan(Math.sqrt(Math.abs(Math.tan(s) * Math.tan(s - d / 2) * Math.tan(s - π / 4 - φ0 / 2) * Math.tan(s - π / 4 - φ / 2))));
     }
 
     // Advance the previous points.
