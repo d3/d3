9cc79b4affda49db84accbf2a8bd9a10cd3a2fa6
Mike Bostock <mbostock@gmail.com>
2012-12-15 13:02:08 -0800

    Delete unused code.

--- src/geo/stream-radians.js
+++
@@ -1,5 +0,0 @@
-function d3_geo_streamRadians(stream) {
-  return d3_geo_streamTransform(stream, function(x, y) {
-    stream.point(x * d3_radians, y * d3_radians);
-  });
-}
--- src/geo/stream-rotate.js
+++
@@ -1,6 +0,0 @@
-function d3_geo_streamRotate(rotate, stream) {
-  return d3_geo_streamTransform(stream, function(x, y) {
-    x = rotate(x, y);
-    stream.point(x[0], x[1]);
-  });
-}
