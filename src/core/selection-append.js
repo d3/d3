0f65d3c93e5eff9f31323b8b7f1472d5b27b1c7f
Nicholas Rushton <nick.rushton@hotmail.com>
2013-01-26 20:52:14 -0800

    shortening implementation of selection append

--- src/core/selection-append.js
+++ src/core/selection-append.js
@@ -1,22 +1,15 @@
 // TODO append(node)?
-// TODO append(function)?
 d3_selectionPrototype.append = function(name) {
   return this.select(d3_selection_append(name));
 };
 
 function d3_selection_append(name){
   function append() {
-    var n = name.local ? name : d3.ns.qualify(name);
+    var n = typeof name === 'function' ? name.apply(this,arguments) : name;
+    n = n.local ? n : d3.ns.qualify(n);
     return n.local ? this.appendChild(document.createElementNS(n.space, n.local)) :
-                        this.appendChild(document.createElementNS(this.namespaceURI, n));
+                     this.appendChild(document.createElementNS(this.namespaceURI, n));
   }
 
-  function appendFunction() {
-     var n = name.apply(this,arguments);
-     n = n.local ? n : d3.ns.qualify(n);
-     return name.local ? this.appendChild(document.createElementNS(n.space, n.local)) :
-                         this.appendChild(document.createElementNS(this.namespaceURI, n));
-  }
-
-  return typeof name === "string" ? append : appendFunction;
+  return append;
 }
