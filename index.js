6d23240a72fbd30a1a61bd9449238684ff0237da
Kristofor Carle <kristofor.carle@syncadd.com>
2013-01-31 15:16:06 -0500

    Fixes issue where jsdom globals are deleted when loading as NodeJS module

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var globals = ["document", "window", "navigator", "CSSStyleDeclaration", "getComputedStyle", "d3"],
+var globals = ["document", "window", "navigator", "CSSStyleDeclaration", "getComputedStyle"],
     globalValues = {};
 
 globals.forEach(function(g) {
-  if (g in global) globalValues[g] = global[g];
+    if (g in global) {
+		console.log("Warning: found pre-existing global: " + g);
+		globalValues[g] = global[g];
+    }
 });
 
 require("./globals");
@@ -11,6 +14,6 @@ require("./d3");
 module.exports = d3;
 
 globals.forEach(function(g) {
+  //restore pre-existing globals
   if (g in globalValues) global[g] = globalValues[g];
-  else delete global[g];
 });
