5c9b90ce078c8035de3385f2d74ed0fd4f3cd188
Jason Davies <jason@jasondavies.com>
2013-01-31 13:23:54 +0000

    d3.geo.distance: accuracy for small distances.

--- test/geo/distance-test.js
+++ test/geo/distance-test.js
@@ -10,6 +10,9 @@ suite.addBatch({
     "computes the great-arc distance": function() {
       assert.equal(d3.geo.distance([0, 0], [0, 0]), 0);
       assert.inDelta(d3.geo.distance([118 + 24 / 60, 33 + 57 / 60], [ 73 + 47 / 60, 40 + 38 / 60]), 3973 / 6371, .5);
+    },
+    "small distance": function() {
+      assert.isTrue(d3.geo.distance([0, 0], [0, 1e-12]) > 0);
     }
   }
 });
