50ab6c8792b68f8b955084f3704e1cf27a0fd7f6
Jason Davies <jason@jasondavies.com>
2013-02-01 13:30:20 +0000

    Fix rounded formatting for 9×10ⁿ.
    Fixes #1042.

--- test/core/format-test.js
+++ test/core/format-test.js
@@ -139,6 +139,8 @@ suite.addBatch({
       assert.strictEqual(format(".4r")(123.45), "123.5");
       assert.strictEqual(format(".5r")(123.45), "123.45");
       assert.strictEqual(format(".6r")(123.45), "123.450");
+      assert.strictEqual(format(".1r")(.9), "0.9");
+      assert.strictEqual(format(".1r")(.09), "0.09");
     },
     "can round very small numbers": function(format) {
       var f = format(".2r");
