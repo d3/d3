2a0e2a12667b85279cfeaa653cc376136a5202e0
Mike Bostock <mbostock@gmail.com>
2013-02-06 10:18:52 -0800

    Sort null nodes at the end.
    Previously, null nodes were passed to the comparator and were indistinguishable
    from non-null nodes with no bound data. For consistency with other selection
    operators that skip null nodes, it seems preferrable to put null nodes at the
    end of the selection rather than passing them to the comparator. Fixes #881.

--- src/core/selection-sort.js
+++ src/core/selection-sort.js
@@ -7,6 +7,6 @@ d3_selectionPrototype.sort = function(comparator) {
 function d3_selection_sortComparator(comparator) {
   if (!arguments.length) comparator = d3.ascending;
   return function(a, b) {
-    return comparator(a && a.__data__, b && b.__data__);
+    return (!a - !b) || comparator(a.__data__, b.__data__);
   };
 }
