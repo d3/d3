4571fb785baf4228bb2043905f5d7ed228cbf92c
Jason Davies <jason@jasondavies.com>
2013-03-04 23:20:44 +0000

    Add Liang–Barsky line clipping for viewport.

--- src/geo/clip-view.js
+++ src/geo/clip-view.js
@@ -1,5 +1,8 @@
-d3.geo.clipView = function d3_geo_clipView(coordinates) {
-  var clipPolygon = d3.geom.polygon(coordinates).clip;
+d3.geo.clipView = function d3_geo_clipView(x0, y0, x1, y1) {
+
+  // TODO optimise polygon clipping.
+  var clipPolygon = d3.geom.polygon([[x0, y0], [x0, y1], [x1, y1], [x1, y0]]).clip,
+      clipLine = d3_geo_clipViewLine(x0, x1, y0, y1);
 
   return function(listener) {
     var clip = {
@@ -21,22 +24,11 @@ d3.geo.clipView = function d3_geo_clipView(coordinates) {
     };
 
     function visible(x, y) {
-      var p = [x, y],
-          i = -1,
-          n = coordinates.length,
-          a = coordinates[n - 1];
-      while (++i < n) {
-        b = coordinates[i];
-        if (!d3_geom_polygonInside(p, a, b)) return false;
-        a = b;
-      }
-      return true;
+      return x0 <= x && x <= x1 && y0 <= y && y <= y1;
     }
 
     function point(x, y) {
-      if (visible(x, y)) {
-        listener.point(x, y);
-      }
+      if (visible(x, y)) listener.point(x, y);
     }
 
     var x0, y0, v0, first;
@@ -54,16 +46,25 @@ d3.geo.clipView = function d3_geo_clipView(coordinates) {
 
     function linePoint(x, y) {
       var v = visible(x, y);
-      if (v) {
-        if (first) listener.lineStart();
-        else if (v !== v0) {
+      if (first) {
+        if (v) {
           listener.lineStart();
-          // TODO intersect
+          listener.point(x, y);
         }
-        listener.point(x, y);
-      } else if (v0) {
-        // TODO intersect
-        listener.lineEnd();
+      } else {
+        if (v && v0) listener.point(x, y);
+        else {
+          var a = [x0, y0],
+              b = [x, y];
+          if (clipLine(a, b)) {
+            if (!v0) {
+              listener.lineStart();
+              listener.point(a[0], a[1]);
+            }
+            listener.point(b[0], b[1]);
+            if (!v) listener.lineEnd();
+          }
+        }
       }
       first = false;
       x0 = x, y0 = y, v0 = v;
@@ -95,4 +96,48 @@ d3.geo.clipView = function d3_geo_clipView(coordinates) {
 
     return clip;
   };
+}
+
+// Liang–Barsky line clipping.
+function d3_geo_clipViewLine(xMin, xMax, yMin, yMax) {
+  return function(a, b) {
+    var dx = b[0] - a[0],
+        dy = b[1] - a[1],
+        t = [0, 1];
+
+    if (Math.abs(dx) < ε && Math.abs(dy) < ε &&
+        xMin <= a[0] && a[0] <= xMax && yMin <= a[1] && a[1] <= yMax) return;
+
+    if (clipT(xMin - a[0],  dx, t) &&
+        clipT(a[0] - xMax, -dx, t) &&
+        clipT(yMin - a[1],  dy, t) &&
+        clipT(a[1] - yMax, -dy, t)) {
+      if (t[1] < 1) {
+        b[0] = a[0] + t[1] * dx;
+        b[1] = a[1] + t[1] * dy;
+      }
+      if (t[0] > 0) {
+        a[0] += t[0] * dx;
+        a[1] += t[0] * dy;
+      }
+      return true;
+    }
+
+    return false;
+  };
+
+  function clipT(num, denominator, t) {
+    if (Math.abs(denominator) < ε) return num <= 0;
+
+    var u = num / denominator;
+
+    if (denominator > 0) {
+      if (u > t[1]) return false;
+      if (u > t[0]) t[0] = u;
+    } else {
+      if (u < t[0]) return false;
+      if (u < t[1]) t[1] = u;
+    }
+    return true;
+  }
 }
