368f80c215f2a17dfca485f9b20b6a7390162aa3
Mike Bostock <mbostock@gmail.com>
2013-03-07 09:53:00 -0800

    Change path to vows for npm test.

--- src/package.js
+++ src/package.js
@@ -4,10 +4,23 @@ console.log(JSON.stringify({
   "name": "d3",
   "version": d3.version,
   "description": "A small, free JavaScript library for manipulating documents based on data.",
-  "keywords": ["dom", "w3c", "visualization", "svg", "animation", "canvas"],
+  "keywords": [
+    "dom",
+    "w3c",
+    "visualization",
+    "svg",
+    "animation",
+    "canvas"
+  ],
   "homepage": "http://d3js.org",
-  "author": {"name": "Mike Bostock", "url": "http://bost.ocks.org/mike"},
-  "repository": {"type": "git", "url": "https://github.com/mbostock/d3.git"},
+  "author": {
+    "name": "Mike Bostock",
+    "url": "http://bost.ocks.org/mike"
+  },
+  "repository": {
+    "type": "git",
+    "url": "https://github.com/mbostock/d3.git"
+  },
   "main": "index.js",
   "browserify" : "index-browserify.js",
   "jam": {
@@ -23,5 +36,7 @@ console.log(JSON.stringify({
     "uglify-js": "2.2.x",
     "vows": "0.7.x"
   },
-  "scripts": {"test": "./node_modules/vows/bin/vows"}
+  "scripts": {
+    "test": "node_modules/.bin/vows"
+  }
 }, null, 2));
