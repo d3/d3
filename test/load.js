5627ecdb9a357d0182ade3ec0816dd0393470441
Mike Bostock <mbostock@gmail.com>
2013-03-14 08:16:52 -0700

    Allow load expression to be overridden.

--- test/load.js
+++ test/load.js
@@ -2,13 +2,20 @@ var smash = require("smash");
 
 module.exports = function() {
   var files = [].slice.call(arguments).map(function(d) { return "src/" + d; }),
+      expression = "d3",
       sandbox = null;
+
   files.unshift("src/start");
   files.push("src/end");
 
   function topic() {
-    smash.load(files, "d3", sandbox, this.callback);
+    smash.load(files, expression, sandbox, this.callback);
   }
+
+  topic.expression = function(_) {
+    expression = _;
+    return topic;
+  };
 
   topic.sandbox = function(_) {
     sandbox = _;
