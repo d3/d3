dcd784246e74d2a4f1e882e5af9db7a555bf106c
Jason Davies <jason@jasondavies.com>
2013-03-21 12:56:54 +0000

    Add missing semicolons to test.

--- test/geo/bounds-test.js
+++ test/geo/bounds-test.js
@@ -14,7 +14,7 @@ suite.addBatch({
           type: "MultiPoint",
           coordinates: [[-123, 39], [-122, 38]]
         }
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "FeatureCollection": function(bounds) {
       assert.deepEqual(bounds({
@@ -35,7 +35,7 @@ suite.addBatch({
             }
           }
         ]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "GeometryCollection": function(bounds) {
       assert.deepEqual(bounds({
@@ -50,46 +50,46 @@ suite.addBatch({
             coordinates: [-122, 38]
           }
         ]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "LineString": function(bounds) {
       assert.deepEqual(bounds({
         type: "LineString",
         coordinates: [[-123, 39], [-122, 38]]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "MultiLineString": function(bounds) {
       assert.deepEqual(bounds({
         type: "MultiLineString",
         coordinates: [[[-123, 39], [-122, 38]]]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "MultiPoint": function(bounds) {
       assert.deepEqual(bounds({
         type: "MultiPoint",
         coordinates: [[-123, 39], [-122, 38]]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "MultiPolygon": function(bounds) {
       assert.deepEqual(bounds({
         type: "MultiPolygon",
         coordinates: [[[[-123, 39], [-122, 39], [-122, 38], [-123, 39]], [[10, 20], [20, 20], [20, 10], [10, 10], [10, 20]]]]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "Point": function(bounds) {
       assert.deepEqual(bounds({
         type: "Point",
         coordinates: [-123, 39]
-      }), [[-123, 39], [-123, 39]])
+      }), [[-123, 39], [-123, 39]]);
     },
     "Polygon": function(bounds) {
       assert.deepEqual(bounds({
         type: "Polygon",
         coordinates: [[[-123, 39], [-122, 39], [-122, 38], [-123, 39]], [[10, 20], [20, 20], [20, 10], [10, 10], [10, 20]]]
-      }), [[-123, 38], [-122, 39]])
+      }), [[-123, 38], [-122, 39]]);
     },
     "NestedCollection": function(bounds) {
-        assert.deepEqual(bounds({
+      assert.deepEqual(bounds({
         type: "FeatureCollection",
         features: [
           {
@@ -109,7 +109,7 @@ suite.addBatch({
             }
           }
         ]
-      }), [[-120,46], [-119,47]])
+      }), [[-120,46], [-119,47]]);
     }
   }
 });
