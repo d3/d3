359546c80c2a07edd8b526b2956091eb35119767
Mike Bostock <mbostock@gmail.com>
2013-03-31 09:46:07 -0700

    More robust type inference for d3.interpolate.
    Fixes #1180 #1179 #1176. Still a work in progress; needs a few more tests.

--- test/assert.js
+++ test/assert.js
@@ -2,6 +2,12 @@ var assert = require("assert");
 
 assert = module.exports = Object.create(assert);
 
+assert.isArray = function(actual, message) {
+  if (!Array.isArray(actual)) {
+    assert.fail(actual, null, message || "expected {actual} to be an Array", null, assert.isArray);
+  }
+};
+
 assert.inDelta = function(actual, expected, delta, message) {
   if (!inDelta(actual, expected, delta)) {
     assert.fail(actual, expected, message || "expected {actual} to be in within *" + delta + "* of {expected}", null, assert.inDelta);
