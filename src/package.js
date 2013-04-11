32e77ed975d4813449dead05cf9bd285389a79c2
Chia-liang Kao <clkao@clkao.org>
2013-04-11 22:20:30 +0800

    Exit with what vows exits.

--- src/package.js
+++ src/package.js
@@ -44,7 +44,7 @@ console.log(JSON.stringify({
     "vows": "0.7.x"
   },
   "scripts": {
-    "test": "node_modules/.bin/vows; echo"
+    "test": "node_modules/.bin/vows"
   },
   "licenses": [
     {
