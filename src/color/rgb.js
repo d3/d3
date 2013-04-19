ff5364d7dbd1aca4fff3ae30cdd3e9037786c97d
Mike Bostock <mbostock@gmail.com>
2013-04-19 10:20:21 -0700

    Allow hue, chroma or saturation to be undefined.
    In HSL space, grayscale colors can now have undefined hue rather than assuming a
    hue of 0°; likewise black and white can have undefined saturation rather than
    assuming 0%. In HCL space, black can now have undefined hue and chroma. (For
    non-black grayscale colors, including white, hue and chroma are implied by the
    D65 standard referent.)
    
    When interpolating between colors with undefined hue, saturation or chroma, the
    defined value is used when available. For example, when interpolating from black
    to blue in HCL space, the intermediate colors are now dark blues (#241178)
    rather than dark purples (#600054).
    
    Fixes #833.

--- src/color/rgb.js
+++ src/color/rgb.js
@@ -117,8 +117,8 @@ function d3_rgb_hsl(r, g, b) {
   var min = Math.min(r /= 255, g /= 255, b /= 255),
       max = Math.max(r, g, b),
       d = max - min,
-      h,
-      s,
+      h = NaN,
+      s = h,
       l = (max + min) / 2;
   if (d) {
     s = l < .5 ? d / (max + min) : d / (2 - max - min);
@@ -126,8 +126,6 @@ function d3_rgb_hsl(r, g, b) {
     else if (g == max) h = (b - r) / d + 2;
     else h = (r - g) / d + 4;
     h *= 60;
-  } else {
-    s = h = 0;
   }
   return d3_hsl(h, s, l);
 }
