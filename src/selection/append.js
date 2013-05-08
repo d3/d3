9c45565afe31d8fafcbbd983b58baa914897be50
jdgarcia <dgarcia@plainconcepts.com>
2013-05-08 14:41:27 +0200

    add script to add overflow:hidden when insert svg element

--- src/selection/append.js
+++ src/selection/append.js
@@ -8,11 +8,19 @@ d3_selectionPrototype.append = function(name) {
   name = d3.ns.qualify(name);
 
   function append() {
-    return this.appendChild(d3_document.createElementNS(this.namespaceURI, name));
+    var element = d3_document.createElementNS(this.namespaceURI, name);
+      if(name.local === "svg"){
+        element.style.overflow = "hidden";
+      }
+      return this.appendChild(element);
   }
 
   function appendNS() {
-    return this.appendChild(d3_document.createElementNS(name.space, name.local));
+    var element = d3_document.createElementNS(name.space, name.local);
+      if(name.local === "svg"){
+        element.style.overflow = "hidden";
+      }
+      return this.appendChild(element);
   }
 
   return this.select(name.local ? appendNS : append);
--- src/selection/insert.js
+++ src/selection/insert.js
@@ -8,15 +8,19 @@ d3_selectionPrototype.insert = function(name, before) {
   if (typeof before !== "function") before = d3_selection_selector(before);
 
   function insert(d, i) {
-    return this.insertBefore(
-        d3_document.createElementNS(this.namespaceURI, name),
-        before.call(this, d, i));
+    var element = d3_document.createElementNS(this.namespaceURI, name);
+      if(name.local === "svg"){
+        element.style.overflow = "hidden";
+      }
+      return this.insertBefore(element, before.call(this, d, i));
   }
 
   function insertNS(d, i) {
-    return this.insertBefore(
-        d3_document.createElementNS(name.space, name.local),
-        before.call(this, d, i));
+    var element = d3_document.createElementNS(name.space, name.local);
+      if(name.local === "svg"){
+        element.style.overflow = "hidden";
+      }
+      return this.insertBefore(element, before.call(this, d, i));
   }
 
   return this.select(name.local ? insertNS : insert);
