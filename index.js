4a37499a4deb0c5bc4dd7ad6d1930c4a17cbfd9e
Young Kim <me@youngfuturist.net>
2013-05-09 09:44:29 -0700

    now require('d3') will make global window and document objects

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var globals = ["document", "window", "d3"],
     globalValues = {};
 
+require("./globals");
+require("./d3");
+
 globals.forEach(function(g) {
   if (g in global) globalValues[g] = global[g];
 });
-
-require("./globals");
-require("./d3");
 
 module.exports = d3;
 
 globals.forEach(function(g) {
   if (g in globalValues) global[g] = globalValues[g];
-  else delete global[g];
+  else delete global[g]
 });
