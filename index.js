9f9bb5486a4267e87d2568fa7c2c48d0bbe3cbf0
Young Kim <me@youngfuturist.net>
2013-05-09 10:43:50 -0700

    removing pointless forEach statements

--- index.js
+++ index.js
@@ -4,13 +4,4 @@ var globals = ["document", "window", "d3"],
 require("./globals");
 require("./d3");
 
-globals.forEach(function(g) {
-  if (g in global) globalValues[g] = global[g];
-});
-
 module.exports = d3;
-
-globals.forEach(function(g) {
-  if (g in globalValues) global[g] = globalValues[g];
-  else delete global[g];
-});
