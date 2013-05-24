9d7e9f07e912f8cdc3a2c4432857f10c448de3d2
Jason Davies <jason@jasondavies.com>
2013-05-24 18:59:50 +0100

    Fix dangling global in projection.clipExtent.

--- d3.js
+++ d3.js
@@ -2777,7 +2777,7 @@ d3 = function() {
       function insidePolygon(p) {
         var wn = 0, n = polygon.length, y = p[1];
         for (var i = 0; i < n; ++i) {
-          for (var j = 1, v = polygon[i], m = v.length, a = v[0]; j < m; ++j) {
+          for (var j = 1, v = polygon[i], m = v.length, a = v[0], b; j < m; ++j) {
             b = v[j];
             if (a[1] <= y) {
               if (b[1] > y && isLeft(a, b, p) > 0) ++wn;
