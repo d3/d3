be2c8e74815c7926ffe9642c6a16abc2832e547a
Jason Davies <jason@jasondavies.com>
2013-06-08 17:12:35 +0100

    Optimise d3.geo.resample.
    This avoids recreating closures for handling polygon rings, which was
    causing the V8 JIT to recompile them.

--- d3.js
+++ d3.js
@@ -3242,14 +3242,14 @@ d3 = function() {
   function d3_geo_resample(project) {
     var δ2 = .5, maxDepth = 16;
     function resample(stream) {
-      var λ0, x0, y0, a0, b0, c0;
+      var λ00, φ00, x00, y00, a00, b00, c00, λ0, x0, y0, a0, b0, c0;
       var resample = {
         point: point,
         lineStart: lineStart,
         lineEnd: lineEnd,
         polygonStart: function() {
           stream.polygonStart();
-          resample.lineStart = polygonLineStart;
+          resample.lineStart = ringStart;
         },
         polygonEnd: function() {
           stream.polygonEnd();
@@ -3275,20 +3275,21 @@ d3 = function() {
         resample.point = point;
         stream.lineEnd();
       }
-      function polygonLineStart() {
-        var λ00, φ00, x00, y00, a00, b00, c00;
+      function ringStart() {
         lineStart();
-        resample.point = function(λ, φ) {
-          linePoint(λ00 = λ, φ00 = φ), x00 = x0, y00 = y0, a00 = a0, b00 = b0, c00 = c0;
-          resample.point = linePoint;
-        };
-        resample.lineEnd = function() {
-          var buffer = [];
-          resampleLineTo(x0, y0, λ0, a0, b0, c0, x00, y00, λ00, a00, b00, c00, maxDepth, buffer);
-          streamLine(buffer, stream);
-          resample.lineEnd = lineEnd;
-          lineEnd();
-        };
+        resample.point = ringPoint;
+        resample.lineEnd = ringEnd;
+      }
+      function ringPoint(λ, φ) {
+        linePoint(λ00 = λ, φ00 = φ), x00 = x0, y00 = y0, a00 = a0, b00 = b0, c00 = c0;
+        resample.point = linePoint;
+      }
+      function ringEnd() {
+        var buffer = [];
+        resampleLineTo(x0, y0, λ0, a0, b0, c0, x00, y00, λ00, a00, b00, c00, maxDepth, buffer);
+        streamLine(buffer, stream);
+        resample.lineEnd = lineEnd;
+        lineEnd();
       }
       function streamLine(line, stream) {
         for (var i = 0, n = line.length, point; i < n; ++i) {
--- src/geo/resample.js
+++ src/geo/resample.js
@@ -6,13 +6,14 @@ function d3_geo_resample(project) {
       maxDepth = 16;
 
   function resample(stream) {
-    var λ0, x0, y0, a0, b0, c0; // previous point
+    var λ00, φ00, x00, y00, a00, b00, c00, // first point
+        λ0, x0, y0, a0, b0, c0; // previous point
 
     var resample = {
       point: point,
       lineStart: lineStart,
       lineEnd: lineEnd,
-      polygonStart: function() { stream.polygonStart(); resample.lineStart = polygonLineStart; },
+      polygonStart: function() { stream.polygonStart(); resample.lineStart = ringStart; },
       polygonEnd: function() { stream.polygonEnd(); resample.lineStart = lineStart; }
     };
 
@@ -39,23 +40,23 @@ function d3_geo_resample(project) {
       stream.lineEnd();
     }
 
-    function polygonLineStart() {
-      var λ00, φ00, x00, y00, a00, b00, c00; // first point
-
+    function ringStart() {
       lineStart();
+      resample.point = ringPoint;
+      resample.lineEnd = ringEnd;
+    }
 
-      resample.point = function(λ, φ) {
-        linePoint(λ00 = λ, φ00 = φ), x00 = x0, y00 = y0, a00 = a0, b00 = b0, c00 = c0;
-        resample.point = linePoint;
-      };
+    function ringPoint(λ, φ) {
+      linePoint(λ00 = λ, φ00 = φ), x00 = x0, y00 = y0, a00 = a0, b00 = b0, c00 = c0;
+      resample.point = linePoint;
+    }
 
-      resample.lineEnd = function() {
-        var buffer = [];
-        resampleLineTo(x0, y0, λ0, a0, b0, c0, x00, y00, λ00, a00, b00, c00, maxDepth, buffer);
-        streamLine(buffer, stream);
-        resample.lineEnd = lineEnd;
-        lineEnd();
-      };
+    function ringEnd() {
+      var buffer = [];
+      resampleLineTo(x0, y0, λ0, a0, b0, c0, x00, y00, λ00, a00, b00, c00, maxDepth, buffer);
+      streamLine(buffer, stream);
+      resample.lineEnd = lineEnd;
+      lineEnd();
     }
 
     function streamLine(line, stream) {
