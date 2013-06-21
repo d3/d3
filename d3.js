6d650d0e9c1f8a8d936268d18db8f623bfb89606
Mike Bostock <mbostock@gmail.com>
2013-06-21 08:44:08 -0700

    Much larger minimum sample distance.

--- d3.js
+++ d3.js
@@ -3368,7 +3368,7 @@ d3 = function() {
       var dx = x1 - x0, dy = y1 - y0, d2 = dx * dx + dy * dy;
       if (d2 > 4 * δ2 && depth--) {
         var a = a0 + a1, b = b0 + b1, c = c0 + c1, m = Math.sqrt(a * a + b * b + c * c), φ2 = Math.asin(c /= m), λ2 = Math.abs(Math.abs(c) - 1) < ε ? (λ0 + λ1) / 2 : Math.atan2(b, a), p = project(λ2, φ2), x2 = p[0], y2 = p[1], dx2 = x2 - x0, dy2 = y2 - y0, dz = dy * dx2 - dx * dy2, tooFar;
-        if (dz * dz / d2 > δ2 || (tooFar = d2 > 512 * δ2)) {
+        if (dz * dz / d2 > δ2 || (tooFar = d2 > 16384 * δ2)) {
           var s0 = resampleLineTo(x0, y0, λ0, a0, b0, c0, x2, y2, λ2, a /= m, b /= m, c, depth, buffer);
           buffer.push(p);
           var s1 = resampleLineTo(x2, y2, λ2, a, b, c, x1, y1, λ1, a1, b1, c1, depth, buffer);
