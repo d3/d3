91fb8594006d2014be9273da392e313c787f2570
Mike Bostock <mbostock@gmail.com>
2013-06-27 09:46:35 -0700

    Simplify loading without globals.

--- globals.js
+++
@@ -1,9 +0,0 @@
-var document = global.document = require("jsdom").jsdom("<html><head></head><body></body></html>"),
-    window = global.window = document.createWindow();
-
-// https://github.com/chad3814/CSSStyleDeclaration/issues/3
-var CSSStyleDeclaration_prototype = window.CSSStyleDeclaration.prototype,
-    CSSStyleDeclaration_setProperty = CSSStyleDeclaration_prototype.setProperty;
-CSSStyleDeclaration_prototype.setProperty = function(name, value, priority) {
-  return CSSStyleDeclaration_setProperty.call(this, name + "", value == null ? null : value + "", priority == null ? null : priority + "");
-};
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-var globals = ["document", "window", "d3"],
-    globalValues = {};
+var fs = require("fs"),
+    document = require("jsdom").jsdom("<html><head></head><body></body></html>"),
+    window = document.createWindow();
 
-globals.forEach(function(g) {
-  if (g in global) globalValues[g] = global[g];
-});
+// https://github.com/chad3814/CSSStyleDeclaration/issues/3
+var CSSStyleDeclaration_prototype = window.CSSStyleDeclaration.prototype,
+    CSSStyleDeclaration_setProperty = CSSStyleDeclaration_prototype.setProperty;
+CSSStyleDeclaration_prototype.setProperty = function(name, value, priority) {
+  return CSSStyleDeclaration_setProperty.call(this, name + "", value == null ? null : value + "", priority == null ? null : priority + "");
+};
 
-require("./globals");
-require("./d3");
-
-module.exports = d3;
-
-globals.forEach(function(g) {
-  if (g in globalValues) global[g] = globalValues[g];
-  else delete global[g];
-});
+module.exports = (new Function("window", "document",
+  "return " + fs.readFileSync("./d3.js", "utf-8"))
+)(window, document);
