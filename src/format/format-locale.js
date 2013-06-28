68809d3dd3b8aabb560520307d079686d93096bc
Mike Bostock <mbostock@gmail.com>
2013-06-28 15:15:09 -0700

    Add currency support to d3.format. Fixes #777.
    You can now prefix the locale’s currency symbol. For example:
    
      d3.format("+$,.2f")(-2.5e5) // -$250,000.00
    
    This implementation is limited in that it does not support currencies where the
    symbol should appear after the value (e.g., "250,000.00 €" as in fr_FR). And
    d3.format currently only supports a single locale. But it’s a start.

--- src/format/format-locale.js
+++ src/format/format-locale.js
@@ -1,3 +1,4 @@
 var d3_format_decimalPoint = {decimal_point},
     d3_format_thousandsSeparator = {thousands_sep},
-    d3_format_grouping = {grouping};
+    d3_format_grouping = {grouping},
+    d3_format_currencySymbol = {currency_symbol};
--- test/format/format-test.js
+++ test/format/format-test.js
@@ -99,6 +99,31 @@ suite.addBatch({
       assert.strictEqual(f(999500), "999.5k");
       assert.strictEqual(f(.009995), "9.995m");
     },
+    "can output a currency": function(format) {
+      var f = format("$");
+      assert.strictEqual(f(0), "$0");
+      assert.strictEqual(f(.042), "$0.042");
+      assert.strictEqual(f(.42), "$0.42");
+      assert.strictEqual(f(4.2), "$4.2");
+      assert.strictEqual(f(-.042), "-$0.042");
+      assert.strictEqual(f(-.42), "-$0.42");
+      assert.strictEqual(f(-4.2), "-$4.2");
+    },
+    "can output a currency with comma-grouping and sign": function(format) {
+      var f = format("+$,.2f");
+      assert.strictEqual(f(0), "+$0.00");
+      assert.strictEqual(f(0.429), "+$0.43");
+      assert.strictEqual(f(-0.429), "-$0.43");
+      assert.strictEqual(f(-1), "-$1.00");
+      assert.strictEqual(f(1e4), "+$10,000.00");
+    },
+    "can output a currency with si-prefix notation": function(format) {
+      var f = format("$.2s");
+      assert.strictEqual(f(0), "$0.0");
+      assert.strictEqual(f(2.5e5), "$250k");
+      assert.strictEqual(f(-2.5e8), "-$250M");
+      assert.strictEqual(f(2.5e11), "$250G");
+    },
     "can output a percentage": function(format) {
       var f = format("%");
       assert.strictEqual(f(0), "0%");
