bef1ccb94ada61807f4e423a6ed2168cfebaf821
Mike Bostock <mbostock@gmail.com>
2013-06-30 10:33:17 -0700

    Accept function for selection append and insert.
    Like selection.select, selection.append and selection.insert can now accept a
    function which returns a node. This makes it slightly easier to append or insert
    elements whose name is computed from data, or to append elements that already
    exist (say from an element pool).
    
    There has been much discussion regarding whether the function should return the
    name of the element or the element itself. Returning a name is less work for the
    caller, but only supports creating new elements; returning a name is also more
    consistent with how D3 defines attribute values, but D3 does not allow attribute
    names to be specified as functions. So, it seemed better to opt for consistency
    with selection.select and selection.selectAll, which accept functions that
    return elements, since this is more expressive. Of course, you can still use
    select and selectAll to append elements, but using append to do that directly is
    more intuitive.
    
    Related #4 #311 #724 #732 #734 #961 #1031 #1271.

--- src/selection/insert.js
+++ src/selection/insert.js
@@ -1,23 +1,9 @@
-import "../core/document";
-import "../core/ns";
 import "selection";
 
 d3_selectionPrototype.insert = function(name, before) {
-  name = d3.ns.qualify(name);
-
-  if (typeof before !== "function") before = d3_selection_selector(before);
-
-  function insert(d, i) {
-    return this.insertBefore(
-        d3_document.createElementNS(this.namespaceURI, name),
-        before.call(this, d, i));
-  }
-
-  function insertNS(d, i) {
-    return this.insertBefore(
-        d3_document.createElementNS(name.space, name.local),
-        before.call(this, d, i));
-  }
-
-  return this.select(name.local ? insertNS : insert);
+  name = d3_selection_creator(name);
+  before = d3_selection_selector(before);
+  return this.select(function() {
+    return this.insertBefore(name.apply(this, arguments), before.apply(this, arguments));
+  });
 };
