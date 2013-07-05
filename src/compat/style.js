b49861d387e82aa6c5483f20c0b85e4ee22d33ea
Jason Davies <jason@jasondavies.com>
2013-07-05 23:34:17 +0100

    Add compatibility shim for setAttribute[NS] in IE9.
    Similar to style.setProperty, IE9 does not always properly coerce
    attribute values to strings.  This causes issues when attempting to set
    an instance of d3.rgb as an attribute value.  Now that d3.interpolate
    can return d3.rgb instances, this situation can occur more frequently.
    
    This compatiblity shim overrides Element.prototype.setAttribute[NS] so
    that all attribute values are coerced to strings in IE9.

--- src/compat/style.js
+++ src/compat/style.js
@@ -3,8 +3,17 @@ import "../core/document";
 try {
   d3_document.createElement("div").style.setProperty("opacity", 0, "");
 } catch (error) {
-  var d3_style_prototype = d3_window.CSSStyleDeclaration.prototype,
+  var d3_element_prototype = d3_window.Element.prototype,
+      d3_element_setAttribute = d3_element_prototype.setAttribute,
+      d3_element_setAttributeNS = d3_element_prototype.setAttributeNS,
+      d3_style_prototype = d3_window.CSSStyleDeclaration.prototype,
       d3_style_setProperty = d3_style_prototype.setProperty;
+  d3_element_prototype.setAttribute = function(name, value) {
+    d3_element_setAttribute.call(this, name, value + "");
+  };
+  d3_element_prototype.setAttributeNS = function(space, local, value) {
+    d3_element_setAttributeNS.call(this, space, local, value + "");
+  };
   d3_style_prototype.setProperty = function(name, value, priority) {
     d3_style_setProperty.call(this, name, value + "", priority);
   };
