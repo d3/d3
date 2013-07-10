88ae6c6a659582ad48fee4c8c6eb010aa04d4271
Mike Bostock <mbostock@gmail.com>
2013-07-10 09:02:10 -0700

    Fix #1373 - suppress touchstart on drag.
    This is not ideal because it disables clicking on draggable links, but as we are
    forced to suppress the touchstart event (before any movement) in order to
    disable touch-to-drag or pinch-to-zoom, I think this is the only option. Or, we
    might dispatch a synthetic click event if the touchstart is prevented.

--- d3.js
+++ d3.js
@@ -1038,10 +1038,10 @@ d3 = function() {
   }
   var d3_event_dragSelect = d3_vendorSymbol(d3_documentElement.style, "userSelect");
   function d3_event_dragSuppress(type) {
-    var selectstart = "selectstart." + type, dragstart = "dragstart." + type, click = "click." + type, w = d3.select(d3_window).on(selectstart, d3_eventPreventDefault).on(dragstart, d3_eventPreventDefault), style = d3_documentElement.style, select = style[d3_event_dragSelect];
+    var touchstart = "touchstart." + type, selectstart = "selectstart." + type, dragstart = "dragstart." + type, click = "click." + type, w = d3.select(d3_window).on(touchstart, d3_eventPreventDefault).on(selectstart, d3_eventPreventDefault).on(dragstart, d3_eventPreventDefault), style = d3_documentElement.style, select = style[d3_event_dragSelect];
     style[d3_event_dragSelect] = "none";
     return function(suppressClick) {
-      w.on(selectstart, null).on(dragstart, null);
+      w.on(touchstart, null).on(selectstart, null).on(dragstart, null);
       style[d3_event_dragSelect] = select;
       if (suppressClick) {
         function off() {
--- src/event/drag.js
+++ src/event/drag.js
@@ -5,15 +5,16 @@ import "../selection/on";
 var d3_event_dragSelect = d3_vendorSymbol(d3_documentElement.style, "userSelect");
 
 function d3_event_dragSuppress(type) {
-  var selectstart = "selectstart." + type,
+  var touchstart = "touchstart." + type,
+      selectstart = "selectstart." + type,
       dragstart = "dragstart." + type,
       click = "click." + type,
-      w = d3.select(d3_window).on(selectstart, d3_eventPreventDefault).on(dragstart, d3_eventPreventDefault),
+      w = d3.select(d3_window).on(touchstart, d3_eventPreventDefault).on(selectstart, d3_eventPreventDefault).on(dragstart, d3_eventPreventDefault),
       style = d3_documentElement.style,
       select = style[d3_event_dragSelect];
   style[d3_event_dragSelect] = "none";
   return function(suppressClick) {
-    w.on(selectstart, null).on(dragstart, null);
+    w.on(touchstart, null).on(selectstart, null).on(dragstart, null);
     style[d3_event_dragSelect] = select;
     if (suppressClick) { // suppress the next click, but only if it’s immediate
       function off() { w.on(click, null); }
