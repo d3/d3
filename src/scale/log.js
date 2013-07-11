4162116088948f23f6233dee26fb72bf3d570a3c
Mike Bostock <mbostock@gmail.com>
2013-07-11 12:55:34 -0700

    Fix rounding problems with log scale nicing.

--- src/scale/log.js
+++ src/scale/log.js
@@ -4,10 +4,18 @@ import "nice";
 import "scale";
 
 d3.scale.log = function() {
-  return d3_scale_log(d3.scale.linear().domain([0, Math.LN10]), 10, d3_scale_logp, d3_scale_powp, [1, 10]);
+  return d3_scale_log(d3.scale.linear().domain([0, 1]), 10, true, [1, 10]);
 };
 
-function d3_scale_log(linear, base, log, pow, domain) {
+function d3_scale_log(linear, base, positive, domain) {
+
+  function log(x) {
+    return (positive ? Math.log(x < 0 ? 0 : x) : -Math.log(x > 0 ? 0 : -x)) / Math.log(base);
+  }
+
+  function pow(x) {
+    return positive ? Math.pow(base, x) : -Math.pow(base, -x);
+  }
 
   function scale(x) {
     return linear(log(x));
@@ -19,8 +27,7 @@ function d3_scale_log(linear, base, log, pow, domain) {
 
   scale.domain = function(x) {
     if (!arguments.length) return domain;
-    if (x[0] < 0) log = d3_scale_logn, pow = d3_scale_pown;
-    else log = d3_scale_logp, pow = d3_scale_powp;
+    positive = x[0] >= 0;
     linear.domain((domain = x.map(Number)).map(log));
     return scale;
   };
@@ -28,23 +35,11 @@ function d3_scale_log(linear, base, log, pow, domain) {
   scale.base = function(_) {
     if (!arguments.length) return base;
     base = +_;
-    return scale;
+    return scale.domain(domain);
   };
 
   scale.nice = function() {
-
-    function floor(x) {
-      return Math.pow(base, Math.floor(Math.log(x) / Math.log(base)));
-    }
-
-    function ceil(x) {
-      return Math.pow(base, Math.ceil(Math.log(x) / Math.log(base)));
-    }
-
-    linear.domain(d3_scale_nice(domain, log === d3_scale_logp
-        ? {floor: floor, ceil: ceil}
-        : {floor: function(x) { return -ceil(-x); }, ceil: function(x) { return -floor(-x); }}).map(log));
-
+    domain = d3_scale_nice(linear.domain(), positive ? Math : d3_scale_logNiceNegative).map(pow);
     return scale;
   };
 
@@ -52,18 +47,17 @@ function d3_scale_log(linear, base, log, pow, domain) {
     var extent = d3_scaleExtent(linear.domain()),
         ticks = [];
     if (extent.every(isFinite)) {
-      var b = Math.log(base),
-          i = Math.floor(extent[0] / b),
-          j = Math.ceil(extent[1] / b),
+      var i = Math.floor(extent[0]),
+          j = Math.ceil(extent[1]),
           u = pow(extent[0]),
           v = pow(extent[1]),
           n = base % 1 ? 2 : base;
-      if (log === d3_scale_logn) {
-        ticks.push(-Math.pow(base, -i));
-        for (; i++ < j;) for (var k = n - 1; k > 0; k--) ticks.push(-Math.pow(base, -i) * k);
-      } else {
+      if (positive) {
         for (; i < j; i++) for (var k = 1; k < n; k++) ticks.push(Math.pow(base, i) * k);
         ticks.push(Math.pow(base, i));
+      } else {
+        ticks.push(-Math.pow(base, -i));
+        for (; i++ < j;) for (var k = n - 1; k > 0; k--) ticks.push(-Math.pow(base, -i) * k);
       }
       for (i = 0; ticks[i] < u; i++) {} // strip small values
       for (j = ticks.length; ticks[j - 1] > v; j--) {} // strip big values
@@ -76,36 +70,20 @@ function d3_scale_log(linear, base, log, pow, domain) {
     if (!arguments.length) return d3_scale_logFormat;
     if (arguments.length < 2) format = d3_scale_logFormat;
     else if (typeof format !== "function") format = d3.format(format);
-    var b = Math.log(base),
-        k = Math.max(.1, n / scale.ticks().length),
-        f = log === d3_scale_logn ? (e = -1e-12, Math.floor) : (e = 1e-12, Math.ceil),
+    var k = Math.max(.1, n / scale.ticks().length),
+        f = positive ? (e = 1e-12, Math.ceil) : (e = -1e-12, Math.floor),
         e;
     return function(d) {
-      return d / pow(b * f(log(d) / b + e)) <= k ? format(d) : "";
+      return d / pow(f(log(d) + e)) <= k ? format(d) : "";
     };
   };
 
   scale.copy = function() {
-    return d3_scale_log(linear.copy(), base, log, pow, domain);
+    return d3_scale_log(linear.copy(), base, positive, domain);
   };
 
   return d3_scale_linearRebind(scale, linear);
 }
 
-var d3_scale_logFormat = d3.format(".0e");
-
-function d3_scale_logp(x) {
-  return Math.log(x < 0 ? 0 : x);
-}
-
-function d3_scale_powp(x) {
-  return Math.exp(x);
-}
-
-function d3_scale_logn(x) {
-  return -Math.log(x > 0 ? 0 : -x);
-}
-
-function d3_scale_pown(x) {
-  return -Math.exp(-x);
-}
+var d3_scale_logFormat = d3.format(".0e"),
+    d3_scale_logNiceNegative = {floor: function(x) { return -Math.ceil(-x); }, ceil: function(x) { return -Math.floor(-x); }};
