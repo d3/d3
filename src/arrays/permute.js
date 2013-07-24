1bec3a67b04cf244a504cb0b1585b0a57c893831
Leonardo Dutra <leodutra.br@gmail.com>
2013-07-24 17:51:35 -0300

    Update permute.js
    Why? Less vars, less operators, and this difference:
    http://jsperf.com/array-constructor-vs-literal-modalities

--- src/arrays/permute.js
+++ src/arrays/permute.js
@@ -1,7 +1,6 @@
 d3.permute = function(array, indexes) {
-  var permutes = [],
-      i = -1,
-      n = indexes.length;
-  while (++i < n) permutes[i] = array[indexes[i]];
+  var i = indexes.length,
+      permutes = Array(i);
+  while (i--) permutes[i] = array[indexes[i]];
   return permutes;
 };
