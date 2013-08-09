b6718f5db3297d7d5f7c5cbd842ade87a25b1e02
MrCox <danny.cox@aunalytics.com>
2013-08-09 21:07:28 +0000

    Updated d3.node

--- d3.js
+++ d3.js
@@ -884,11 +884,17 @@ d3 = function() {
   d3_selectionPrototype.empty = function() {
     return !this.node();
   };
-  d3_selectionPrototype.node = function() {
+  d3_selectionPrototype.node = function(N) {
     for (var j = 0, m = this.length; j < m; j++) {
       for (var group = this[j], i = 0, n = group.length; i < n; i++) {
         var node = group[i];
-        if (node) return node;
+        if (!arguments.length || N == 0){
+          if (node) return node;
+        } else {
+          if (i == N){
+            if (node) return node;
+          }
+        }
       }
     }
     return null;
