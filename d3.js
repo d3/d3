e6551e5703f09261f1b5521c3fcf84dcdce700f2
Mike Bostock <mbostock@gmail.com>
2013-08-21 11:57:51 -0700

    Compress slightly.

--- d3.js
+++ d3.js
@@ -8850,12 +8850,9 @@ d3 = function() {
     };
     function tickMethod(extent, count) {
       var span = extent[1] - extent[0], target = span / count, i = d3.bisect(d3_time_scaleSteps, target);
-      if (i == d3_time_scaleSteps.length) return [ methods.year, d3_scale_linearTickRange(extent.map(function(d) {
+      return i == d3_time_scaleSteps.length ? [ methods.year, d3_scale_linearTickRange(extent.map(function(d) {
         return d / 31536e6;
-      }), count)[2] ];
-      if (!i) return [ d3_time_scaleMilliseconds, d3_scale_linearTickRange(extent, count)[2] ];
-      if (target / d3_time_scaleSteps[i - 1] < d3_time_scaleSteps[i] / target) --i;
-      return methods[i];
+      }), count)[2] ] : !i ? [ d3_time_scaleMilliseconds, d3_scale_linearTickRange(extent, count)[2] ] : methods[target / d3_time_scaleSteps[i - 1] < d3_time_scaleSteps[i] / target ? i - 1 : i];
     }
     scale.nice = function(interval) {
       var domain = scale.domain(), extent = d3_scaleExtent(domain), method = interval == null ? tickMethod(extent, 10) : typeof interval === "number" ? tickMethod(extent, interval) : [ interval ];
--- src/time/scale.js
+++ src/time/scale.js
@@ -34,10 +34,9 @@ function d3_time_scale(linear, methods, format) {
     var span = extent[1] - extent[0],
         target = span / count,
         i = d3.bisect(d3_time_scaleSteps, target);
-    if (i == d3_time_scaleSteps.length) return [methods.year, d3_scale_linearTickRange(extent.map(function(d) { return d / 31536e6; }), count)[2]];
-    if (!i) return [d3_time_scaleMilliseconds, d3_scale_linearTickRange(extent, count)[2]];
-    if (target / d3_time_scaleSteps[i - 1] < d3_time_scaleSteps[i] / target) --i;
-    return methods[i];
+    return i == d3_time_scaleSteps.length ? [methods.year, d3_scale_linearTickRange(extent.map(function(d) { return d / 31536e6; }), count)[2]]
+        : !i ? [d3_time_scaleMilliseconds, d3_scale_linearTickRange(extent, count)[2]]
+        : methods[target / d3_time_scaleSteps[i - 1] < d3_time_scaleSteps[i] / target ? i - 1 : i];
   }
 
   scale.nice = function(interval) { // TODO skip
