6f47f19d813f479ad9d4dddeb36488b19be54e06
Mike Bostock <mbostock@gmail.com>
2013-09-24 11:05:14 -0700

    Shorten.

--- src/geom/voronoi/cell.js
+++ src/geom/voronoi/cell.js
@@ -22,6 +22,10 @@ function d3_geom_voronoiCloseCells(extent) {
       x1 = extent[1][0],
       y0 = extent[0][1],
       y1 = extent[1][1],
+      x2,
+      y2,
+      x3,
+      y3,
       cells = d3_geom_voronoiCells,
       iCell = cells.length,
       cell,
@@ -41,14 +45,14 @@ function d3_geom_voronoiCloseCells(extent) {
     iLeft = 0;
     while (iLeft < nHalfEdges) {
       iRight = (iLeft + 1) % nHalfEdges;
-      end = halfEdges[iLeft].end();
-      start = halfEdges[iRight].start();
-      if (abs(end[0] - start[0]) > ε || abs(end[1] - start[1]) > ε) {
+      start = halfEdges[iRight].start(), x2 = start[0], y2 = start[1];
+      end = halfEdges[iLeft].end(), x3 = end[0], y3 = end[1];
+      if (abs(x3 - x2) > ε || abs(y3 - y2) > ε) {
         edge = d3_geom_voronoiCreateBorderEdge(cell.site, end,
-            abs(end[0] - x0) < ε && y1 - end[1] > ε ? [x0, abs(start[0] - x0) < ε ? start[1] : y1]
-            : abs(end[1] - y1) < ε && x1 - end[0] > ε ? [abs(start[1] - y1) < ε ? start[0] : x1, y1]
-            : abs(end[0] - x1) < ε && end[1] - y0 > ε ? [x1, abs(start[0] - x1) < ε ? start[1] : y0]
-            : abs(end[1] - y0) < ε && end[0] - x0 > ε ? [abs(start[1] - y0) < ε ? start[0] : x0, y0]
+            abs(x3 - x0) < ε && y1 - y3 > ε ? [x0, abs(x2 - x0) < ε ? y2 : y1]
+            : abs(y3 - y1) < ε && x1 - x3 > ε ? [abs(y2 - y1) < ε ? x2 : x1, y1]
+            : abs(x3 - x1) < ε && y3 - y0 > ε ? [x1, abs(x2 - x1) < ε ? y2 : y0]
+            : abs(y3 - y0) < ε && x3 - x0 > ε ? [abs(y2 - y0) < ε ? x2 : x0, y0]
             : null);
         halfEdges.splice(iLeft + 1, 0, new d3_geom_voronoiHalfEdge(edge, cell.site, null));
         nHalfEdges = halfEdges.length;
