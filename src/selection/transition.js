182b936a03bfda4770accea69bc0ff22b913eb39
sumh <42424861@qq.com>
2013-10-02 12:42:57 +0800

    Optimized transition.js
    X-Ref 1bec3a67b
    
    sumh <42424861@qq.com>

--- src/selection/transition.js
+++ src/selection/transition.js
@@ -3,16 +3,18 @@ import "selection";
 
 d3_selectionPrototype.transition = function() {
   var id = d3_transitionInheritId || ++d3_transitionId,
-      subgroups = [],
+      m = this.length,
+      subgroups = new Array(m),
       subgroup,
-      node,
-      transition = d3_transitionInherit || {time: Date.now(), ease: d3_ease_cubicInOut, delay: 0, duration: 250};
+      group,
+      transition = d3_transitionInherit || {time: Date.now(), ease: d3_ease_cubicInOut, delay: 0, duration: 250}
+      j,
+      i;
 
-  for (var j = -1, m = this.length; ++j < m;) {
-    subgroups.push(subgroup = []);
-    for (var group = this[j], i = -1, n = group.length; ++i < n;) {
-      if (node = group[i]) d3_transitionNode(node, i, id, transition);
-      subgroup.push(node);
+  for (var j = -1; ++j < m;) {
+    subgroups[j] = subgroup = new Array(n = (group = this[j]).length);
+    for (i = -1; ++i < n;) {
+      if (subgroup[i] = group[i]) d3_transitionNode(node, i, id, transition);
     }
   }
 
