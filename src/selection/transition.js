a910b2e80ab3c950e125c6ab174edefcbfa37d39
sumh <42424861@qq.com>
2013-10-02 13:46:23 +0800

    Optimized transition
    X-Ref 1bec3a67b
    
    sumh <42424861@qq.com>

--- src/selection/transition.js
+++ src/selection/transition.js
@@ -7,7 +7,7 @@ d3_selectionPrototype.transition = function() {
       subgroups = new Array(m),
       subgroup,
       group,
-      transition = d3_transitionInherit || {time: Date.now(), ease: d3_ease_cubicInOut, delay: 0, duration: 250}
+      transition = d3_transitionInherit || {time: Date.now(), ease: d3_ease_cubicInOut, delay: 0, duration: 250},
       j,
       i;
 
--- src/transition/select.js
+++ src/transition/select.js
@@ -3,22 +3,26 @@ import "transition";
 
 d3_transitionPrototype.select = function(selector) {
   var id = this.id,
-      subgroups = [],
+      m = this.length,
+      subgroups = new Array(m),
       subgroup,
       subnode,
-      node;
+      group,
+      node,
+      j,
+      i;
 
   selector = d3_selection_selector(selector);
 
-  for (var j = -1, m = this.length; ++j < m;) {
-    subgroups.push(subgroup = []);
-    for (var group = this[j], i = -1, n = group.length; ++i < n;) {
+  for (var j = -1; ++j < m;) {
+    subgroups[j] = subgroup = new Array(n = (group = this[j]).length);
+    for (i = -1; ++i < n;) {
       if ((node = group[i]) && (subnode = selector.call(node, node.__data__, i, j))) {
         if ("__data__" in node) subnode.__data__ = node.__data__;
         d3_transitionNode(subnode, i, id, node.__transition__[id]);
-        subgroup.push(subnode);
+        subgroup[i] = subnode;
       } else {
-        subgroup.push(null);
+        subgroup[i] = null;
       }
     }
   }
