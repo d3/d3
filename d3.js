ee2476b09cb7c9af4c97f81ffc898dee6bf8a4bd
Mike Bostock <mbostock@gmail.com>
2013-10-05 14:59:03 -0700

    Use objects rather than arrays, cont.

--- d3.js
+++ d3.js
@@ -4710,7 +4710,19 @@ d3 = function() {
         end = halfEdges[iHalfEdge].end(), x3 = end.x, y3 = end.y;
         start = halfEdges[++iHalfEdge % nHalfEdges].start(), x2 = start.x, y2 = start.y;
         if (abs(x3 - x2) > ε || abs(y3 - y2) > ε) {
-          edge = d3_geom_voronoiCreateBorderEdge(cell.site, end, abs(x3 - x0) < ε && y1 - y3 > ε ? [ x0, abs(x2 - x0) < ε ? y2 : y1 ] : abs(y3 - y1) < ε && x1 - x3 > ε ? [ abs(y2 - y1) < ε ? x2 : x1, y1 ] : abs(x3 - x1) < ε && y3 - y0 > ε ? [ x1, abs(x2 - x1) < ε ? y2 : y0 ] : abs(y3 - y0) < ε && x3 - x0 > ε ? [ abs(y2 - y0) < ε ? x2 : x0, y0 ] : null);
+          edge = d3_geom_voronoiCreateBorderEdge(cell.site, end, abs(x3 - x0) < ε && y1 - y3 > ε ? {
+            x: x0,
+            y: abs(x2 - x0) < ε ? y2 : y1
+          } : abs(y3 - y1) < ε && x1 - x3 > ε ? {
+            x: abs(y2 - y1) < ε ? x2 : x1,
+            y: y1
+          } : abs(x3 - x1) < ε && y3 - y0 > ε ? {
+            x: x1,
+            y: abs(x2 - x1) < ε ? y2 : y0
+          } : abs(y3 - y0) < ε && x3 - x0 > ε ? {
+            x: abs(y2 - y0) < ε ? x2 : x0,
+            y: y0
+          } : null);
           halfEdges.splice(iHalfEdge, 0, new d3_geom_voronoiHalfEdge(edge, cell.site, null));
           ++nHalfEdges;
         }
