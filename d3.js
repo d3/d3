91506b31eab565e632b7e6a3014bda766ce60efb
Mike Bostock <mbostock@gmail.com>
2013-10-25 14:53:47 -0700

    Prefer selectstart to user-select.
    Rather than both setting the user-select to none and suppressing the selectstart
    event, we now prefer to suppress the selectstart event if supported. This avoids
    redundantly setting the user-select style, which can be slow. Fixes #1599.

--- d3.js
+++ d3.js
@@ -1059,13 +1059,16 @@ d3 = function() {
       }
     };
   }
-  var d3_event_dragSelect = d3_vendorSymbol(d3_documentElement.style, "userSelect"), d3_event_dragId = 0;
+  var d3_event_dragSelect = "onselectstart" in d3_document ? null : d3_vendorSymbol(d3_documentElement.style, "userSelect"), d3_event_dragId = 0;
   function d3_event_dragSuppress() {
-    var name = ".dragsuppress-" + ++d3_event_dragId, touchmove = "touchmove" + name, selectstart = "selectstart" + name, dragstart = "dragstart" + name, click = "click" + name, w = d3.select(d3_window).on(touchmove, d3_eventPreventDefault).on(selectstart, d3_eventPreventDefault).on(dragstart, d3_eventPreventDefault), style = d3_documentElement.style, select = style[d3_event_dragSelect];
-    style[d3_event_dragSelect] = "none";
+    var name = ".dragsuppress-" + ++d3_event_dragId, click = "click" + name, w = d3.select(d3_window).on("touchmove" + name, d3_eventPreventDefault).on("dragstart" + name, d3_eventPreventDefault).on("selectstart" + name, d3_eventPreventDefault);
+    if (d3_event_dragSelect) {
+      var style = d3_documentElement.style, select = style[d3_event_dragSelect];
+      style[d3_event_dragSelect] = "none";
+    }
     return function(suppressClick) {
       w.on(name, null);
-      style[d3_event_dragSelect] = select;
+      if (d3_event_dragSelect) style[d3_event_dragSelect] = select;
       if (suppressClick) {
         function off() {
           w.on(click, null);
