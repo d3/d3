fa618397f6f09c274c72b11dcd91800456d1604a
Daniel Goldbach <dan.goldbach@gmail.com>
2013-11-24 15:02:59 +1100

    Add methods .size(), .empty() to d3.{set,map}

--- src/arrays/set.js
+++ src/arrays/set.js
@@ -7,19 +7,29 @@ d3.set = function(array) {
   return set;
 };
 
-function d3_Set() {}
+function d3_Set() {
+  this._size = 0;
+}
 
 d3_class(d3_Set, {
   has: function(value) {
     return d3_map_prefix + value in this;
   },
   add: function(value) {
+    if (!this.has(value)) this._size++;
     this[d3_map_prefix + value] = true;
     return value;
   },
   remove: function(value) {
     value = d3_map_prefix + value;
+    if (value in this) this._size--;
     return value in this && delete this[value];
+  },
+  size: function() {
+    return this._size;
+  },
+  empty: function() {
+    return this._size === 0;
   },
   values: function() {
     var values = [];
