b42592a5074bf50ed9af8fc9161506c057ed9d40
Daniel Suess <daniel.suess@akquinet.de>
2013-11-25 22:51:07 +0100

    function to set localized strings for weekday and month at runtime

--- src/time/format-localized.js
+++ src/time/format-localized.js
@@ -4,8 +4,40 @@ var d3_time_formatDateTime = "%a %b %e %X %Y",
     d3_time_formatTime = "%H:%M:%S";
 
 // The weekday and month names.
-var d3_time_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+// start of i18n patch
+// d3_time_daySymbols will be set another time in time.js, but setLocalizedStrings can just be called later
+var d3_time_daySymbols = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    d3_time_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
     d3_time_dayAbbreviations = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
     d3_time_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
     d3_time_monthAbbreviations = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+/**
+ * Set localized strings for weekdays and month.
+ * When d3 is initialized some time variables are used to calculate e. g. european and us calendar weeks.
+ * As far as I know, renaming this strings later does not produce side effects.
+ * @param time_days from sunday to saturday
+ * @param time_dayAbbreviations 3 letters, from sun to sat
+ * @param time_months from january to december
+ * @param time_monthAbbreviations 3 letters, from jan to dec
+ */
+d3.time.setLocalizedStrings = function(time_days, time_dayAbbreviations, time_months, time_monthAbbreviations){
+    function isValidArray(array, expectedSize){
+        return (array && Object.prototype.toString.apply(array) === '[object Array]' && array.length == expectedSize ? true : false);
+    }
+    // overwrite if valid, otherwise keep english presets
+    if(isValidArray(time_days, 7)){
+        d3_time_days = time_days;
+        d3_time_daySymbols = time_days;
+    }
+    if(isValidArray(time_dayAbbreviations, 7)){
+        d3_time_dayAbbreviations = time_dayAbbreviations;
+    }
+    if (isValidArray(time_months, 12)){
+        d3_time_months = time_months;
+    }
+    if(isValidArray(time_monthAbbreviations, 12)){
+        d3_time_monthAbbreviations = time_monthAbbreviations;
+    }
+};
+// end of i18n patch
