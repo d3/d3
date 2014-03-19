1ce09c9b9308cdceeb75c838c8951a6b35a43008
Mike Bostock <mbostock@gmail.com>
2014-03-19 14:42:12 -0700

    d3.touch defaults to changedTouches.
    This way, it’s easier to tell whether the touch changed during the event. This
    also fixes #1600 because the drag behavior now only dispatches a drag event on
    elements that moved, even if multiple touches are active.

--- src/event/touch.js
+++ src/event/touch.js
@@ -2,7 +2,7 @@ import "event";
 import "mouse";
 
 d3.touch = function(container, touches, identifier) {
-  if (arguments.length < 3) identifier = touches, touches = d3_eventSource().touches;
+  if (arguments.length < 3) identifier = touches, touches = d3_eventSource().changedTouches;
   if (touches) for (var i = 0, n = touches.length, touch; i < n; ++i) {
     if ((touch = touches[i]).identifier === identifier) {
       return d3_mousePoint(container, touch);
