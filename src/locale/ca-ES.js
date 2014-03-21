17e1f967304f75637405133a4d97cca7b6c31c54
Roger Bramon <rogerbramon@gmail.com>
2014-03-21 22:04:36 +0100

    Add ca-ES for localization

---
+++ src/locale/ca-ES.js
@@ -0,0 +1,16 @@
+import "locale";
+
+d3.locale.ca_ES = d3.locale({
+  decimal: ",",
+  thousands: ".",
+  grouping: [3],
+  currency: ["", " €"],
+  dateTime: "%A, %e de %B de %Y, %X",
+  date: "%d/%m/%Y",
+  time: "%H:%M:%S",
+  periods: ["AM", "PM"],
+  days: ["diumenge", "dilluns", "dimarts", "dimecres", "dijous", "divendres", "dissabte"],
+  shortDays: ["dg.", "dl.", "dt.", "dc.", "dj.", "dv.", "ds."],
+  months: ["gener", "febrer", "març", "abril", "maig", "juny", "juliol", "agost", "setembre", "octubre", "novembre", "desembre"],
+  shortMonths: ["gen.", "febr.", "març", "abr.", "maig", "juny", "jul.", "ag.", "set.", "oct.", "nov.", "des."]
+});
