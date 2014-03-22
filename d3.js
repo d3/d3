5ed75d07f6ece0a83d3ad32fd29e72625c9e47a4
Mike Bostock <mbostock@gmail.com>
2014-03-22 12:36:14 -0700

    Fix NaN in d3_geo_clipAntimeridianIntersect.
    If φ0 or φ1 is ±π/2, then cos(φ0) or cos(φ1) is zero, leading to a divide by
    zero and returned NaN. Using atan2 avoids this.

--- d3.js
+++ d3.js
@@ -3400,7 +3400,7 @@
   }
   function d3_geo_clipAntimeridianIntersect(λ0, φ0, λ1, φ1) {
     var cosφ0, cosφ1, sinλ0_λ1 = Math.sin(λ0 - λ1);
-    return abs(sinλ0_λ1) > ε ? Math.atan((Math.sin(φ0) * (cosφ1 = Math.cos(φ1)) * Math.sin(λ1) - Math.sin(φ1) * (cosφ0 = Math.cos(φ0)) * Math.sin(λ0)) / (cosφ0 * cosφ1 * sinλ0_λ1)) : (φ0 + φ1) / 2;
+    return abs(sinλ0_λ1) > ε ? Math.atan2(Math.sin(φ0) * (cosφ1 = Math.cos(φ1)) * Math.sin(λ1) - Math.sin(φ1) * (cosφ0 = Math.cos(φ0)) * Math.sin(λ0), cosφ0 * cosφ1 * sinλ0_λ1) : (φ0 + φ1) / 2;
   }
   function d3_geo_clipAntimeridianInterpolate(from, to, direction, listener) {
     var φ;
--- src/geo/clip-antimeridian.js
+++ src/geo/clip-antimeridian.js
@@ -65,9 +65,7 @@ function d3_geo_clipAntimeridianIntersect(λ0, φ0, λ1, φ1) {
       cosφ1,
       sinλ0_λ1 = Math.sin(λ0 - λ1);
   return abs(sinλ0_λ1) > ε
-      ? Math.atan((Math.sin(φ0) * (cosφ1 = Math.cos(φ1)) * Math.sin(λ1)
-                 - Math.sin(φ1) * (cosφ0 = Math.cos(φ0)) * Math.sin(λ0))
-                 / (cosφ0 * cosφ1 * sinλ0_λ1))
+      ? Math.atan2((Math.sin(φ0) * (cosφ1 = Math.cos(φ1)) * Math.sin(λ1) - Math.sin(φ1) * (cosφ0 = Math.cos(φ0)) * Math.sin(λ0)), (cosφ0 * cosφ1 * sinλ0_λ1))
       : (φ0 + φ1) / 2;
 }
 
