b86e4e4ae146bf807c966785ac86f6b967359ca2
Mike Bostock <mbostock@gmail.com>
2014-03-23 22:42:28 -0700

    Getter support for transition.{delay,duration}.
    Fixes #1773.

--- d3.js
+++ d3.js
@@ -8578,6 +8578,7 @@
   };
   d3_transitionPrototype.delay = function(value) {
     var id = this.id;
+    if (arguments.length < 1) return this.node().__transition__[id].delay;
     return d3_selection_each(this, typeof value === "function" ? function(node, i, j) {
       node.__transition__[id].delay = +value.call(node, node.__data__, i, j);
     } : (value = +value, function(node) {
@@ -8586,6 +8587,7 @@
   };
   d3_transitionPrototype.duration = function(value) {
     var id = this.id;
+    if (arguments.length < 1) return this.node().__transition__[id].duration;
     return d3_selection_each(this, typeof value === "function" ? function(node, i, j) {
       node.__transition__[id].duration = Math.max(1, value.call(node, node.__data__, i, j));
     } : (value = Math.max(1, value), function(node) {
--- src/transition/duration.js
+++ src/transition/duration.js
@@ -3,6 +3,7 @@ import "transition";
 
 d3_transitionPrototype.duration = function(value) {
   var id = this.id;
+  if (arguments.length < 1) return this.node().__transition__[id].duration;
   return d3_selection_each(this, typeof value === "function"
       ? function(node, i, j) { node.__transition__[id].duration = Math.max(1, value.call(node, node.__data__, i, j)); }
       : (value = Math.max(1, value), function(node) { node.__transition__[id].duration = value; }));
