650a9a922fbc48be440f718d31c077ee737cf892
Jason Davies <jason@jasondavies.com>
2014-06-23 11:12:16 +0100

    Add getter for transition.text.

--- d3.js
+++ d3.js
@@ -8526,13 +8526,17 @@
     return this.tween("style." + name, styleTween);
   };
   d3_transitionPrototype.text = function(value) {
-    return d3_transition_tween(this, "text", value, d3_transition_text);
+    if (arguments.length) return d3_transition_tween(this, "text", value, d3_transition_text);
+    var node = this.node(), tween = node.__transition__[this.id].tween.get("text");
+    return tween ? tween.$ : node.textContent;
   };
   function d3_transition_text(b) {
     if (b == null) b = "";
-    return function() {
+    f.$ = b;
+    return f;
+    function f() {
       this.textContent = b;
-    };
+    }
   }
   d3_transitionPrototype.remove = function() {
     return this.each("end.transition", function() {
