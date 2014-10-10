0a412f620e6ac3a040334947434a29d6ca7ad884
Mike Bostock <mbostock@gmail.com>
2014-10-10 09:46:55 -0700

    Fix #2006 - prevent leak of d3 global in node.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var document = require("jsdom").jsdom("<html><head></head><body></body></html>"),
-    window = document.parentWindow,
+var document = require("jsdom").jsdom(),
     globals = {};
 
-// stash globals
+// Stash old globals.
+if ("d3" in global) globals.d3 = global.d3;
 if ("window" in global) globals.window = global.window;
-global.window = window;
 if ("document" in global) globals.document = global.document;
+
+// Set temporary globals to pretend we’re in a browser.
+global.window = document.parentWindow;
 global.document = document;
 
 module.exports = require("./d3");
 
-// restore globals
-if ("window" in globals) global.window = globals.window;
-else delete global.window;
-if ("document" in globals) global.document = globals.document;
-else delete global.document;
+// Restore old globals.
+if ("d3" in globals) global.d3 = globals.d3; else delete global.d3;
+if ("window" in globals) global.window = globals.window; else delete global.window;
+if ("document" in globals) global.document = globals.document; else delete global.document;
