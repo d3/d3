9592f9b8fd2797655c8490b5c4171e5800f75223
Mike Bostock <mbostock@gmail.com>
2014-10-10 10:59:49 -0700

    No d3_zero.

--- d3.js
+++ d3.js
@@ -5905,16 +5905,15 @@
       return s.join("");
     };
   }
-  function d3_zero() {
-    return 0;
-  }
   function d3_uninterpolateNumber(a, b) {
-    return (b -= a = +a) === 0 ? d3_zero : function(x) {
+    if ((b -= a = +a) === 0) b = Infinity;
+    return function(x) {
       return (x - a) / b;
     };
   }
   function d3_uninterpolateClamp(a, b) {
-    return (b -= a = +a) === 0 ? d3_zero : function(x) {
+    if ((b -= a = +a) === 0) b = Infinity;
+    return function(x) {
       return Math.max(0, Math.min(1, (x - a) / b));
     };
   }
--- src/interpolate/uninterpolate.js
+++ src/interpolate/uninterpolate.js
@@ -1,13 +1,9 @@
-import "../core/zero";
-
 function d3_uninterpolateNumber(a, b) {
-  return (b -= a = +a) === 0
-      ? d3_zero
-      : function(x) { return (x - a) / b; };
+  if ((b -= a = +a) === 0) b = Infinity;
+  return function(x) { return (x - a) / b; };
 }
 
 function d3_uninterpolateClamp(a, b) {
-  return (b -= a = +a) === 0
-      ? d3_zero
-      : function(x) { return Math.max(0, Math.min(1, (x - a) / b)); };
+  if ((b -= a = +a) === 0) b = Infinity;
+  return function(x) { return Math.max(0, Math.min(1, (x - a) / b)); };
 }
