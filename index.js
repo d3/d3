f8514d3a51e71faf9513a32f64e7e1ca7da20796
Mike Bostock <mbostock@gmail.com>
2014-10-10 09:35:51 -0700

    Remove CSSStyleDeclaration fix.
    This is no longer necessary with the upgrade to JSDOM 1.0.

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ global.window = window;
 if ("document" in global) globals.document = global.document;
 global.document = document;
 
-// https://github.com/chad3814/CSSStyleDeclaration/issues/3
-var CSSStyleDeclaration_prototype = window.CSSStyleDeclaration.prototype,
-    CSSStyleDeclaration_setProperty = CSSStyleDeclaration_prototype.setProperty;
-CSSStyleDeclaration_prototype.setProperty = function(name, value, priority) {
-  return CSSStyleDeclaration_setProperty.call(this, name + "", value == null ? null : value + "", priority == null ? null : priority + "");
-};
-
 module.exports = require("./d3");
 
 // restore globals
