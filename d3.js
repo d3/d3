1531870f0c83c0c15c3ad77884568790481a610c
Jason Davies <jason@jasondavies.com>
2014-10-10 17:53:19 +0100

    Slightly simpler version using d3_zero.
    As suggested by @mbostock.  Note that we preserve the behaviour for
    ranges that are coerced to NaNs.

--- d3.js
+++ d3.js
@@ -5905,15 +5905,16 @@
       return s.join("");
     };
   }
+  function d3_zero() {
+    return 0;
+  }
   function d3_uninterpolateNumber(a, b) {
-    b = b - (a = +a) || Infinity;
-    return function(x) {
+    return (b -= a = +a) === 0 ? d3_zero : function(x) {
       return (x - a) / b;
     };
   }
   function d3_uninterpolateClamp(a, b) {
-    b = b - (a = +a) || Infinity;
-    return function(x) {
+    return (b -= a = +a) === 0 ? d3_zero : function(x) {
       return Math.max(0, Math.min(1, (x - a) / b));
     };
   }
--- src/interpolate/uninterpolate.js
+++ src/interpolate/uninterpolate.js
@@ -1,9 +1,13 @@
+import "../core/zero";
+
 function d3_uninterpolateNumber(a, b) {
-  b = b - (a = +a) || Infinity;
-  return function(x) { return (x - a) / b; };
+  return (b -= a = +a) === 0
+      ? d3_zero
+      : function(x) { return (x - a) / b; };
 }
 
 function d3_uninterpolateClamp(a, b) {
-  b = b - (a = +a) || Infinity;
-  return function(x) { return Math.max(0, Math.min(1, (x - a) / b)); };
+  return (b -= a = +a) === 0
+      ? d3_zero
+      : function(x) { return Math.max(0, Math.min(1, (x - a) / b)); };
 }
