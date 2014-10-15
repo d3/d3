9bcc57c7ab66efe489d9dd07e53323e07683e389
Mike Bostock <mbostock@gmail.com>
2014-10-15 09:53:45 -0700

    Use Object.create(null) for maps.

--- src/arrays/map.js
+++ src/arrays/map.js
@@ -7,92 +7,70 @@ d3.map = function(object) {
   return map;
 };
 
-function d3_Map() {}
+function d3_Map() {
+  this._ = Object.create(null);
+}
 
 d3_class(d3_Map, {
   has: d3_map_has,
   get: function(key) {
-    return this[d3_map_escape(key)];
+    return this._[key];
   },
   set: function(key, value) {
-    return this[d3_map_escape(key)] = value;
+    return this._[key] = value;
   },
   remove: d3_map_remove,
   keys: d3_map_keys,
   values: function() {
     var values = [];
-    for (var key in this) {
-      if (d3_map_hasOwnProperty.call(this, key)) {
-        values.push(this[key]);
-      }
+    for (var key in this._) {
+      values.push(this._[key]);
     }
     return values;
   },
   entries: function() {
     var entries = [];
-    for (var key in this) {
-      if (d3_map_hasOwnProperty.call(this, key)) {
-        entries.push({key: d3_map_unescape(key), value: this[key]});
-      }
+    for (var key in this._) {
+      entries.push({key: key, value: this._[key]});
     }
     return entries;
   },
   size: d3_map_size,
   empty: d3_map_empty,
   forEach: function(f) {
-    for (var key in this) {
-      if (d3_map_hasOwnProperty.call(this, key)) {
-        f.call(this, d3_map_unescape(key), this[key]);
-      }
+    for (var key in this._) {
+      f.call(this, key, this._[key]);
     }
   }
 });
 
-var d3_map_prefix = "\0", // prevent collision with built-ins
-    d3_map_builtin = new d3_Map,
-    d3_map_hasOwnProperty = Object.prototype.hasOwnProperty;
-
-function d3_map_escape(key) {
-  return (key += "") in d3_map_builtin || key[0] === d3_map_prefix ? d3_map_prefix + key : key;
-}
-
-function d3_map_unescape(key) {
-  return (key += "")[0] === d3_map_prefix ? key.slice(1) : key;
-}
-
 function d3_map_has(key) {
-  return d3_map_escape(key) in this;
+  return key in this._;
 }
 
 function d3_map_remove(key) {
-  return (key = d3_map_escape(key)) in this && delete this[key];
+  return key in this._ && delete this._[key];
 }
 
 function d3_map_keys() {
   var keys = [];
-  for (var key in this) {
-    if (d3_map_hasOwnProperty.call(this, key)) {
-      keys.push(d3_map_unescape(key));
-    }
+  for (var key in this._) {
+    keys.push(key);
   }
   return keys;
 }
 
 function d3_map_size() {
   var size = 0;
-  for (var key in this) {
-    if (d3_map_hasOwnProperty.call(this, key)) {
-      ++size;
-    }
+  for (var key in this._) {
+    ++size;
   }
   return size;
 }
 
 function d3_map_empty() {
-  for (var key in this) {
-    if (d3_map_hasOwnProperty.call(this, key)) {
-      return false;
-    }
+  for (var key in this._) {
+    return false;
   }
   return true;
 }
--- src/arrays/set.js
+++ src/arrays/set.js
@@ -7,12 +7,14 @@ d3.set = function(array) {
   return set;
 };
 
-function d3_Set() {}
+function d3_Set() {
+  this._ = Object.create(null);
+}
 
 d3_class(d3_Set, {
   has: d3_map_has,
   add: function(key) {
-    this[d3_map_escape(key)] = true;
+    this._[key] = true;
     return key;
   },
   remove: d3_map_remove,
@@ -20,13 +22,8 @@ d3_class(d3_Set, {
   size: d3_map_size,
   empty: d3_map_empty,
   forEach: function(f) {
-    for (var key in this) {
-      if (d3_map_hasOwnProperty.call(this, key)) {
-        f.call(this, d3_map_unescape(key));
-      }
+    for (var key in this._) {
+      f.call(this, key);
     }
   }
 });
-
-// In case Object.defineProperty is not supported…
-for (var key in new d3_Set) d3_map_builtin[key] = true;
