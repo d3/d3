74aaf245d0b4584186e261af785d6301541915ab
Mike Bostock <mbostock@gmail.com>
2014-10-16 23:20:36 -0700

    Use d3_numeric for !isNaN.

--- d3.js
+++ d3.js
@@ -78,21 +78,21 @@
     }
     return [ a, c ];
   };
-  d3.sum = function(array, f) {
-    var s = 0, n = array.length, a, i = -1;
-    if (arguments.length === 1) {
-      while (++i < n) if (!isNaN(a = +array[i])) s += a;
-    } else {
-      while (++i < n) if (!isNaN(a = +f.call(array, array[i], i))) s += a;
-    }
-    return s;
-  };
   function d3_number(x) {
     return x === null ? NaN : +x;
   }
   function d3_numeric(x) {
     return !isNaN(x);
   }
+  d3.sum = function(array, f) {
+    var s = 0, n = array.length, a, i = -1;
+    if (arguments.length === 1) {
+      while (++i < n) if (d3_numeric(a = +array[i])) s += a;
+    } else {
+      while (++i < n) if (d3_numeric(a = +f.call(array, array[i], i))) s += a;
+    }
+    return s;
+  };
   d3.mean = function(array, f) {
     var s = 0, n = array.length, a, i = -1, j = n;
     if (arguments.length === 1) {
