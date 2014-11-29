57f72bb51c42f73eda3f1d9ae1635e317baa699e
Dan Dascalescu <ddascalescu+github@gmail.com>
2014-11-29 14:39:59 -0800

    Simplified Meteor publish and run scripts

--- meteor/runtests.sh
+++ meteor/runtests.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Test Meteor package before publishing to Atmospherejs.com
 
-# Make sure Meteor is installed, per https://www.meteor.com/install. The curl'ed script is totally safe; takes 2 minutes to read its source and check.
+# Make sure Meteor is installed, per https://www.meteor.com/install.
+# The curl'ed script is totally safe; takes 2 minutes to read its source and check.
 type meteor >/dev/null 2>&1 || { curl https://install.meteor.com/ | sh; }
 
 # sanity check: make sure we're in the root directory of the checkout
@@ -15,23 +16,15 @@ int_trap() {
 
 trap int_trap INT
 
-# test any package*.js packages we may have, e.g. package.js, package-compat.js
-for PACKAGE_FILE in meteor/package*.js; do
+PACKAGE_NAME=$(grep -i name package.js | head -1 | cut -d "'" -f 2)
 
-  PACKAGE_NAME=$(grep -i name $PACKAGE_FILE | head -1 | cut -d "'" -f 2)
+echo "Testing $PACKAGE_NAME..."
 
-  echo "Testing $PACKAGE_NAME..."
+# Meteor expects package.js in the root directory of the checkout, so copy it there temporarily
+cp meteor/package.js .
 
-  # Meteor expects package.js to be in the root directory of the checkout, so copy there our package file under that name, temporarily
-  cp $PACKAGE_FILE ./package.js
+# provide an invalid MONGO_URL so Meteor doesn't bog us down with an empty Mongo database
+MONGO_URL=mongodb:// meteor test-packages ./
 
-  # provide an invalid MONGO_URL so Meteor doesn't bog us down with an empty Mongo database
-  MONGO_URL=mongodb:// meteor test-packages ./
-
-  rm -rf ".build.$PACKAGE_NAME"
-  rm -rf ".build.local-test:$PACKAGE_NAME"
-  rm versions.json 2>/dev/null
-
-  rm package.js
-
-done
+# cleanup temporary build files and package.js
+rm -rf ".build.$PACKAGE_NAME" ".build.local-test:$PACKAGE_NAME" versions.json package.js
