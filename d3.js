1fad2e0cf7fba217eba0eadf3cae17c15090f1dd
Mike Bostock <mbostock@gmail.com>
2015-02-07 18:48:24 -0800

    Simpler no-global for CommonJS.
    We don’t need the index.js to unset the global if we don’t set it.

--- d3.js
+++ d3.js
@@ -9499,6 +9499,5 @@
   d3.xml = d3_xhrType(function(request) {
     return request.responseXML;
   });
-  if (typeof define === "function" && define.amd) define(d3); else if (typeof module === "object" && module.exports) module.exports = d3;
-  this.d3 = d3;
+  if (typeof define === "function" && define.amd) define(this.d3 = d3); else if (typeof module === "object" && module.exports) module.exports = d3; else this.d3 = d3;
 }();
