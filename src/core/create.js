fe16b71ba7bd4f9e48b2bc034c23a5bb06cb6c1f
wassx <cwat-swasserb@lnz124084d02.clients.emea.cpwr.corp>
2015-02-18 15:22:36 +0100

    Revert "Merge branch 'adopt' of https://github.com/mbostock/d3"
    This reverts commit 8397db00454bb5c982c297a7e5b68d63abc8aadc, reversing
    changes made to e1441c1472c8cc40b92cd63d8e9bc9e2600df2bb.

--- src/core/create.js
+++
@@ -1,5 +0,0 @@
-d3.create = function(name) {
-  return d3.select((name = d3.ns.qualify(name)).local
-      ? document.createElementNS(name.space, name.local)
-      : document.createElement(name));
-};
--- src/core/selection-adopt.js
+++
@@ -1,5 +0,0 @@
-d3_selectionPrototype.adopt = function(selection) {
-  return this.select(function(d, i, j) {
-    return (d = selection[i]) && (d = d[j]) && this.appendChild(d);
-  });
-};
