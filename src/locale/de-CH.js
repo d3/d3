fc7ebe5c88983f79f126fbf4242d9068bc9c047a
Patrick Ruckstuhl <patrick@ch.tario.org>
2015-03-27 15:14:39 +0100

    Create de-CH.js
    Support for de_CH locale

---
+++ src/locale/de-CH.js
@@ -0,0 +1,15 @@
+import "locale";
+d3.locale.de_CH = d3.locale({
+decimal: ".",
+thousands: "'",
+grouping: [3],
+currency: ["", " CHF"],
+dateTime: "%A, der %e. %B %Y, %X",
+date: "%d.%m.%Y",
+time: "%H:%M:%S",
+periods: ["AM", "PM"], // unused
+days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"],
+shortDays: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"],
+months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+shortMonths: ["Jan", "Feb", "Mrz", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"]
+});
