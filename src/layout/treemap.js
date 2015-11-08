7a46a88fba2f83b44db588add1b8edca5b8be30a
Mike Bostock <mbostock@gmail.com>
2015-11-08 14:24:12 -0800

    Fix #2419 - handle empty trees.

--- src/layout/treemap.js
+++ src/layout/treemap.js
@@ -148,10 +148,9 @@ d3.layout.treemap = function() {
   function treemap(d) {
     var nodes = stickies || hierarchy(d),
         root = nodes[0];
-    root.x = 0;
-    root.y = 0;
-    root.dx = size[0];
-    root.dy = size[1];
+    root.x = root.y = 0;
+    if (root.value) root.dx = size[0], root.dy = size[1];
+    else root.dx = root.dy = 0;
     if (stickies) hierarchy.revalue(root);
     scale([root], root.dx * root.dy / root.value);
     (stickies ? stickify : squarify)(root);
