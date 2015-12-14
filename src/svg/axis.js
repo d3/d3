73d15220f00a8571f422755538c0d51289d0ae0c
John Harvey <jharvey@merkleinc.com>
2015-12-14 13:56:59 -0500

    doing a proper selection for tick0

--- src/svg/axis.js
+++ src/svg/axis.js
@@ -58,7 +58,8 @@ d3.svg.axis = function() {
       //set text of first tick in axis to
       //"firstTick".
       if (firstTick != null){
-        tick[0][0].select("text").text(firstTick); 
+        var tickZero = g.select(".tick");
+        tickZero.select("text").text(firstTick); 
       };
 
       if (orient === "bottom" || orient === "top") {
