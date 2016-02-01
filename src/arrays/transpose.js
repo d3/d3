bf0f88b6bc67faff0a95b89f4d911fd2cfc08fdd
Mike Bostock <mbostock@gmail.com>
2016-02-01 13:58:34 -0800

    Back-port d3.{transpose,zip} from d3-array.
    Fixes #2728.

--- src/arrays/transpose.js
+++ src/arrays/transpose.js
@@ -1,5 +1,15 @@
-import "zip";
+import "min";
 
 d3.transpose = function(matrix) {
-  return d3.zip.apply(d3, matrix);
+  if (!(n = matrix.length)) return [];
+  for (var i = -1, m = d3.min(matrix, d3_transposeLength), transpose = new Array(m); ++i < m;) {
+    for (var j = -1, n, row = transpose[i] = new Array(n); ++j < n;) {
+      row[j] = matrix[j][i];
+    }
+  }
+  return transpose;
 };
+
+function d3_transposeLength(d) {
+  return d.length;
+}
