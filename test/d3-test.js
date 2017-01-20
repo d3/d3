17fbf8d4b16ed19303d71dee4881d871bddbc037
Mike Bostock <mbostock@gmail.com>
2017-01-20 15:11:32 -0800

    Add test for d3.event re-export on Node. Related #3055.

--- test/d3-test.js
+++ test/d3-test.js
@@ -1,9 +1,21 @@
 var tape = require("tape"),
     d3 = require("../"),
+    d3Selection = require("d3-selection"),
     testExports = require("./test-exports");
 
 tape("version matches package.json", function(test) {
   test.equal(d3.version, require("../package.json").version);
+  test.end();
+});
+
+tape("d3.event is a getter for d3Selection.event", function(test) {
+  test.equal(d3.event, null);
+  try {
+    d3Selection.event = 42;
+    test.equal(d3.event, 42);
+  } finally {
+    d3Selection.event = null;
+  }
   test.end();
 });
 
