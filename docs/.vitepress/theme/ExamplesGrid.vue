33bb6586cdd89e486fcf23e2e1ea658c97940477
Mike Bostock <mbostock@gmail.com>
2023-06-10 12:21:03 -0700

    hex grid

--- docs/.vitepress/theme/ExamplesGrid.vue
+++ docs/.vitepress/theme/ExamplesGrid.vue
@@ -12,8 +12,9 @@ const n = 60; // maximum number of examples to show
 const slice = d3.shuffler(d3.randomLcg(d3.utcDay()))(data.slice()).slice(0, n);
 const sample = shallowRef(slice.slice(0, 55)); // initial guess
 const container = ref();
+const xn = ref(10); // number of rows
+const yn = ref(5); // number of columns
 const x = ref(720);
-const width = ref(1440);
 
 // Some browsers trigger pointermove more frequently than desirable, so we
 // debounce events for a smooth transitions.
@@ -29,9 +30,9 @@ function afterpointermove() {
 
 onMounted(() => {
   observer = new ResizeObserver(() => {
-    const m = getComputedStyle(container.value).gridTemplateColumns.split(" ").length;
-    sample.value = slice.slice(0, Math.min(Math.min(6, n / m) * m, n - (n % m)));
-    width.value = document.body.clientWidth;
+    xn.value = Math.ceil(document.body.clientWidth / 200) + 3;
+    yn.value = Math.min(5, Math.floor(n / xn.value));
+    sample.value = slice.slice(0, yn.value * xn.value);
   });
   observer.observe(document.body);
   addEventListener("pointermove", onpointermove);
@@ -45,41 +46,54 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div style="margin-left: -100px; margin-right: -100px;">
-    <div :class="$style.examples" ref="container" :style="`transform: perspective(75em) rotateX(35deg) rotateZ(-7deg) translateX(${width / 6 - x / 10}px) translateY(-100px) scale(1.4);`">
-      <a v-for="d in sample" :href="`https://observablehq.com/${d.path}`" :title="[d.title, d.author].join('\n')" target="_blank">
-        <img :src="`https://static.observableusercontent.com/thumbnail/${d.thumbnail}.jpg`" style="object-fit: cover;" width="640" height="400" />
-      </a>
-    </div>
+  <div :class="$style.examples" ref="container" :style="`transform: translate(${60 - x / 100}vw, 30%);`">
+    <a v-for="(d, i) in sample" :href="`https://observablehq.com/${d.path}`" :title="[d.title, d.author].join('\n')" target="_blank" :style="`--x: ${(i % xn) - xn / 2 + (Math.floor(i / xn) % 2) * 0.5}; --y: ${Math.floor(i / xn) - yn / 2}; animation-delay: ${((i % xn) / xn + (d3.randomLcg(1 / i)()) - 0.4) * 1}s;`">
+      <img :src="`https://static.observableusercontent.com/thumbnail/${d.thumbnail}.jpg`" width="640" height="400" />
+    </a>
   </div>
 </template>
 
 <style module>
 
 .examples {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
-  padding: 4px;
-  grid-gap: 4px;
   position: relative;
-  background: #e3e3e5;
-  border: solid 1px currentColor;
-  box-shadow: 0 4px 8px rgba(0,0,0,0.2);
+  height: 640px;
   transition: transform 150ms ease-out;
+  filter: drop-shadow(0 4px 8px rgba(0,0,0,0.2));
 }
 
 .examples a {
-  transition: transform 250ms ease, border-color 250ms ease, box-shadow 250ms ease;
-  border: solid 1px transparent;
-  margin: -2px;
+  position: absolute;
+  --transform: perspective(75em) rotateX(30deg) rotateZ(-7deg) translate(calc(var(--x) * 100%), calc(var(--y) * 86.67%)) scale(1.145);
+  transform: var(--transform);
+  animation: drop-in 500ms ease backwards;
+  transition: transform 250ms ease, filter 250ms ease;
 }
 
 .examples a:hover {
-  border: solid 1px black;
-  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.3);
-  position: relative;
-  transform: perspective(75em) translateZ(100px);
-  z-index: 2;
+  transform: var(--transform) translateZ(10px) scale(1.1);
+  filter: drop-shadow(0 4px 8px rgba(0, 0, 0, 0.4));
+  z-index: 3;
+}
+
+.examples img {
+  aspect-ratio: 1;
+  object-fit: cover;
+  clip-path: polygon(50.0% 100.0%, 93.3% 75.0%, 93.3% 25.0%, 50.0% 0.0%, 6.7% 25.0%, 6.7% 75.0%);
+  width: 200px;
+}
+
+@keyframes drop-in {
+  from {
+    transform: var(--transform) translateZ(1000px);
+    filter: blur(20px);
+    opacity: 0;
+  }
+  to {
+    transform: var(--transform);
+    filter: none;
+    opacity: 1;
+  }
 }
 
 </style>
