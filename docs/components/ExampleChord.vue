d1146708e01b5cf2f8b199c7ec59e0e8a0fe7b5b
Mike Bostock <mbostock@gmail.com>
2023-06-20 09:33:11 -0700

    tweak chord example

--- docs/components/ExampleChord.vue
+++ docs/components/ExampleChord.vue
@@ -15,6 +15,9 @@ const matrix = [
   [ 1013,   990,  940, 6907]  // from red
 ];
 
+const colors = ["#000000", "#ffdd89", "#957244", "#f26223"];
+const names = ["black", "blond", "brown", "red"];
+
 const sum = d3.sum(matrix.flat());
 const tickStepMinor = d3.tickStep(0, sum, 100);
 const tickStepMajor = d3.tickStep(0, sum, 20);
@@ -35,14 +38,6 @@ const arc = d3.arc()
 const ribbon = d3.ribbon()
     .radius(innerRadius);
 
-const color = d3.scaleOrdinal()
-    .domain(d3.range(matrix.length))
-    .range(["#000000", "#ffdd89", "#957244", "#f26223"]);
-
-const name = d3.scaleOrdinal()
-    .domain(d3.range(matrix.length))
-    .range(["black", "blond", "brown", "red"]);
-
 const chords = d3.chord()
     .padAngle(0.05)
     .sortSubgroups(d3.descending)
@@ -53,9 +48,9 @@ const chords = d3.chord()
   <div style="margin: 1em 0;">
     <svg :width="width" :height="height" :viewBox="[-width / 2, -height / 2, width, height].join(' ')" fill="currentColor" style="font: 10px sans-serif; max-width: 100%; height: auto;">
       <g v-for="d in chords.groups">
-        <path :d="arc(d)" :fill="color(d.index)" :stroke="color(d.index)">
+        <path :d="arc(d)" :fill="colors[d.index]" :stroke="colors[d.index]">
           <title>
-            {{ `${name(d.index)}\n${d.value.toLocaleString("en-US")}` }}
+            {{ `${d.value.toLocaleString("en-US")} ${names[d.index]}` }}
           </title>
         </path>
         <g v-for="d in groupTicks(d, tickStepMinor)" :transform="`rotate(${d.angle * 180 / Math.PI - 90}) translate(${outerRadius},0)`">
@@ -64,9 +59,9 @@ const chords = d3.chord()
         </g>
       </g>
       <g fill-opacity="0.67" stroke="var(--vp-c-bg)">
-        <path v-for="d in chords" :d="ribbon(d)" :fill="color(d.target.index)">
+        <path v-for="d in chords" :d="ribbon(d)" :fill="colors[d.target.index]">
           <title>
-            {{ `${name(d.source.index)} → ${name(d.target.index)}: ${d.source.value.toLocaleString("en-US")}${d.source.index !== d.target.index ? `\n${name(d.target.index)} → ${name(d.source.index)}: ${d.target.value.toLocaleString("en-US")}` : ``}` }}
+            {{ `${d.source.value.toLocaleString("en-US")} ${names[d.source.index]} → ${names[d.target.index]}${d.source.index !== d.target.index ? `\n${d.target.value.toLocaleString("en-US")} ${names[d.target.index]} → ${names[d.source.index]}` : ``}` }}
           </title>
         </path>
       </g>
